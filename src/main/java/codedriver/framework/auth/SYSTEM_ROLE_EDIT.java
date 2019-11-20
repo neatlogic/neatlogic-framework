3a9aa930099f05a9614abaefaf6af7c7c497cef2
吕佐康 <lvzk@techsure.com.cn>
2019-11-20 11:10:24 +0800

    添加角色管理 注解

---
+++ src/main/java/codedriver/framework/auth/SYSTEM_ROLE_EDIT.java
@@ -0,0 +1,22 @@
+package codedriver.framework.auth;
+
+import codedriver.framework.common.auth.AuthBase;
+
+public class SYSTEM_ROLE_EDIT extends AuthBase{
+
+	@Override
+	public String getAuthDisplayName() {
+		return "角色管理权限";
+	}
+
+	@Override
+	public String getAuthIntroduction() {
+		return "拥有此权限才能查看，添加，修改和删除角色管理的内容。";
+	}
+
+	@Override
+	public String getAuthName() {
+		return "SYSTEM_ROLE_EDIT";
+	}
+
+}

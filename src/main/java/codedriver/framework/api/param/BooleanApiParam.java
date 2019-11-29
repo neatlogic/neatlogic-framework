810a6f24822872716c6dfbd5c8163a3d959f08a1
吕佐康 <lvzk@techsure.com.cn>
2019-11-29 11:28:40 +0800

    update boolean

---
+++ src/main/java/codedriver/framework/api/param/BooleanApiParam.java
@@ -0,0 +1,29 @@
+package codedriver.framework.api.param;
+
+import codedriver.framework.api.core.ApiParamBase;
+import codedriver.framework.api.core.ApiParamType;
+
+public class BooleanApiParam extends ApiParamBase {
+
+	@Override
+	public String getAuthName() {
+
+		return "整形参数认证";
+	}
+
+	@Override
+	public boolean doAuth(String param) {
+		try {
+			Boolean.valueOf(param);
+			return true;
+		} catch (Exception ex) {
+			return false;
+		}
+	}
+
+	@Override
+	public ApiParamType getAuthType() {
+		return ApiParamType.BOOLEAN;
+	}
+
+}

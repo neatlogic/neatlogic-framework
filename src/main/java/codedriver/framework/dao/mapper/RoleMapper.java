ca34b14e2fbaeff3b89bad94884ec5ff00981fd9
chenqiwei <chenqw@techsure.com.cn>
2019-12-08 11:31:51 +0800

    init

--- src/main/java/codedriver/framework/dao/mapper/RoleMapper.java
+++ src/main/java/codedriver/framework/dao/mapper/RoleMapper.java
@@ -1,25 +1,25 @@
 package codedriver.framework.dao.mapper;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
+
 import codedriver.framework.dto.RoleVo;
 
 public interface RoleMapper {
-	public List<RoleVo> selectAllRole();
-	
-	public List<RoleVo> getRoleByName(RoleVo roleVo);
-		
-	public RoleVo getRoleInfoByName(@Param("name") String name);
-		
+	public List<RoleVo> searchRole(RoleVo roleVo);
+
+	public int searchRoleCount(RoleVo roleVO);
+
+	public RoleVo getRoleByRoleName(String name);
+
 	public int insertRole(RoleVo roleVo);
-		
-	public int insertTeamChildrenRole(@Param("parentId") Long parentId, @Param("roleName") String roleName);
-	
+
 	public int updateRole(RoleVo roleVo);
-	
-	public int checkRoleNameExist(RoleVo roleVo);
-	
-	public int deleteRole(@Param("name") String name);
-	
-	public int deleteTeamRoleByRoleName(@Param("name") String name);
+
+	public int deleteRoleByRoleName(String name);
+
+	public int deleteMenuRoleByRoleName(String name);
+
+	public int deleteUserRoleByRoleName(String name);
+
+	public int deleteTeamRoleByRoleName(String name);
 }

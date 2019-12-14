d4cd02c374f4a1726760bc4ca748873b3f333a73
林邦泉 <linbq@techsure.com.cn>
2019-12-14 09:38:06 +0800

    --story=1002071 --user=林邦泉 接口日志 https://www.tapd.cn/54247054/s/1038118

--- src/main/java/codedriver/framework/restful/logger/ApiAuditLogger.java
+++ src/main/java/codedriver/framework/restful/logger/ApiAuditLogger.java
@@ -30,7 +30,7 @@ public class ApiAuditLogger {
 	private final static String API_LOG_CONFIG = "api_log_config";// 接口访问日志配置在config表的key值
 	private final static long FILE_SIZE_UNIT = 1024 * 1024; // 日志文件大小单位MB
 	private final static String DEFAULT_FILE = "logs/api.log";// 默认文件路径
-	private final static long DEFAULT_MAX_FILE_SIZE = 1 * FILE_SIZE_UNIT;// 默认单个文件大小
+	private final static long DEFAULT_MAX_FILE_SIZE = 10 * FILE_SIZE_UNIT;// 默认单个文件大小
 	private final static int DEFAULT_MAX_HISTORY = -1;//默认保留全部历史文件
 
 	public final static int THREAD_COUNT = 3;

47fcaf5833a2e5f40073d21fa3fb55f7ba2a728f
林邦泉 <linbq@techsure.com.cn>
2019-12-25 20:29:02 +0800

    --story=1001914 --user=林邦泉 服务目录页面优化 https://www.tapd.cn/54247054/s/1039990

--- src/main/java/codedriver/framework/restful/core/ApiComponentBase.java
+++ src/main/java/codedriver/framework/restful/core/ApiComponentBase.java
@@ -3,6 +3,8 @@ package codedriver.framework.restful.core;
 import java.lang.reflect.Method;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,12 @@ import org.springframework.util.ClassUtils;
 
 import com.alibaba.fastjson.JSONObject;
 
+import codedriver.framework.exception.core.ApiRuntimeException;
 import codedriver.framework.restful.dao.mapper.ApiMapper;
 import codedriver.framework.restful.dto.ApiVo;
 
 public abstract class ApiComponentBase extends ApiValidateAndHelpBase implements ApiComponent, MyApiComponent {
-	// private static final Logger logger =
-	// LoggerFactory.getLogger(ApiComponentBase.class.getName());
+	private static final Logger logger = LoggerFactory.getLogger(ApiComponentBase.class.getName());
 
 	@Autowired
 	private ApiMapper apiMapper;
@@ -43,7 +45,11 @@ public abstract class ApiComponentBase extends ApiValidateAndHelpBase implements
 				validApi(this.getClass(), paramObj, JSONObject.class);
 				result = myDoService(paramObj);
 			} catch (Exception ex) {
-				throw ex;
+				if(ex.getCause() != null && ex.getCause() instanceof ApiRuntimeException ){
+					throw new ApiRuntimeException(ex.getCause().getMessage());
+				}else {
+					throw ex;
+				}			
 			}
 		} catch (Exception e) {
 			error = e.getMessage() == null ? ExceptionUtils.getStackTrace(e) : e.getMessage();

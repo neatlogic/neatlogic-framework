5baf898f0e06c090d6532340c0b1328d9c7cd454
林邦泉 <linbq@techsure.com.cn>
2020-01-16 10:36:27 +0800

    --story=1002145 --user=林邦泉 流程管理&编辑页面 https://www.tapd.cn/54247054/s/1043110

--- src/main/java/codedriver/framework/inform/core/InformComponentFactory.java
+++ src/main/java/codedriver/framework/inform/core/InformComponentFactory.java
@@ -1,6 +1,8 @@
 package codedriver.framework.inform.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
@@ -8,6 +10,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 import codedriver.framework.common.RootComponent;
+import codedriver.framework.inform.dto.InformPluginTypeVo;
 
 /**
  * @program: codedriver
@@ -18,8 +21,13 @@ import codedriver.framework.common.RootComponent;
 public class InformComponentFactory implements ApplicationListener<ContextRefreshedEvent> {
     private static Map<String, InformComponentBase> informPluginMap = new HashMap<>();
 
+    private static List<InformPluginTypeVo> informPluginTypeList = new ArrayList<>();
+    
+    public static List<InformPluginTypeVo> getInformPluginTypeList(){
+    	return informPluginTypeList;
+    }
     public static InformComponentBase getInformPlugin(String informPluginType){
-        if (informPluginType.contains(informPluginType)){
+        if (informPluginMap.containsKey(informPluginType)){
             return informPluginMap.get(informPluginType);
         }
         throw new RuntimeException("找不到类型为：" + informPluginType + "的流程组件");
@@ -33,6 +41,10 @@ public class InformComponentFactory implements ApplicationListener<ContextRefres
             InformComponentBase plugin = entry.getValue();
             if (plugin.getId() != null){
                 informPluginMap.put(plugin.getId(), plugin);
+                InformPluginType informPluginType = InformPluginType.getInformParamType(plugin.getId());
+                if(informPluginType != null) {
+                	informPluginTypeList.add(new InformPluginTypeVo(informPluginType.getValue(), informPluginType.getText()));
+                }
             }
         }
     }

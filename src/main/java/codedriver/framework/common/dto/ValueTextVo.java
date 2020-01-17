f9629bc215e5f3e92da55d182d7ff73a9f937ec0
林邦泉 <linbq@techsure.com.cn>
2020-01-17 20:37:21 +0800

    --story=1002145 --user=林邦泉 流程管理&编辑页面 https://www.tapd.cn/54247054/s/1043571

---
+++ src/main/java/codedriver/framework/common/dto/ValueTextVo.java
@@ -0,0 +1,25 @@
+package codedriver.framework.common.dto;
+
+public class ValueTextVo {
+
+	private String value;
+	private String text;
+	public ValueTextVo() {
+	}
+	public ValueTextVo(String value, String text) {
+		this.value = value;
+		this.text = text;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+}

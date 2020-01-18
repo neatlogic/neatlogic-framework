9e4b44098f612995528473fa5926275c1b6b88d0
chenqiwei <chenqw@techsure.com.cn>
2020-01-18 21:21:11 +0800

    init

--- src/main/java/codedriver/framework/common/dto/ValueTextVo.java
+++ src/main/java/codedriver/framework/common/dto/ValueTextVo.java
@@ -4,21 +4,27 @@ public class ValueTextVo {
 
 	private String value;
 	private String text;
+
 	public ValueTextVo() {
 	}
+
 	public ValueTextVo(String value, String text) {
 		this.value = value;
 		this.text = text;
 	}
+
 	public String getValue() {
 		return value;
 	}
+
 	public void setValue(String value) {
 		this.value = value;
 	}
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}

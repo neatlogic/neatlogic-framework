5af8bf5eae98b54231fcaf970e40b671d3121568
吕佐康 <lvzk@techsure.com.cn>
2020-04-08 20:54:23 +0800

    update

--- src/main/java/codedriver/framework/common/constvalue/GroupSearch.java
+++ src/main/java/codedriver/framework/common/constvalue/GroupSearch.java
@@ -3,7 +3,9 @@ package codedriver.framework.common.constvalue;
 public enum GroupSearch {
 	USER("user", "用户类型"),
 	TEAM("team", "组类型"),
-	ROLE("role", "角色类型");
+	ROLE("role", "角色类型"),
+	COMMON("common", "公共类型"),
+	;
 	
 	private String value;
 	private String text;

28be811393ae5dba487e498f289f3b14a6e588b0
chenqiwei <chenqw@techsure.com.cn>
2020-05-11 22:44:15 +0800

    test

--- src/main/java/codedriver/framework/heartbeat/core/HeartbeatManager.java
+++ src/main/java/codedriver/framework/heartbeat/core/HeartbeatManager.java
@@ -15,6 +15,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -30,6 +32,7 @@ import codedriver.framework.heartbeat.dto.ServerClusterVo;
 import codedriver.framework.heartbeat.dto.ServerCounterVo;
 
 @RootComponent
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class HeartbeatManager extends ApplicationListenerBase  {
 	private Logger logger = LoggerFactory.getLogger(HeartbeatManager.class);
 	@Autowired

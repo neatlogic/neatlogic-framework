deaaaa896e1aca00b8d5f67b659029f5f88a8fd3
吕佐康 <lvzk@techsure.com.cn>
2020-05-16 16:13:06 +0800

    update

--- src/main/java/codedriver/framework/dashboard/core/DashboardChartFactory.java
+++ src/main/java/codedriver/framework/dashboard/core/DashboardChartFactory.java
@@ -1,6 +1,8 @@
 package codedriver.framework.dashboard.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -8,6 +10,7 @@ import org.reflections.Reflections;
 
 public class DashboardChartFactory {
 	private static Map<String, DashboardChartBase> chartMap = new HashMap<>();
+	private static List<DashboardChartBase> charList = new ArrayList<DashboardChartBase>();
 	static {
 		Reflections reflections = new Reflections("codedriver.framework.dashboard.core.charts");
 		Set<Class<? extends DashboardChartBase>> modules = reflections.getSubTypesOf(DashboardChartBase.class);
@@ -17,6 +20,7 @@ public class DashboardChartFactory {
 				chart = c.newInstance();
 				for (String ch : chart.getSupportChart()) {
 					chartMap.put(ch, chart);
+					charList.add(chart);
 				}
 			} catch (Exception ex) {
 				ex.printStackTrace();
@@ -26,5 +30,9 @@ public class DashboardChartFactory {
 
 	public static DashboardChartBase getChart(String type) {
 		return chartMap.get(type);
+	}
+	
+	public static List<DashboardChartBase> getDashboardChartList() {
+		return charList;
 	}
 }

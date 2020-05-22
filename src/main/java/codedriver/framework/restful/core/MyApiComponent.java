dfa6bf2808d90b33a93aeac6d2db95ec622c3b40
chenqiwei <chenqw@techsure.com.cn>
2020-05-22 17:18:13 +0800

    增加测试代码

--- src/main/java/codedriver/framework/restful/core/MyApiComponent.java
+++ src/main/java/codedriver/framework/restful/core/MyApiComponent.java
@@ -4,4 +4,8 @@ import com.alibaba.fastjson.JSONObject;
 
 public interface MyApiComponent extends IApiComponent {
 	public abstract Object myDoService(JSONObject jsonObj) throws Exception;
+
+	public default Object myDoTest(JSONObject jsonObj) {
+		return null;
+	}
 }

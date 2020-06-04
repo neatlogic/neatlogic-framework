0f88711c8c48e1850809545676093389c6cbdb34
林邦泉 <linbq@techsure.com.cn>
2020-06-04 13:06:31 +0800

    --story=1002990 --user=林邦泉 通知配置采用通知策略，以及通知模板优化 https://www.tapd.cn/54247054/s/1068838

--- src/main/java/codedriver/framework/notify/core/NotifyPolicyHandlerBase.java
+++ src/main/java/codedriver/framework/notify/core/NotifyPolicyHandlerBase.java
@@ -25,26 +25,6 @@ public abstract class NotifyPolicyHandlerBase implements INotifyPolicyHandler{
 		for(BasicType type : BasicType.values()) {
 			resultList.add(new ValueTextVo(type.getName(), type.getText()));
 		}
-//		for (ApiParamType type : ApiParamType.values()) {
-//			NotifyPolicyParamTypeVo notifyPolicyParamTypeVo = new NotifyPolicyParamTypeVo();
-//			notifyPolicyParamTypeVo.setHandler(type.getValue());
-//			notifyPolicyParamTypeVo.setHandlerName(type.getText());
-//			notifyPolicyParamTypeVo.setHandlerType("input");
-//			notifyPolicyParamTypeVo.setType("base");
-//			notifyPolicyParamTypeVo.setDefaultExpression("equals");
-//			List<ProcessExpressionVo> expressionList = new ArrayList<>();
-//			ProcessExpressionVo processExpressionVo1 = new ProcessExpressionVo();
-//			processExpressionVo1.setExpression("equals");
-//			processExpressionVo1.setExpressionName("等于");
-//			expressionList.add(processExpressionVo1);
-//			notifyPolicyParamTypeVo.setExpressionList(expressionList);
-//			ProcessExpressionVo processExpressionVo2 = new ProcessExpressionVo();
-//			processExpressionVo2.setExpression("like");
-//			processExpressionVo2.setExpressionName("包含");
-//			expressionList.add(processExpressionVo2);
-//			notifyPolicyParamTypeVo.setExpressionList(expressionList);
-//			resultList.add(notifyPolicyParamTypeVo);
-//		}
 		return resultList;
 	}
 

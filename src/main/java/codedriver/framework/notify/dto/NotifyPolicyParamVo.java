3efd99d9aac11b1074b698e305fa083d9e468abb
林邦泉 <linbq@techsure.com.cn>
2020-06-05 10:07:53 +0800

    --story=1002990 --user=林邦泉 通知配置采用通知策略，以及通知模板优化 https://www.tapd.cn/54247054/s/1069070

--- src/main/java/codedriver/framework/notify/dto/NotifyPolicyParamVo.java
+++ src/main/java/codedriver/framework/notify/dto/NotifyPolicyParamVo.java
@@ -7,7 +7,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
 
-public class NotifyPolicyParamVo  {
+public class NotifyPolicyParamVo {
 
 	private String handler;
 	private String handlerName;

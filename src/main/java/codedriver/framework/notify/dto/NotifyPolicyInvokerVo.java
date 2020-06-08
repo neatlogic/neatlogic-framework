6a317396262cb78aa6fb80019ea2a8d8470dafb8
林邦泉 <linbq@techsure.com.cn>
2020-06-08 19:07:18 +0800

    --story=1002990 --user=林邦泉 通知配置采用通知策略，以及通知模板优化 https://www.tapd.cn/54247054/s/1069958

--- src/main/java/codedriver/framework/notify/dto/NotifyPolicyInvokerVo.java
+++ src/main/java/codedriver/framework/notify/dto/NotifyPolicyInvokerVo.java
@@ -10,8 +10,6 @@ public class NotifyPolicyInvokerVo extends BasePageVo {
 	private Long policyId;
 	private String invoker;
 	private JSONObject config;
-//	private String function;
-//	private String name;
 	
 	public Long getPolicyId() {
 		return policyId;
@@ -26,18 +24,6 @@ public class NotifyPolicyInvokerVo extends BasePageVo {
 		this.invoker = invoker;
 	}
 	public JSONObject getConfig() {
-//		if(MapUtils.isEmpty(config)) {
-//			JSONObject configObj = new JSONObject();
-//			if(StringUtils.isNotBlank(function)) {
-//				configObj.put("function", function);
-//			}
-//			if(StringUtils.isNotBlank(name)) {
-//				configObj.put("name", name);
-//			}
-//			if(MapUtils.isNotEmpty(configObj)) {
-//				config = configObj;
-//			}
-//		}
 		return config;
 	}
 	public void setConfig(String config) {
@@ -56,22 +42,4 @@ public class NotifyPolicyInvokerVo extends BasePageVo {
 		return null;
 	}
 	
-//	public String getFunction() {
-//		if(StringUtils.isBlank(function) && MapUtils.isNotEmpty(config)) {
-//			function = config.getString("function");
-//		}
-//		return function;
-//	}
-//	public void setFunction(String function) {
-//		this.function = function;
-//	}
-//	public String getName() {
-//		if(StringUtils.isBlank(name) && MapUtils.isNotEmpty(config)) {
-//			name = config.getString("name");
-//		}
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
 }

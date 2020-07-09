32f45e4a5036a0df47c475c035a9681810eb204d
林邦泉 <linbq@techsure.com.cn>
2020-07-09 15:12:41 +0800

    --story=1003107 --user=林邦泉 条件分流节点异常提示 https://www.tapd.cn/54247054/s/1076319

--- src/main/java/codedriver/framework/condition/core/ConditionHandlerFactory.java
+++ src/main/java/codedriver/framework/condition/core/ConditionHandlerFactory.java
@@ -1,5 +1,47 @@
 package codedriver.framework.condition.core;
 
-public class ConditionHandlerFactory {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import codedriver.framework.applicationlistener.core.ApplicationListenerBase;
+import codedriver.framework.common.RootComponent;
+
+@RootComponent
+public class ConditionHandlerFactory extends ApplicationListenerBase {
+
+	private static Map<String, IConditionHandler> conditionHandlerMap = new HashMap<>();
+	
+	public static IConditionHandler getHandler(String name) {
+		return conditionHandlerMap.get(name);
+	}
+	
+	public static List<IConditionHandler> getConditionHandlerList(){
+		List<IConditionHandler> conditionHandlerList = new ArrayList<>();
+		for(Entry<String, IConditionHandler> entry : conditionHandlerMap.entrySet()) {
+			conditionHandlerList.add(entry.getValue());
+		}
+		return conditionHandlerList;
+	}
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		ApplicationContext context = event.getApplicationContext();
+		Map<String, IConditionHandler> myMap = context.getBeansOfType(IConditionHandler.class);
+		for(Entry<String, IConditionHandler> entry : myMap.entrySet()) {
+			conditionHandlerMap.put(entry.getValue().getName(), entry.getValue());
+		}
+	}
+
+	@Override
+	protected void myInit() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/codedriver/framework/condition/core/IConditionHandler.java
+++ src/main/java/codedriver/framework/condition/core/IConditionHandler.java
@@ -1,5 +1,65 @@
 package codedriver.framework.condition.core;
 
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.common.constvalue.ParamType;
+
 public interface IConditionHandler {
 
+	/**
+	 * @Description: 条件英文名
+	 * @Param:
+	 * @return: java.lang.String
+	 * @Date: 2020/2/11
+	 */
+	public String getName();
+	
+	/**
+	 * @Description: 条件显示名
+	 * @Param:
+	 * @return: java.lang.String
+	 * @Date: 2020/2/11
+	 */
+	public String getDisplayName();
+
+	/**
+	 * @Description: 获取控件类型，
+	 * @Param: simple:目前用于用于工单中心条件过滤简单模式
+	 *         custom:目前用于用于工单中心条件过自定义模式、条件分流和sla条件
+	 * @return: java.lang.String
+	 * @Date: 2020/2/11
+	 */
+	public String getHandler(String processWorkcenterConditionType);
+	
+	/**
+	 * @Description: 获取类型
+	 * @Param: 
+	 * @return: java.lang.String
+	 * @Date: 2020/2/11
+	 */
+	public String getType();
+	
+	/**
+	 * @Description: 获取控件配置
+	 * @Param: 
+	 * @return: com.alibaba.fastjson.JSONObject
+	 * @Date: 2020/2/11
+	 */
+	public JSONObject getConfig();
+	
+	/**
+	 * @Description: 获取控件页面显示排序，越小越靠前
+	 * @Param: 
+	 * @return: java.lang.Integer
+	 * @Date: 2020/2/11
+	 */
+	public Integer getSort();
+	
+	/**
+	 * @Description: 基本类型（表达式）
+	 * @Param: 
+	 * @return: java.lang.Integer
+	 * @Date: 2020/2/11
+	 */
+	public ParamType getParamType();
 }

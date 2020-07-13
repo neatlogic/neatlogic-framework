020ea0dc27ebc41881ced352de8503ea1bee801c
chenqiwei <chenqw@techsure.com.cn>
2020-07-13 11:10:40 +0800

    adjust

--- src/main/java/codedriver/framework/util/LargeTextUtil.java
+++ src/main/java/codedriver/framework/util/LargeTextUtil.java
@@ -1,10 +1,27 @@
 package codedriver.framework.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import codedriver.framework.common.config.Config;
+import codedriver.framework.minio.core.MinioManager;
 
 @Component
 public class LargeTextUtil {
-//	private static Logger logger = LoggerFactory.getLogger(LargeTextUtil.class);
+	private static Logger logger = LoggerFactory.getLogger(LargeTextUtil.class);
+
+	@Autowired
+	private static MinioManager minioManager;
 //
 //	private static FileSystem fileSystem;
 
@@ -53,85 +70,85 @@ public class LargeTextUtil {
 //		return desFile.getAbsolutePath();
 //	}
 //
-//	public static String readContent(String filePath, Long start, Integer length) {
-//		String content = null;
-//		Reader reader = null;
-//		InputStream stream = null;
-//		if (StringUtils.isNotBlank(filePath) && length > 0) {
-//			try {
-//				stream = fileSystem.open(new Path(filePath));
-//				reader = new InputStreamReader(stream, "utf-8");
-//				int index = 0;
-//				char[] buffer = new char[length];
-//				if (start > 0) {
-//					reader.skip(start);
-//				}
-//				if ((index = reader.read(buffer)) > 0) {
-//					content = new String(buffer, 0, index);
-//				}
-//			} catch (IllegalArgumentException | IOException e) {
-//				logger.error(e.getMessage(), e);
-//			} finally {
-//				if (reader != null) {
-//					try {
-//						reader.close();
-//					} catch (IOException ex) {
-//						logger.error(ex.getMessage(), ex);
-//					}
-//				}
-//				if (stream != null) {
-//					try {
-//						stream.close();
-//					} catch (IOException ex) {
-//						logger.error(ex.getMessage(), ex);
-//					}
-//				}
-//			}
-//		}
-//		return content;
-//	}
-//
-//	public static void readContent(String filePath, OutputStream out) {
-//		if (StringUtils.isNotBlank(filePath)) {
-//			InputStream stream = null;
-//			try {
-//				stream = fileSystem.open(new Path(filePath));
-//				IOUtils.copy(stream, out);
-//			} catch (Exception ex) {
-//				logger.error(ex.getMessage(), ex);
-//			} finally {
-//				if (stream != null) {
-//					try {
-//						stream.close();
-//					} catch (IOException e) {
-//						logger.error("FileInputStream close error : " + e.getMessage(), e);
-//					}
-//				}
-//			}
-//		}
-//	}
-//
-//	public static String readContent(String filePath) {
-//		if (StringUtils.isNotBlank(filePath)) {
-//			String result = "";
-//			InputStream stream = null;
-//			try {
-//				stream = fileSystem.open(new Path(filePath));
-//				result = IOUtils.toString(stream, "utf-8");
-//			} catch (Exception ex) {
-//				logger.error(ex.getMessage(), ex);
-//			} finally {
-//				if (stream != null) {
-//					try {
-//						stream.close();
-//					} catch (IOException e) {
-//						logger.error(e.getMessage(), e);
-//					}
-//				}
-//			}
-//			return result;
-//		}
-//		return null;
-//	}
+	public static String readContent(String filePath, Long start, Integer length) {
+		String content = null;
+		Reader reader = null;
+		InputStream stream = null;
+		if (StringUtils.isNotBlank(filePath) && length > 0) {
+			try {
+				stream = minioManager.getObject(Config.MINIO_BUCKET(), filePath);
+				reader = new InputStreamReader(stream, "utf-8");
+				int index = 0;
+				char[] buffer = new char[length];
+				if (start > 0) {
+					reader.skip(start);
+				}
+				if ((index = reader.read(buffer)) > 0) {
+					content = new String(buffer, 0, index);
+				}
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (IOException ex) {
+						logger.error(ex.getMessage(), ex);
+					}
+				}
+				if (stream != null) {
+					try {
+						stream.close();
+					} catch (IOException ex) {
+						logger.error(ex.getMessage(), ex);
+					}
+				}
+			}
+		}
+		return content;
+	}
+
+	public static void readContent(String filePath, OutputStream out) {
+		if (StringUtils.isNotBlank(filePath)) {
+			InputStream stream = null;
+			try {
+				stream = minioManager.getObject(Config.MINIO_BUCKET(), filePath);
+				IOUtils.copy(stream, out);
+			} catch (Exception ex) {
+				logger.error(ex.getMessage(), ex);
+			} finally {
+				if (stream != null) {
+					try {
+						stream.close();
+					} catch (IOException e) {
+						logger.error("FileInputStream close error : " + e.getMessage(), e);
+					}
+				}
+			}
+		}
+	}
+
+	public static String readContent(String filePath) {
+		if (StringUtils.isNotBlank(filePath)) {
+			String result = "";
+			InputStream stream = null;
+			try {
+				stream = minioManager.getObject(Config.MINIO_BUCKET(), filePath);
+				result = IOUtils.toString(stream, "utf-8");
+			} catch (Exception ex) {
+				logger.error(ex.getMessage(), ex);
+			} finally {
+				if (stream != null) {
+					try {
+						stream.close();
+					} catch (IOException e) {
+						logger.error(e.getMessage(), e);
+					}
+				}
+			}
+			return result;
+		}
+		return null;
+	}
 
 }

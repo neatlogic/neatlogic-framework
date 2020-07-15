84ce47fe71d60e2c11ebd71e113e7cf2ab126d22
林邦泉 <linbq@techsure.com.cn>
2020-07-15 14:43:34 +0800

    --story=1003127 --user=林邦泉 接口访问次数统计 https://www.tapd.cn/54247054/s/1077239

--- src/main/java/codedriver/framework/restful/audit/ApiAccessCountUpdateThread.java
+++ src/main/java/codedriver/framework/restful/audit/ApiAccessCountUpdateThread.java
@@ -19,18 +19,23 @@ import codedriver.framework.asynchronization.thread.CodeDriverThread;
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.restful.dao.mapper.ApiMapper;
 import codedriver.framework.transaction.util.TransactionUtil;
-
+/**
+ * 
+* @Time:2020年7月15日
+* @ClassName: ApiAccessCountUpdateThread 
+* @Description: 接口访问次数统计任务类
+ */
 @Service
 public class ApiAccessCountUpdateThread extends CodeDriverThread {
 	
 	private static  Logger logger = LoggerFactory.getLogger(ApiAccessCountUpdateThread.class);
-	
-	private final static int ACCESS_COUNT_THRESHOLD = 1000;
+	/** 缓存访问次数阈值，同个租户访问1000次后写入数据库 **/
+	private final static int ACCESS_COUNT_THRESHOLD = 10;
 	
 	private static ApiMapper apiMapper;
 
 	private static TransactionUtil transactionUtil;
-
+	/** 缓存租户访问记录 **/
 	private static ConcurrentMap<String, List<String>> tenantAccessTokenMap = new ConcurrentHashMap<>();
 	
 	private String token;
@@ -55,8 +60,10 @@ public class ApiAccessCountUpdateThread extends CodeDriverThread {
 	protected void execute() {
 		if(StringUtils.isNotBlank(token)) {
 			String tenantUuid = TenantContext.get().getTenantUuid();
+			/** 从缓存中获取当前租户访问记录**/
 			List<String> accessTokenList = tenantAccessTokenMap.get(tenantUuid);
 			if(accessTokenList == null) {
+				/** 首次访问初始化缓存 **/
 				synchronized(ApiAccessCountUpdateThread.class) {
 					accessTokenList = tenantAccessTokenMap.get(tenantUuid);
 					if(accessTokenList == null) {
@@ -67,8 +74,9 @@ public class ApiAccessCountUpdateThread extends CodeDriverThread {
 			}
 			synchronized(accessTokenList) {
 				accessTokenList.add(token);
-//				System.out.println(accessTokenList.size());
+				/** 累计达到缓存次数阈值时，写入数据库并清空缓存 **/
 				if(accessTokenList.size() == ACCESS_COUNT_THRESHOLD) {
+					/** 统计每个接口的访问次数 **/
 					Map<String, Integer> tokenAccessCountMap = new HashMap<>();
 					for(String token : accessTokenList) {
 						Integer count = tokenAccessCountMap.get(token);

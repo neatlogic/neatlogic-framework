b9ca4a0f60340f0ebd9db11af68b93d97a73c414
Aienao <laiwt@techsure.com.cn>
2020-07-21 15:04:38 +0800

    操作审计暂存-改造API前暂存

---
+++ src/main/java/codedriver/framework/restful/annotation/OperationType.java
@@ -0,0 +1,16 @@
+package codedriver.framework.restful.annotation;
+
+import codedriver.framework.reminder.core.OperationTypeEnum;
+
+import java.lang.annotation.*;
+
+/**
+ * 用来标识API类型的注解
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface OperationType {
+
+	OperationTypeEnum type() default OperationTypeEnum.SEARCH;
+}

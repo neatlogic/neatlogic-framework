ecd962ba9eafcd0f6435322f1302fc150c8fa889
Aienao <laiwt@techsure.com.cn>
2020-08-12 16:15:27 +0800

    --story=1003188 --user=赖文韬 接口审计记录调整为存储文件 https://www.tapd.cn/54247054/s/1081968

---
+++ src/main/java/codedriver/framework/common/audit/AuditVoHandler.java
@@ -0,0 +1,29 @@
+package codedriver.framework.common.audit;
+
+public interface AuditVoHandler {
+
+	public String getParam();
+
+	public void setParam(String param);
+
+	public Object getResult();
+
+	public void setResult(String result);
+
+	public String getError();
+
+	public void setError(String error);
+
+	public String getParamFilePath();
+
+	public void setParamFilePath(String paramFilePath);
+
+	public String getResultFilePath();
+
+	public void setResultFilePath(String resultFilePath);
+
+	public String getErrorFilePath();
+
+	public void setErrorFilePath(String errorFilePath);
+
+}

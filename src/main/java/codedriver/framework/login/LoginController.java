9effe820f1238e9c6624aede63504b610b490a6e
chenqiwei <chenqw@techsure.com.cn>
2020-08-25 14:22:27 +0800

    cookie压缩

--- src/main/java/codedriver/framework/login/LoginController.java
+++ src/main/java/codedriver/framework/login/LoginController.java
@@ -1,8 +1,10 @@
 package codedriver.framework.login;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Base64;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.zip.GZIPOutputStream;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -39,113 +41,123 @@ import codedriver.framework.service.TenantService;
 @Controller
 @RequestMapping("/login/")
 public class LoginController {
-	Logger logger = LoggerFactory.getLogger(LoginController.class);
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-	@Autowired
-	private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
-	@Autowired
-	private TenantService tenantService;
+    @Autowired
+    private TenantService tenantService;
 
-	@RequestMapping(value = "/check/{tenant}")
-	public void dispatcherForPost(@RequestBody String json, @PathVariable("tenant") String tenant, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		JSONObject returnObj = new JSONObject();
-		JSONObject jsonObj = JSONObject.parseObject(json);
-		TenantContext tenantContext = TenantContext.init();
-		try {
-			
-			if (StringUtils.isBlank(tenant)) {
-				tenant = request.getHeader("Tenant");
-			}
-			if (StringUtils.isNotBlank(tenant)) {
-				// 使用master库
-				tenantContext.setUseDefaultDatasource(true);
-				TenantVo tenantVo = tenantService.getTenantByUuid(tenant);
-				if (tenantVo == null) {
-					throw new TenantNotFoundException(tenant);
-				}
-				if (tenantVo.getIsActive().equals(0)) {
-					throw new TenantUnActiveException(tenant);
-				}
-				tenantContext.switchTenant(tenant);
-				// 还原回租户库
-				tenantContext.setUseDefaultDatasource(false);
-			}
-			//验证并获取用户
-			UserVo  checkUserVo = null;
-			Map<String,ILoginAuth> loginAuthMap =  LoginAuthFactory.getLoginAuthMap();
-			for(Entry<String, ILoginAuth> loginAuthEntry : loginAuthMap.entrySet()) {
-				ILoginAuth loginAuth = loginAuthEntry.getValue();
-				checkUserVo =  loginAuth.auth(request, jsonObj);
-				if(checkUserVo != null && StringUtils.isNotBlank(checkUserVo.getUuid())) {
-					break;
-				}
-			}
-			if (checkUserVo != null) {
-				// 保存 user 登录访问时间
-				if (userMapper.getUserSessionByUserUuid(checkUserVo.getUuid()) != null) {
-					userMapper.updateUserSession(checkUserVo.getUuid());
-				} else {
-					userMapper.insertUserSession(checkUserVo.getUuid());
-				}
-				JSONObject jwtHeadObj = new JSONObject();
-				jwtHeadObj.put("alg", "HS256");
-				jwtHeadObj.put("typ", "JWT");
+    @RequestMapping(value = "/check/{tenant}")
+    public void dispatcherForPost(@RequestBody String json, @PathVariable("tenant") String tenant,
+        HttpServletRequest request, HttpServletResponse response) throws Exception {
+        JSONObject returnObj = new JSONObject();
+        JSONObject jsonObj = JSONObject.parseObject(json);
+        TenantContext tenantContext = TenantContext.init();
+        try {
 
-				JSONObject jwtBodyObj = new JSONObject();
-				jwtBodyObj.put("useruuid", checkUserVo.getUuid());
-				jwtBodyObj.put("userid", checkUserVo.getUserId());
-				jwtBodyObj.put("username", checkUserVo.getUserName());
-				jwtBodyObj.put("tenant", tenant);
-				if (CollectionUtils.isNotEmpty(checkUserVo.getRoleUuidList())) {
-					JSONArray roleList = new JSONArray();
-					for (String role : checkUserVo.getRoleUuidList()) {
-						roleList.add(role);
-					}
-					jwtBodyObj.put("rolelist", roleList);
-				}
+            if (StringUtils.isBlank(tenant)) {
+                tenant = request.getHeader("Tenant");
+            }
+            if (StringUtils.isNotBlank(tenant)) {
+                // 使用master库
+                tenantContext.setUseDefaultDatasource(true);
+                TenantVo tenantVo = tenantService.getTenantByUuid(tenant);
+                if (tenantVo == null) {
+                    throw new TenantNotFoundException(tenant);
+                }
+                if (tenantVo.getIsActive().equals(0)) {
+                    throw new TenantUnActiveException(tenant);
+                }
+                tenantContext.switchTenant(tenant);
+                // 还原回租户库
+                tenantContext.setUseDefaultDatasource(false);
+            }
+            // 验证并获取用户
+            UserVo checkUserVo = null;
+            Map<String, ILoginAuth> loginAuthMap = LoginAuthFactory.getLoginAuthMap();
+            for (Entry<String, ILoginAuth> loginAuthEntry : loginAuthMap.entrySet()) {
+                ILoginAuth loginAuth = loginAuthEntry.getValue();
+                checkUserVo = loginAuth.auth(request, jsonObj);
+                if (checkUserVo != null && StringUtils.isNotBlank(checkUserVo.getUuid())) {
+                    break;
+                }
+            }
+            if (checkUserVo != null) {
+                // 保存 user 登录访问时间
+                if (userMapper.getUserSessionByUserUuid(checkUserVo.getUuid()) != null) {
+                    userMapper.updateUserSession(checkUserVo.getUuid());
+                } else {
+                    userMapper.insertUserSession(checkUserVo.getUuid());
+                }
+                JSONObject jwtHeadObj = new JSONObject();
+                jwtHeadObj.put("alg", "HS256");
+                jwtHeadObj.put("typ", "JWT");
 
-				String jwthead = Base64.getUrlEncoder().encodeToString(jwtHeadObj.toJSONString().getBytes());
-				String jwtbody = Base64.getUrlEncoder().encodeToString(jwtBodyObj.toJSONString().getBytes());
+                JSONObject jwtBodyObj = new JSONObject();
+                jwtBodyObj.put("useruuid", checkUserVo.getUuid());
+                jwtBodyObj.put("userid", checkUserVo.getUserId());
+                jwtBodyObj.put("username", checkUserVo.getUserName());
+                jwtBodyObj.put("tenant", tenant);
+                if (CollectionUtils.isNotEmpty(checkUserVo.getRoleUuidList())) {
+                    JSONArray roleList = new JSONArray();
+                    for (String role : checkUserVo.getRoleUuidList()) {
+                        roleList.add(role);
+                    }
+                    jwtBodyObj.put("rolelist", roleList);
+                }
 
-				SecretKeySpec signingKey = new SecretKeySpec(Config.JWT_SECRET().getBytes(), "HmacSHA1");
-				Mac mac;
+                String jwthead = Base64.getUrlEncoder().encodeToString(jwtHeadObj.toJSONString().getBytes());
+                String jwtbody = Base64.getUrlEncoder().encodeToString(jwtBodyObj.toJSONString().getBytes());
 
-				mac = Mac.getInstance("HmacSHA1");
-				mac.init(signingKey);
-				byte[] rawHmac = mac.doFinal((jwthead + "." + jwtbody).getBytes());
-				String jwtsign = Base64.getUrlEncoder().encodeToString(rawHmac);
+                SecretKeySpec signingKey = new SecretKeySpec(Config.JWT_SECRET().getBytes(), "HmacSHA1");
+                Mac mac;
 
-				Cookie authCookie = new Cookie("codedriver_authorization", "Bearer_" + jwthead + "." + jwtbody + "." + jwtsign);
-				authCookie.setPath("/" + tenant);
-				String domainName = request.getServerName();
-				if (StringUtils.isNotBlank(domainName)) {
-					String[] ds = domainName.split("\\.");
-					int len = ds.length;
-					if (len > 2 && !StringUtils.isNumeric(ds[len - 1])) {
-						authCookie.setDomain(ds[len - 2] + "." + ds[len - 1]);
-					}
-				}
-				Cookie tenantCookie = new Cookie("codedriver_tenant", tenant);
-				tenantCookie.setPath("/" + tenant);
-				response.addCookie(authCookie);
-				response.addCookie(tenantCookie);
-				// 允许跨域携带cookie
-				response.setHeader("Access-Control-Allow-Credentials", "true");
-				response.setContentType(Config.RESPONSE_TYPE_JSON);
-				returnObj.put("Status", "OK");
-				returnObj.put("JwtToken", jwthead + "." + jwtbody + "." + jwtsign);
-				response.getWriter().print(returnObj);
-			} else {
-				throw new UserAuthFailedException();
-			}
-		} catch (UserAuthFailedException | TenantNotFoundException ex) {
-			ReturnJson.error(ex.getMessage(), response);
-		} catch (Exception ex) {
-			logger.error(ex.getMessage(), ex);
-			ReturnJson.error(ex.getMessage(), response);
-		} finally {
-			tenantContext.release();
-		}
-	}
+                mac = Mac.getInstance("HmacSHA1");
+                mac.init(signingKey);
+                byte[] rawHmac = mac.doFinal((jwthead + "." + jwtbody).getBytes());
+                String jwtsign = Base64.getUrlEncoder().encodeToString(rawHmac);
+
+                // 压缩cookie内容
+                String c = "Bearer_" + jwthead + "." + jwtbody + "." + jwtsign;
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                GZIPOutputStream gzipOutputStream = new GZIPOutputStream(bos);
+                gzipOutputStream.write(c.getBytes());
+                gzipOutputStream.close();
+                String cc = Base64.getEncoder().encodeToString(bos.toByteArray());
+                bos.close();
+
+                Cookie authCookie = new Cookie("codedriver_authorization", "GZIP_" + cc);
+                authCookie.setPath("/" + tenant);
+                String domainName = request.getServerName();
+                if (StringUtils.isNotBlank(domainName)) {
+                    String[] ds = domainName.split("\\.");
+                    int len = ds.length;
+                    if (len > 2 && !StringUtils.isNumeric(ds[len - 1])) {
+                        authCookie.setDomain(ds[len - 2] + "." + ds[len - 1]);
+                    }
+                }
+                Cookie tenantCookie = new Cookie("codedriver_tenant", tenant);
+                tenantCookie.setPath("/" + tenant);
+                response.addCookie(authCookie);
+                response.addCookie(tenantCookie);
+                // 允许跨域携带cookie
+                response.setHeader("Access-Control-Allow-Credentials", "true");
+                response.setContentType(Config.RESPONSE_TYPE_JSON);
+                returnObj.put("Status", "OK");
+                returnObj.put("JwtToken", jwthead + "." + jwtbody + "." + jwtsign);
+                response.getWriter().print(returnObj);
+            } else {
+                throw new UserAuthFailedException();
+            }
+        } catch (UserAuthFailedException | TenantNotFoundException ex) {
+            ReturnJson.error(ex.getMessage(), response);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            ReturnJson.error(ex.getMessage(), response);
+        } finally {
+            tenantContext.release();
+        }
+    }
 }

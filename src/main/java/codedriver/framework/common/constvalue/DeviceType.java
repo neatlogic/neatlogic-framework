94646e63f8717013a5a79dca611fab8ecf174531
吕佐康 <lvzk@techsure.com.cn>
2020-09-18 18:24:30 +0800

    --story=1003362 --user=lvzk PC-服务目录-服务扩展支持保存“使用范围（all/pc/移动端）” https://www.tapd.cn/54247054/s/1090814

---
+++ src/main/java/codedriver/framework/common/constvalue/DeviceType.java
@@ -0,0 +1,40 @@
+package codedriver.framework.common.constvalue;
+
+public enum DeviceType {
+	ALL("all","所有人"),MOBILE("mobile","手机端"),PC("pc","电脑端");
+
+	private String status;
+	private String text;
+
+	private DeviceType(String _status, String _text) {
+		this.status = _status;
+		this.text = _text;
+	}
+
+	public String getValue() {
+		return status;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public static String getValue(String _status) {
+		for (DeviceType s : DeviceType.values()) {
+			if (s.getValue().equals(_status)) {
+				return s.getValue();
+			}
+		}
+		return null;
+	}
+
+	public static String getText(String _status) {
+		for (DeviceType s : DeviceType.values()) {
+			if (s.getValue().equals(_status)) {
+				return s.getText();
+			}
+		}
+		return "";
+	}
+
+}

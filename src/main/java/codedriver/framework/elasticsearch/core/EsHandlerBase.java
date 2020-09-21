4c0d4fc6218993f9b89aa039a3a491cf6467250a
吕佐康 <lvzk@techsure.com.cn>
2020-09-21 18:44:30 +0800

    update es

--- src/main/java/codedriver/framework/elasticsearch/core/EsHandlerBase.java
+++ src/main/java/codedriver/framework/elasticsearch/core/EsHandlerBase.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.techsure.multiattrsearch.MultiAttrsObjectPool;
 
+import codedriver.framework.asynchronization.threadlocal.TenantContext;
+
 public abstract class EsHandlerBase implements IElasticSearchHandler {
 	
 	protected MultiAttrsObjectPool objectPool;
@@ -12,9 +14,12 @@ public abstract class EsHandlerBase implements IElasticSearchHandler {
 		if(objectPool == null) {
 		    objectPool = ElasticSearchPoolManager.getObjectPool(poolName);
 		}
-		if(objectPool != null && StringUtils.isNotBlank(tenant)) {
-		    objectPool.checkout(tenant);
-
+		if(objectPool != null) {
+		    if(StringUtils.isNotBlank(tenant)) {
+		        objectPool.checkout(tenant);
+		    }else {
+		        objectPool.checkout(TenantContext.get().getTenantUuid());
+		    }
 		}
 		return objectPool;
 	}

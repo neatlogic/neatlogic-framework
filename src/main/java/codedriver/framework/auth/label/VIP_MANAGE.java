952d3c42ae8abfabdcaa457f79cfdeb14910a84b
Aienao <laiwt@techsure.com.cn>
2020-10-13 17:31:42 +0800

    --story=1003435 --user=赖文韬 vip-后端 https://www.tapd.cn/54247054/s/1096373

---
+++ src/main/java/codedriver/framework/auth/label/VIP_MANAGE.java
@@ -0,0 +1,21 @@
+package codedriver.framework.auth.label;
+
+import codedriver.framework.auth.core.AuthBase;
+
+public class VIP_MANAGE extends AuthBase {
+
+	@Override
+	public String getAuthDisplayName() {
+		return "VIP管理权限";
+	}
+
+	@Override
+	public String getAuthIntroduction() {
+		return "vip用户拥有更高的工单优先级和SLA响应权限";
+	}
+
+	@Override
+	public String getAuthGroup() {
+		return "framework";
+	}
+}

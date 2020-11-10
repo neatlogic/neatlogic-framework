8cea20af7b7d513696533e160739bdc78ef139f7
林邦泉 <linbq@techsure.com.cn>
2020-11-10 11:35:12 +0800

    --bug=1005052 --user=林邦泉 工单处理-条件节点的条件是“上报内容”，表达式“等于”和“为空”没有生效 https://www.tapd.cn/54247054/s/1103626

--- src/main/java/codedriver/framework/dto/ConditionParamVo.java
+++ src/main/java/codedriver/framework/dto/ConditionParamVo.java
@@ -10,7 +10,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
 
 import codedriver.framework.common.constvalue.ParamType;
-import codedriver.framework.notify.dto.ExpressionVo;
+//import codedriver.framework.notify.dto.ExpressionVo;
 
 public class ConditionParamVo {
 	private String name;

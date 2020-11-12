05147ea19d4c784929bbfa64a74b7977eb045227
Aienao <laiwt@techsure.com.cn>
2020-11-12 15:39:46 +0800

    --story=1003608 --user=赖文韬 批量导入配置项-后端 https://www.tapd.cn/54247054/s/1103711

--- src/main/java/codedriver/framework/exception/team/FoundRepeatNameTeamException.java
+++ src/main/java/codedriver/framework/exception/team/FoundRepeatNameTeamException.java
@@ -7,6 +7,6 @@ public class FoundRepeatNameTeamException extends ApiRuntimeException {
 	private static final long serialVersionUID = -7791153993204463500L;
 
 	public FoundRepeatNameTeamException(String name) {
-		super("名为：" + name + "的分组存在多个");
+		super("存在多个名为：" + name + "的分组");
 	}
 }

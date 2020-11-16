f0c4386fad19c20a86d980a153403d5373b53326
chenqiwei <chenqw@techsure.com.cn>
2020-11-16 11:52:12 +0800

    adjust

--- src/main/java/codedriver/framework/lock/service/LockManager.java
+++ src/main/java/codedriver/framework/lock/service/LockManager.java
@@ -4,15 +4,16 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import codedriver.framework.common.RootConfiguration;
 import codedriver.framework.lock.dao.mapper.LockMapper;
 
 @Service
-public class LockServiceImpl implements LockService {
+@RootConfiguration
+public class LockManager {
 
     @Autowired
     private LockMapper lockMapper;
 
-    @Override
     public String getLockById(String lock) {
         if (StringUtils.isBlank(lockMapper.getLockById(lock))) {
             lockMapper.insertLock(lock);
--- src/main/java/codedriver/framework/lock/service/LockService.java
+++
@@ -1,6 +0,0 @@
-package codedriver.framework.lock.service;
-
-public interface LockService {
-    public String getLockById(String lock);
-
-}

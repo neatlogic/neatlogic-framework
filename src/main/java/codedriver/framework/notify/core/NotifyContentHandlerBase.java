2b0426bdcff96fb50b24e4a4a89feb67ecd0c310
Aienao <laiwt@techsure.com.cn>
2020-12-23 11:39:03 +0800

    --story=1003797 --user=赖文韬 定时发送待办任务通知 https://www.tapd.cn/54247054/s/1118262 【待我处理的工单插件】

--- src/main/java/codedriver/framework/notify/core/NotifyContentHandlerBase.java
+++ src/main/java/codedriver/framework/notify/core/NotifyContentHandlerBase.java
@@ -1,6 +1,30 @@
 package codedriver.framework.notify.core;
 
+import codedriver.framework.common.dto.ValueTextVo;
+import codedriver.framework.dto.ConditionParamVo;
+import com.alibaba.fastjson.JSONArray;
+
+import java.util.List;
+
 public abstract class NotifyContentHandlerBase implements INotifyContentHandler{
+    @Override
+    public List<ConditionParamVo> getConditionOptionList() {
+        return getMyConditionOptionList();
+    }
 
+    @Override
+    public JSONArray getMessageAttrList(String handler) {
+        return getMyMessageAttrList(handler);
+    }
 
+    @Override
+    public List<ValueTextVo> getDataColumnList() {
+        return getMyDataColumnList();
+    }
+
+    protected abstract List<ConditionParamVo> getMyConditionOptionList();
+
+    protected abstract JSONArray getMyMessageAttrList(String handler);
+
+    protected abstract List<ValueTextVo> getMyDataColumnList();
 }

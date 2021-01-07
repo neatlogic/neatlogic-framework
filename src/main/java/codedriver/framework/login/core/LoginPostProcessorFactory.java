3d50054618f31af8d19b56abd31c85a3da9b400e
林邦泉 <linbq@techsure.com.cn>
2021-01-07 11:56:08 +0800

    --story=1003521 --user=林邦泉 消息中心&系统公告 https://www.tapd.cn/54247054/s/1121895

--- src/main/java/codedriver/framework/login/core/LoginPostProcessorFactory.java
+++ src/main/java/codedriver/framework/login/core/LoginPostProcessorFactory.java
@@ -17,6 +17,8 @@ import java.util.Set;
  * @Description: 登录后处理器工厂类
  * @Author: linbq
  * @Date: 2021/1/6 15:06
+ * Copyright(c) 2020 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  **/
 @RootComponent
 public class LoginPostProcessorFactory extends ApplicationListenerBase {

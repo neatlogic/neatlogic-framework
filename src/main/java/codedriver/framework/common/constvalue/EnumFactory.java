a21c16d9795d0e5c908af2f1478f158f1404d476
Aienao <laiwt@techsure.com.cn>
2021-01-12 19:12:59 +0800

    --story=1003883 --user=赖文韬 统一枚举接口 https://www.tapd.cn/54247054/s/1123761

--- src/main/java/codedriver/framework/common/constvalue/EnumFactory.java
+++ src/main/java/codedriver/framework/common/constvalue/EnumFactory.java
@@ -3,7 +3,6 @@ package codedriver.framework.common.constvalue;
 import org.reflections.Reflections;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,16 +17,14 @@ import java.util.Set;
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  **/
 public class EnumFactory{
-    private static Map<String, List> enumMap = new HashMap<>();
+    private static Map<String, Class<? extends IEnum>> enumMap = new HashMap<>();
 
     static {
         Reflections reflections = new Reflections("codedriver");
         Set<Class<? extends IEnum>> enumClasses = reflections.getSubTypesOf(IEnum.class);
         for (Class<? extends IEnum> c: enumClasses) {
             try {
-                Object[] objects = c.getEnumConstants();
-                List valueTextList = (List) c.getMethod("getValueTextList").invoke(objects[0]);
-                enumMap.put(c.getName(), valueTextList);
+                enumMap.put(c.getName(), c);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -35,13 +32,13 @@ public class EnumFactory{
     }
 
     /**
-     * @Description: 通过枚举类的完整类名获取其枚举值
+     * @Description: 通过枚举的全类名获取枚举类
      * @Author: laiwt
-     * @Date: 2021/1/12 11:29
+     * @Date: 2021/1/12 19:10
      * @Params: [enumClass]
-     * @Returns: com.alibaba.fastjson.JSONArray
+     * @Returns: java.lang.Class<? extends codedriver.framework.common.constvalue.IEnum>
     **/
-    public static List getValueTextList(String enumClass) {
+    public static Class<? extends IEnum> getEnumClass(String enumClass) {
         return enumMap.get(enumClass);
     }
 

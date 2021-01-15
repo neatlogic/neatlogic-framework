c8e1224900923eabf1371a48be5489b0e12eac6d
Aienao <laiwt@techsure.com.cn>
2021-01-15 09:30:46 +0800

    --story=1003884 --user=赖文韬 系统公告-后端 https://www.tapd.cn/54247054/s/1124599

--- src/main/java/codedriver/framework/systemnotice/exception/SystemNoticeHasBeenIssuedException.java
+++ src/main/java/codedriver/framework/systemnotice/exception/SystemNoticeHasBeenIssuedException.java
@@ -7,6 +7,6 @@ public class SystemNoticeHasBeenIssuedException extends ApiRuntimeException {
 	private static final long serialVersionUID = 3061025905329230055L;
 
 	public SystemNoticeHasBeenIssuedException(String title) {
-		super("系统公告：'" + title + "'已下发，不可编辑或删除");
+		super("系统公告：'" + title + "'已下发，不可操作");
 	}
 }

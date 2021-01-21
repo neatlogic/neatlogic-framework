146b8407038aa8f004172229eebf3a403a9b9d22
chenqiwei <chenqw@techsure.com.cn>
2021-01-21 16:57:57 +0800

    修改类名
    Signed-off-by: chenqiwei <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/exception/database/DataBaseNotFoundException.java
+++ src/main/java/codedriver/framework/exception/database/DataBaseNotFoundException.java
@@ -3,10 +3,10 @@ package codedriver.framework.exception.database;
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.exception.core.ApiRuntimeException;
 
-public class DataBaseNotFoundExceptionException extends ApiRuntimeException {
+public class DataBaseNotFoundException extends ApiRuntimeException {
 
 
-    public DataBaseNotFoundExceptionException() {
+    public DataBaseNotFoundException() {
         super("租户：" + TenantContext.get().getTenantUuid() + "缺少DATA数据库，请补充创建。");
     }
 }

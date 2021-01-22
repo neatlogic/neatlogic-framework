17582009677ae5db2edc2136c8c43cf5c2d640e9
吕佐康 <lvzk@techsure.com.cn>
2021-01-22 19:02:20 +0800

    --story=1003928 --user=lvzk 工单中心后端重构-对接工单中心查询-where过滤工单id https://www.tapd.cn/54247054/s/1128919

--- src/main/java/codedriver/framework/common/constvalue/Expression.java
+++ src/main/java/codedriver/framework/common/constvalue/Expression.java
@@ -1,20 +1,21 @@
 package codedriver.framework.common.constvalue;
 
 import codedriver.framework.dto.ExpressionVo;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public enum Expression implements IEnum{
-	LIKE("like", "包含", " %s contains %s ", " %s.%s like '%s%' ",1),
-	NOTLIKE("notlike", "不包含", " not %s contains %s ", " %s.%s not like '%s%' ",1),
-	EQUAL("equal", "等于", " %s = %s ", " %s.%s = %s ",1),
-	UNEQUAL("unequal", "不等于", " not %s = %s "," %s.%s != %s ",1),
-	INCLUDE("include", "包括", " %s contains any ( %s ) "," %s.%s in ( %s ) ",1),
-	EXCLUDE("exclude", "不包括", " not %s contains any ( %s ) "," %s.%s not in ( %s ) ",1),
+	LIKE("like", "包含", " %s contains %s ", " %s.%s like '%s%%' ",1),
+	NOTLIKE("notlike", "不包含", " not %s contains %s ", " %s.%s not like '%s%%' ",1),
+	EQUAL("equal", "等于", " %s = %s ", " %s.%s = '%s' ",1),
+	UNEQUAL("unequal", "不等于", " not %s = %s "," %s.%s != '%s' ",1),
+	INCLUDE("include", "包括", " %s contains any ( %s ) "," %s.%s in ( '%s' ) ",1),
+	EXCLUDE("exclude", "不包括", " not %s contains any ( %s ) "," %s.%s not in ( '%s' ) ",1),
 	BETWEEN("between","属于"," %s between '%s' and '%s' "," %s.%s between '%s' and '%s' ",1),
-	GREATERTHAN("greater-than", "晚于", " %s > %s "," %s.%s > %s ",1),
-	LESSTHAN("less-than", "早于", " %s < %s "," %s.%s < %s ",1),
+	GREATERTHAN("greater-than", "晚于", " %s > %s "," %s.%s > '%s' ",1),
+	LESSTHAN("less-than", "早于", " %s < %s "," %s.%s < '%s' ",1),
 	ISNULL("is-null", "为空", " %s = '' "," %s.%s is null ",0),
 	MATCH("match","包含(分词)"," %s match '%s'"," %s.%s match ( %s ) against (' %s ' IN BOOLEAN MODE) ",0),
 	ISNOTNULL("is-not-null", "不为空", " not %s = '' "," %s is not null ",0);
@@ -70,13 +71,17 @@ public enum Expression implements IEnum{
 		return null;
 	}
 
-	public static String getExpressionSql(String _expression) {
+	public static String getExpressionSql(String _expression,String ...values) {
+//		int valueSize = values.length;
+//		List<String> valueList = Arrays.asList(values);
+		String expression = StringUtils.EMPTY;
 		for (Expression s : Expression.values()) {
 			if (s.getExpression().equals(_expression)) {
-				return s.getExpressionSql();
+				expression = s.getExpressionSql();
+				break;
 			}
 		}
-		return null;
+		return String.format(expression,values);
 	}
 	
 	public static Expression getProcessExpression(String _expression) {

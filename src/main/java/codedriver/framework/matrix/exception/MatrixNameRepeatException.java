554ec95f94750d3c158b01e426edd8620d5ddee2
Aienao <laiwt@techsure.com.cn>
2021-02-22 17:46:16 +0800

    --story=1004033 --user=赖文韬 校验提示样式统一 https://www.tapd.cn/54247054/s/1136286

--- src/main/java/codedriver/framework/matrix/exception/MatrixNameRepeatException.java
+++ src/main/java/codedriver/framework/matrix/exception/MatrixNameRepeatException.java
@@ -1,12 +1,12 @@
 package codedriver.framework.matrix.exception;
 
-import codedriver.framework.exception.core.ApiRuntimeException;
+import codedriver.framework.exception.core.ApiFieldValidRuntimeException;
 
-public class MatrixNameRepeatException extends ApiRuntimeException {
+public class MatrixNameRepeatException extends ApiFieldValidRuntimeException {
 
 	private static final long serialVersionUID = -9118655742835275741L;
 
 	public MatrixNameRepeatException(String name) {
-		super("矩阵名 :'" + name + "'已存在");
+		super("矩阵:'" + name + "'已存在");
 	}
 }

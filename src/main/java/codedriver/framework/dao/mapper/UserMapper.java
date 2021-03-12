58d19b0a9b5df7ef01a98fc03612851ae462279d
Aienao <laiwt@techsure.com.cn>
2021-03-12 19:31:00 +0800

    --bug=1007549 --user=赖文韬 定时任务-发送的定时消... https://www.tapd.cn/54247054/s/1142236

--- src/main/java/codedriver/framework/dao/mapper/UserMapper.java
+++ src/main/java/codedriver/framework/dao/mapper/UserMapper.java
@@ -70,13 +70,13 @@ public interface UserMapper {
     public List<String> getUserUuidListByTeamUuidList(List<String> teamUuidList);
 
     /**
-     * @Description: 根据分组uuid，找出所有用户uuid与其所在所有分组的uuid
+     * @Description: 根据分组uuid，找出所有用户uuid与其所在所有分组的uuid和角色uuid
      * @Author: laiwt
      * @Date: 2021/3/12 15:15
      * @Params: [teamUuidList]
      * @Returns: java.util.List<codedriver.framework.dto.UserVo>
     **/
-    public List<UserVo> getUserTeamListByTeamUuidList(List<String> teamUuidList);
+    public List<UserVo> getUserTeamRoleListByTeamUuidList(List<String> teamUuidList);
 
     public List<String> getUserUuidListByRoleUuidList(List<String> roleUuidList);
 

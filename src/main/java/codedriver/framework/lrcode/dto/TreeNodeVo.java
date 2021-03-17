3f0b57e524fa35c412bb89b9431a3efd7544a910
林邦泉 <linbq@techsure.com.cn>
2021-03-17 17:28:58 +0800

    --story=1003130 --user=林邦泉 树结构代码优化 https://www.tapd.cn/54247054/s/1143741

---
+++ src/main/java/codedriver/framework/lrcode/dto/TreeNodeVo.java
@@ -0,0 +1,110 @@
+package codedriver.framework.lrcode.dto;
+
+import codedriver.framework.common.dto.BasePageVo;
+
+import java.io.Serializable;
+
+/**
+ * @Title: TreeNodeVo
+ * @Package codedriver.framework.tree.dto
+ * @Description: TODO
+ * @Author: linbq
+ * @Date: 2021/3/17 6:30
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ **/
+public class TreeNodeVo {
+
+    public final static String ROOT_UUID = "0";
+    public final static String ROOT_PARENTUUID = "-1";
+
+    private String idKey;
+    private String parentIdKey;
+    private Object idValue;
+    private Object parentIdValue;
+//    private String uuid;
+//    private String parentUuid;
+    private String tableName;
+    private Integer lft;
+    private Integer rht;
+    private int childrenCount;
+
+    public String getIdKey() {
+        return idKey;
+    }
+
+    public void setIdKey(String idKey) {
+        this.idKey = idKey;
+    }
+
+    public String getParentIdKey() {
+        return parentIdKey;
+    }
+
+    public void setParentIdKey(String parentIdKey) {
+        this.parentIdKey = parentIdKey;
+    }
+
+    public Object getIdValue() {
+        return idValue;
+    }
+
+    public void setIdValue(Object idValue) {
+        this.idValue = idValue;
+    }
+
+    public Object getParentIdValue() {
+        return parentIdValue;
+    }
+
+    public void setParentIdValue(Object parentIdValue) {
+        this.parentIdValue = parentIdValue;
+    }
+//    public String getUuid() {
+//        return uuid;
+//    }
+//
+//    public void setUuid(String uuid) {
+//        this.uuid = uuid;
+//    }
+//
+//    public String getParentUuid() {
+//        return parentUuid;
+//    }
+//
+//    public void setParentUuid(String parentUuid) {
+//        this.parentUuid = parentUuid;
+//    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public Integer getLft() {
+        return lft;
+    }
+
+    public void setLft(Integer lft) {
+        this.lft = lft;
+    }
+
+    public Integer getRht() {
+        return rht;
+    }
+
+    public void setRht(Integer rht) {
+        this.rht = rht;
+    }
+
+    public int getChildrenCount() {
+        return childrenCount;
+    }
+
+    public void setChildrenCount(int childrenCount) {
+        this.childrenCount = childrenCount;
+    }
+}

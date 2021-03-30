549e88f9c6ca52cdb33df8ad90904fad06a0e0d5
Aienao <laiwt@techsure.com.cn>
2021-03-30 10:11:34 +0800

    --story=1004251 --user=赖文韬 【Bug转需求】矩阵管理... https://www.tapd.cn/54247054/s/1146561

---
+++ src/main/java/codedriver/framework/matrix/exception/MatrixExternalDataIsNotJsonException.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.matrix.exception;
+
+import codedriver.framework.exception.core.ApiRuntimeException;
+
+public class MatrixExternalDataIsNotJsonException extends ApiRuntimeException {
+
+    private static final long serialVersionUID = 7391664365834538416L;
+
+    public MatrixExternalDataIsNotJsonException() {
+        super("集成配置接口返回结果不是JSON格式");
+    }
+}

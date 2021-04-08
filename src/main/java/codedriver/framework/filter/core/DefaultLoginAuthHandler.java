4253f7b4a9c9d4783e51bd52ff063b7154a1cf2a
林邦泉 <linbq@techsure.com.cn>
2021-04-08 10:11:43 +0800

    --bug=1007785 --user=林邦泉 【消息中心】消息通知-数量显示有误 https://www.tapd.cn/54247054/s/1148843

--- src/main/java/codedriver/framework/filter/core/DefaultLoginAuthHandler.java
+++ src/main/java/codedriver/framework/filter/core/DefaultLoginAuthHandler.java
@@ -1,7 +1,6 @@
 package codedriver.framework.filter.core;
 
 import codedriver.framework.common.config.Config;
-import codedriver.framework.common.constvalue.SystemUser;
 import codedriver.framework.dto.UserVo;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -105,10 +104,6 @@ public class DefaultLoginAuthHandler extends LoginAuthHandlerBase {
                 }
             }
 
-        }
-        String abc = request.getHeader("abc");
-        if(abc.equals("true")){
-            return SystemUser.SYSTEM.getUserVo();
         }
         return userVo;
     }

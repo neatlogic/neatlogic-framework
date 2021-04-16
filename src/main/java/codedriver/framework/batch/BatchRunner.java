6c7fec7b799d2a1beba3451b3e72c78375b49ee3
陈其炜 <chenqw@techsure.com.cn>
2021-04-16 11:17:39 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/batch/BatchRunner.java
+++ src/main/java/codedriver/framework/batch/BatchRunner.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.batch;
 
 import codedriver.framework.asynchronization.thread.CodeDriverThread;
@@ -32,7 +37,7 @@ public class BatchRunner<T> {
             try {
                 latch.await();
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                logger.error(e.getMessage(), e);
             }
         }
     }
@@ -70,6 +75,4 @@ public class BatchRunner<T> {
             }
         }
     }
-
-
 }

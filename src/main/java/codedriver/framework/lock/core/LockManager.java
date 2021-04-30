b4918376b90c591713e0fd09aecb83e5a2459a2a
陈其炜 <chenqw@techsure.com.cn>
2021-04-30 18:27:11 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/lock/core/LockManager.java
+++ src/main/java/codedriver/framework/lock/core/LockManager.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.lock.core;
 
 import codedriver.framework.common.RootConfiguration;
@@ -10,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.sql.SQLException;
 
 @Service
 @RootConfiguration
@@ -29,27 +36,47 @@ public class LockManager {
         return lockMapper.getLockByIdForUpdate(lock);
     }
 
+    /**
+     * 执行串行操作
+     *
+     * @param lock   锁唯一名
+     * @param method 逻辑
+     */
     public static void invoke(String lock, ISerialMethod method) {
         lock = Md5Util.encryptMD5(lock);
-        boolean needOpenNewTx = false;
-        if (TransactionSynchronizationManager.isSynchronizationActive()) {
-            needOpenNewTx = true;
-        }
-        TransactionStatus transactionStatus = null;
-        if (needOpenNewTx) {
-            transactionStatus = TransactionUtil.openTx();
-        }
-        lockMapper.insertLock(lock);
-        lockMapper.getLockByIdForUpdate(lock);
-        try {
-            method.execute();
-        } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
-        }
-        lockMapper.deleteLock(lock);
-        if (needOpenNewTx) {
-            TransactionUtil.commitTx(transactionStatus);
+        while (true) {
+            boolean needOpenNewTx = TransactionSynchronizationManager.isSynchronizationActive();
+            TransactionStatus transactionStatus = null;
+            if (needOpenNewTx) {
+                transactionStatus = TransactionUtil.openTx();
+            }
+            try {
+                lockMapper.insertLock(lock);
+                lockMapper.getLockByIdForUpdate(lock);
+            } catch (Exception ex) {
+                TransactionUtil.rollbackTx(transactionStatus);
+                if (ex.getCause() instanceof SQLException) {
+                    if (ex.getCause().getMessage().contains("Lock wait timeout exceeded")) {
+                        logger.error("锁：" + lock + "等待超时，重新开始获取");
+                        continue;
+                    }
+                }
+                logger.error(ex.getMessage(), ex);
+                break;
+            }
+            try {
+                method.execute();
+            } catch (Exception ex) {
+                logger.error(ex.getMessage(), ex);
+            }
+            lockMapper.deleteLock(lock);
+            if (needOpenNewTx) {
+                TransactionUtil.commitTx(transactionStatus);
+            }
+            break;
+
         }
     }
+
 
 }

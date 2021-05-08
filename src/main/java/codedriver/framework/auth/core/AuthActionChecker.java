a0c46d77b86c1e3be7109dcc685c9ce045393285
陈其炜 <chenqw@techsure.com.cn>
2021-05-08 14:50:15 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/auth/core/AuthActionChecker.java
+++ src/main/java/codedriver/framework/auth/core/AuthActionChecker.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.auth.core;
 
 import codedriver.framework.asynchronization.threadlocal.UserContext;
@@ -49,11 +54,7 @@ public class AuthActionChecker {
                     }
                 }
             }
-            if (userMapper.checkUserAuthorityIsExists(userContext.getUserUuid(true), actionList) > 0) {
-                return true;
-            } else {
-                return false;
-            }
+            return userMapper.checkUserAuthorityIsExists(userContext.getUserUuid(true), actionList) > 0;
         } else {
             return false;
         }

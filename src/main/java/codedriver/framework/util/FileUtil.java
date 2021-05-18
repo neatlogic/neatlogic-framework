548df2c83852f766a2adc172c3dc0719aaf2c7d9
赖文韬 <laiwt@techsure.com.cn>
2021-05-18 10:09:13 +0800

    --task=1004635 --user=赖文韬 导出自定义工具-接口 https://www.tapd.cn/54247054/s/1155192

--- src/main/java/codedriver/framework/util/FileUtil.java
+++ src/main/java/codedriver/framework/util/FileUtil.java
@@ -5,8 +5,13 @@
 
 package codedriver.framework.util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 /**
  * 下载文件工具类
+ *
  * @author linbq
  * @since 2021/4/29 11:17
  **/
@@ -15,30 +20,52 @@ public class FileUtil {
      * chrome、firefox、edge浏览器下载文件时，文件名包含~@#$&+=;这八个英文字符时会变成乱码_%40%23%24%26%2B%3D%3B，
      * 下面是对@#$&+=;这七个字符做特殊处理，
      * 对于~这个字符还是会变成下划线_，暂无法处理
+     *
      * @param fileName 文件名
      * @return 返回处理后的文件名
      */
-    public static String fileNameSpecialCharacterHandling(String fileName){
-        if(fileName.contains("%40")){
-            fileName = fileName.replace("%40","@");
+    public static String fileNameSpecialCharacterHandling(String fileName) {
+        if (fileName.contains("%40")) {
+            fileName = fileName.replace("%40", "@");
         }
-        if(fileName.contains("%23")){
-            fileName = fileName.replace("%23","#");
+        if (fileName.contains("%23")) {
+            fileName = fileName.replace("%23", "#");
         }
-        if(fileName.contains("%24")){
-            fileName = fileName.replace("%24","$");
+        if (fileName.contains("%24")) {
+            fileName = fileName.replace("%24", "$");
         }
-        if(fileName.contains("%26")){
-            fileName = fileName.replace("%26","&");
+        if (fileName.contains("%26")) {
+            fileName = fileName.replace("%26", "&");
         }
-        if(fileName.contains("%2B")){
-            fileName = fileName.replace("%2B","+");
+        if (fileName.contains("%2B")) {
+            fileName = fileName.replace("%2B", "+");
         }
-        if(fileName.contains("%3D")){
-            fileName = fileName.replace("%3D","=");
+        if (fileName.contains("%3D")) {
+            fileName = fileName.replace("%3D", "=");
         }
-        if(fileName.contains("%3B")){
-            fileName = fileName.replace("%3B",";");
+        if (fileName.contains("%3B")) {
+            fileName = fileName.replace("%3B", ";");
+        }
+        return fileName;
+    }
+
+    /**
+     * Firefox浏览器userAgent：Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0
+     * Chrome浏览器userAgent：Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36
+     * Edg浏览器userAgent：Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.46
+     *
+     * @param userAgent
+     * @param fileName
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static String getEncodedFileName(String userAgent, String fileName) throws UnsupportedEncodingException {
+        if (userAgent.indexOf("Gecko") > 0) {
+            //chrome、firefox、edge浏览器下载文件
+            fileName = URLEncoder.encode(fileName, "UTF-8");
+            fileName = fileNameSpecialCharacterHandling(fileName);
+        } else {
+            fileName = new String(fileName.replace(" ", "").getBytes(StandardCharsets.UTF_8), "ISO8859-1");
         }
         return fileName;
     }

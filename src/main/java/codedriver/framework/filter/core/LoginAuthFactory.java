68f9bd79581d9eec1497e018239197d62a32ce89
陈其炜 <chenqw@techsure.com.cn>
2021-07-07 15:43:08 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/filter/core/LoginAuthFactory.java
+++ src/main/java/codedriver/framework/filter/core/LoginAuthFactory.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.filter.core;
 
 import codedriver.framework.applicationlistener.core.ApplicationListenerBase;
@@ -9,32 +14,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RootComponent
-public class LoginAuthFactory extends ApplicationListenerBase{
-	private static final Map<String, ILoginAuthHandler> loginAuthMap = new HashMap<>();
-	
-	public static Map<String, ILoginAuthHandler> getLoginAuthMap() {
-		return loginAuthMap;
-	}
-	
-	public static final ILoginAuthHandler getLoginAuth(String type) {
-		return loginAuthMap.get(type.toUpperCase());
-	}
+public class LoginAuthFactory extends ApplicationListenerBase {
+    private static final Map<String, ILoginAuthHandler> loginAuthMap = new HashMap<>();
 
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent event) {
-		ApplicationContext context = event.getApplicationContext();
-		Map<String, ILoginAuthHandler> myMap = context.getBeansOfType(ILoginAuthHandler.class);
-		for (Map.Entry<String, ILoginAuthHandler> entry : myMap.entrySet()) {
-		    ILoginAuthHandler authAuth = entry.getValue();
-			loginAuthMap.put(authAuth.getType().toUpperCase(), authAuth);
-		}
-		
-	}
 
-	@Override
-	protected void myInit() {
-		// TODO Auto-generated method stub
-		
-	}
+    public static ILoginAuthHandler getLoginAuth(String type) {
+        return loginAuthMap.get(type.toUpperCase());
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext context = event.getApplicationContext();
+        Map<String, ILoginAuthHandler> myMap = context.getBeansOfType(ILoginAuthHandler.class);
+        for (Map.Entry<String, ILoginAuthHandler> entry : myMap.entrySet()) {
+            ILoginAuthHandler authAuth = entry.getValue();
+            loginAuthMap.put(authAuth.getType().toUpperCase(), authAuth);
+        }
+
+    }
+
+    @Override
+    protected void myInit() {
+        // TODO Auto-generated method stub
+
+    }
 
 }

4341c54bc73283b15e4b1a334d942b6cad30ae71
陈其炜 <chenqw@techsure.com.cn>
2021-07-29 19:01:17 +0800

    --bug=1008440 --user=陈其炜 视图-添加视图时，画布为空，直接保存，接口返回异常，但是页面没有异常提示 https://www.tapd.cn/54247054/s/1167540
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/util/JavascriptUtil.java
+++ src/main/java/codedriver/framework/util/JavascriptUtil.java
@@ -1,16 +1,19 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.util;
 
-import java.io.StringWriter;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alibaba.fastjson.JSONObject;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.StringWriter;
 
 public class JavascriptUtil {
 	static Logger logger = LoggerFactory.getLogger(JavascriptUtil.class);

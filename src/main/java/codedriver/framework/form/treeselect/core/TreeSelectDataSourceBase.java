64c4d127aaf46a735f2318ff09e4d9fc472db2af
吕佐康 <lvzk@techsure.com.cn>
2021-08-11 18:04:58 +0800

    --story=1005064 --user=lvzk 苏州银行-表单组件中添加一个选择知识库文档类型的组件 https://www.tapd.cn/54247054/s/1170486

---
+++ src/main/java/codedriver/framework/form/treeselect/core/TreeSelectDataSourceBase.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.form.treeselect.core;
+
+public abstract class TreeSelectDataSourceBase implements ITreeSelectDataSourceHandler {
+
+}

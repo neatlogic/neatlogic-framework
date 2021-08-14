93e00447ba7c8ccf0320c0f78a83afef961bf602
陈其炜 <chenqw@techsure.com.cn>
2021-08-14 13:55:00 +0800

    bugfixed
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/asynchronization/thread/ModuleInitApplicationListener.java
+++ src/main/java/codedriver/framework/asynchronization/thread/ModuleInitApplicationListener.java
@@ -8,10 +8,7 @@ package codedriver.framework.asynchronization.thread;
 import java.util.concurrent.Phaser;
 
 public class ModuleInitApplicationListener {
-    /**
-     * 因为root-context.xml，所以初始化计数器值为1
-     **/
-    private final static Phaser moduleInitPhaser = new Phaser(1);
+    private final static Phaser moduleInitPhaser = new Phaser(0);
 
     public static Phaser getModuleinitphaser() {
         return moduleInitPhaser;

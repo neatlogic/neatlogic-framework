bda148de36ce963d29a2b68c3dd2ac06deac9c43
吕佐康 <lvzk@techsure.com.cn>
2021-08-20 16:50:07 +0800

    --task=1005165 --user=lvzk 后端-控件选项支持预设范围 https://www.tapd.cn/54247054/s/1172569

--- src/main/java/codedriver/module/framework/groupsearch/handler/TeamGroupHandler.java
+++ src/main/java/codedriver/module/framework/groupsearch/handler/TeamGroupHandler.java
@@ -22,153 +22,157 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
 @Service
 public class TeamGroupHandler implements IGroupSearchHandler {
-	@Autowired
-	private TeamMapper teamMapper;
-	@Autowired
-	private RoleMapper roleMapper;
-	
-	@Override
-	public String getName() {
-		return GroupSearch.TEAM.getValue();
-	}
-	
-	@Override
-	public String getHeader() {
-		return getName()+"#";
-	}
+    @Autowired
+    private TeamMapper teamMapper;
+    @Autowired
+    private RoleMapper roleMapper;
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> List<T> search(JSONObject jsonObj) {
-		//总显示选项个数
-		Integer total = jsonObj.getInteger("total");
-		if(total == null) {
-			total = 18;
-		}
-		List<TeamVo> teamList = new ArrayList<TeamVo>();
-		TeamVo teamVo = new TeamVo();	
-		teamVo.setNeedPage(true);
-		teamVo.setPageSize(total);
-		teamVo.setCurrentPage(1);
-		teamVo.setKeyword(jsonObj.getString("keyword"));
-		teamVo.setIsDelete(0);
-		//如果存在rangeList 则需要过滤option
-		List<Object> rangeList = jsonObj.getJSONArray("rangeList");
-		if(CollectionUtils.isNotEmpty(rangeList)){
-			List<String> roleList = new ArrayList<>();
-			Set<String> teamSet = new HashSet<>();
-			Set<String> parentTeamSet = new HashSet<>();
-			rangeList.forEach(r->{
-				if(r.toString().startsWith(GroupSearch.ROLE.getValuePlugin())){
-					roleList.add(GroupSearch.removePrefix(r.toString()));
-				} else if(r.toString().startsWith(GroupSearch.TEAM.getValuePlugin())){
-					teamSet.add(GroupSearch.removePrefix(r.toString()));
-				}
-			});
-			if(CollectionUtils.isNotEmpty(roleList)) {
-				List<RoleTeamVo> roleTeamVoList = roleMapper.getRoleTeamListByRoleUuidList(roleList);
-				roleTeamVoList.forEach(rt->{
-					if(rt.getCheckedChildren() == 1){//如果组穿透
-						parentTeamSet.add(rt.getTeamUuid());
-					}else{
-						teamSet.add(rt.getTeamUuid());
-					}
-				});
-			}
-			teamVo.setTeamUuidList(new ArrayList<>(teamSet));
-			teamVo.setParentTeamUuidList(new ArrayList<>(parentTeamSet));
-			teamVo.setRangeList(rangeList.stream().map(Object::toString).collect(Collectors.toList()));
-		}
-		teamList = teamMapper.searchTeam(teamVo);
-		setFullPathAndParentName(teamList);
-		return (List<T>) teamList;
-	}
+    @Override
+    public String getName() {
+        return GroupSearch.TEAM.getValue();
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> List<T> reload(JSONObject jsonObj) {
-		List<TeamVo> teamList = new ArrayList<TeamVo>();
-		List<String> teamUuidList = new ArrayList<String>();
-		for(Object value :jsonObj.getJSONArray("valueList")) {
-			if(value.toString().startsWith(getHeader())){
-				teamUuidList.add(value.toString().replace(getHeader(), ""));
-			}
-		}
-		if(teamUuidList.size()>0) {
-			teamList = teamMapper.getTeamByUuidList(teamUuidList);
-			setFullPathAndParentName(teamList);
-		}
-		return (List<T>) teamList;
-	}
+    @Override
+    public String getHeader() {
+        return getName() + "#";
+    }
 
-	@Override
-	public <T> JSONObject repack(List<T> teamList) {
-		JSONObject teamObj = new JSONObject();
-		teamObj.put("value", "team");
-		teamObj.put("text", "分组");
-		JSONArray teamArray = new JSONArray();
-		for(T team:teamList) {
-			JSONObject teamTmp = new JSONObject();
-			teamTmp.put("value", getHeader()+((TeamVo) team).getUuid());
-			if(DeviceType.MOBILE.getValue().equals(CommonUtil.getDevice())){
-				teamTmp.put("text", StringUtils.isNotBlank(((TeamVo) team).getParentName())
-						? ((TeamVo) team).getName() + "(" + ((TeamVo) team).getParentName() + ")"
-						: ((TeamVo) team).getName());
-			}else{
-				teamTmp.put("text", ((TeamVo) team).getName());
-			}
-			teamTmp.put("fullPath",((TeamVo) team).getFullPath());
-			teamTmp.put("parentPathList",((TeamVo) team).getParentPathList());
-			teamArray.add(teamTmp);
-		}
-		teamObj.put("sort", getSort());
-		teamObj.put("dataList", teamArray);
-		return teamObj;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> List<T> search(JSONObject jsonObj) {
+        //总显示选项个数
+        Integer total = jsonObj.getInteger("total");
+        if (total == null) {
+            total = 18;
+        }
+        List<TeamVo> teamList = new ArrayList<TeamVo>();
+        TeamVo teamVo = new TeamVo();
+        teamVo.setNeedPage(true);
+        teamVo.setPageSize(total);
+        teamVo.setCurrentPage(1);
+        teamVo.setKeyword(jsonObj.getString("keyword"));
+        teamVo.setIsDelete(0);
+        //如果存在rangeList 则需要过滤option
+        List<Object> rangeList = jsonObj.getJSONArray("rangeList");
+        if (CollectionUtils.isNotEmpty(rangeList)) {
+            List<String> roleList = new ArrayList<>();
+            Set<String> teamSet = new HashSet<>();
+            Set<String> parentTeamSet = new HashSet<>();
+            rangeList.forEach(r -> {
+                if (r.toString().startsWith(GroupSearch.ROLE.getValuePlugin())) {
+                    roleList.add(GroupSearch.removePrefix(r.toString()));
+                } else if (r.toString().startsWith(GroupSearch.TEAM.getValuePlugin())) {
+                    teamSet.add(GroupSearch.removePrefix(r.toString()));
+                }
+            });
+            if (CollectionUtils.isNotEmpty(roleList)) {
+                List<RoleTeamVo> roleTeamVoList = roleMapper.getRoleTeamListByRoleUuidList(roleList);
+                roleTeamVoList.forEach(rt -> {
+                    if (rt.getCheckedChildren() == 1) {//如果组穿透
+                        parentTeamSet.add(rt.getTeamUuid());
+                    } else {
+                        teamSet.add(rt.getTeamUuid());
+                    }
+                });
+            }
+            if (CollectionUtils.isEmpty(teamSet) && CollectionUtils.isEmpty(parentTeamSet)) {//如果rangList不为空 但teamUuidList和parenTeamUuidList都为空则 无需返回分组
+                teamSet.add("team#no_team");
+            }
+            teamVo.setTeamUuidList(new ArrayList<>(teamSet));
+            teamVo.setParentTeamUuidList(new ArrayList<>(parentTeamSet));
+            teamVo.setRangeList(rangeList.stream().map(Object::toString).collect(Collectors.toList()));
+        }
+        teamList = teamMapper.searchTeam(teamVo);
+        setFullPathAndParentName(teamList);
+        return (List<T>) teamList;
+    }
 
-	@Override
-	public int getSort() {
-		return 3;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> List<T> reload(JSONObject jsonObj) {
+        List<TeamVo> teamList = new ArrayList<TeamVo>();
+        List<String> teamUuidList = new ArrayList<String>();
+        for (Object value : jsonObj.getJSONArray("valueList")) {
+            if (value.toString().startsWith(getHeader())) {
+                teamUuidList.add(value.toString().replace(getHeader(), ""));
+            }
+        }
+        if (teamUuidList.size() > 0) {
+            teamList = teamMapper.getTeamByUuidList(teamUuidList);
+            setFullPathAndParentName(teamList);
+        }
+        return (List<T>) teamList;
+    }
 
-	@Override
-	public Boolean isLimit() {
-		return true;
-	}
+    @Override
+    public <T> JSONObject repack(List<T> teamList) {
+        JSONObject teamObj = new JSONObject();
+        teamObj.put("value", "team");
+        teamObj.put("text", "分组");
+        JSONArray teamArray = new JSONArray();
+        for (T team : teamList) {
+            JSONObject teamTmp = new JSONObject();
+            teamTmp.put("value", getHeader() + ((TeamVo) team).getUuid());
+            if (DeviceType.MOBILE.getValue().equals(CommonUtil.getDevice())) {
+                teamTmp.put("text", StringUtils.isNotBlank(((TeamVo) team).getParentName())
+                        ? ((TeamVo) team).getName() + "(" + ((TeamVo) team).getParentName() + ")"
+                        : ((TeamVo) team).getName());
+            } else {
+                teamTmp.put("text", ((TeamVo) team).getName());
+            }
+            teamTmp.put("fullPath", ((TeamVo) team).getFullPath());
+            teamTmp.put("parentPathList", ((TeamVo) team).getParentPathList());
+            teamArray.add(teamTmp);
+        }
+        teamObj.put("sort", getSort());
+        teamObj.put("dataList", teamArray);
+        return teamObj;
+    }
 
-	/**
-	 * @Description: 查询分组的全路径与父分组名称
-	 * @Author: laiwt
-	 * @Date: 2021/2/1 15:51
-	 * @Params: [teamList]
-	 * @Returns: void
-	 **/
-	private void setFullPathAndParentName(List<TeamVo> teamList) {
-		if (CollectionUtils.isNotEmpty(teamList)) {
-			List<TeamVo> nameRepeatedCount = teamMapper.getRepeatTeamNameByNameList(teamList.stream().map(TeamVo::getName).collect(Collectors.toList()));
-			Map<String, Integer> map = nameRepeatedCount.stream().collect(Collectors.toMap(e -> e.getName(), e -> e.getNameRepeatCount()));
-			for (TeamVo team : teamList) {
-				List<TeamVo> ancestorsAndSelf = teamMapper.getAncestorsAndSelfByLftRht(team.getLft(), team.getRht(), null);
-				if (CollectionUtils.isNotEmpty(ancestorsAndSelf)) {
-					List<String> pathNameList = new ArrayList<>();
-					for (TeamVo ancestor : ancestorsAndSelf) {
-						pathNameList.add(ancestor.getName());
-					}
-					if(CollectionUtils.isNotEmpty(pathNameList)){
-						team.setFullPath(String.join("->",pathNameList));
-						team.setParentPathList(pathNameList);
-					}
-				}
-				/** 如果有重名的分组，找出其父分组的名称 **/
-				if(map.get(team.getName()) > 1){
-					TeamVo parent = teamMapper.getTeamByUuid(team.getParentUuid());
-					if(parent != null){
-						team.setParentName(parent.getName());
-					}
-				}
-			}
-		}
-	}
+    @Override
+    public int getSort() {
+        return 3;
+    }
+
+    @Override
+    public Boolean isLimit() {
+        return true;
+    }
+
+    /**
+     * @Description: 查询分组的全路径与父分组名称
+     * @Author: laiwt
+     * @Date: 2021/2/1 15:51
+     * @Params: [teamList]
+     * @Returns: void
+     **/
+    private void setFullPathAndParentName(List<TeamVo> teamList) {
+        if (CollectionUtils.isNotEmpty(teamList)) {
+            List<TeamVo> nameRepeatedCount = teamMapper.getRepeatTeamNameByNameList(teamList.stream().map(TeamVo::getName).collect(Collectors.toList()));
+            Map<String, Integer> map = nameRepeatedCount.stream().collect(Collectors.toMap(e -> e.getName(), e -> e.getNameRepeatCount()));
+            for (TeamVo team : teamList) {
+                List<TeamVo> ancestorsAndSelf = teamMapper.getAncestorsAndSelfByLftRht(team.getLft(), team.getRht(), null);
+                if (CollectionUtils.isNotEmpty(ancestorsAndSelf)) {
+                    List<String> pathNameList = new ArrayList<>();
+                    for (TeamVo ancestor : ancestorsAndSelf) {
+                        pathNameList.add(ancestor.getName());
+                    }
+                    if (CollectionUtils.isNotEmpty(pathNameList)) {
+                        team.setFullPath(String.join("->", pathNameList));
+                        team.setParentPathList(pathNameList);
+                    }
+                }
+                /** 如果有重名的分组，找出其父分组的名称 **/
+                if (map.get(team.getName()) > 1) {
+                    TeamVo parent = teamMapper.getTeamByUuid(team.getParentUuid());
+                    if (parent != null) {
+                        team.setParentName(parent.getName());
+                    }
+                }
+            }
+        }
+    }
 }

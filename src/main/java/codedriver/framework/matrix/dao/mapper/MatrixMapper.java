5ecd46ef60ff13e642eb08e692143e1fda691bc8
林邦泉 <linbq@techsure.com.cn>
2021-09-28 12:03:36 +0800

    --story=1005347 --user=林邦泉 矩阵后端代码优化 https://www.tapd.cn/54247054/s/1180403

--- src/main/java/codedriver/framework/matrix/dao/mapper/MatrixMapper.java
+++ src/main/java/codedriver/framework/matrix/dao/mapper/MatrixMapper.java
@@ -6,23 +6,23 @@ import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.matrix.dto.MatrixVo;
 
 public interface MatrixMapper {
-     int insertMatrix(MatrixVo matrixVo);
+    int insertMatrix(MatrixVo matrixVo);
 
-     int searchMatrixCount(MatrixVo matrixVo);
+    int searchMatrixCount(MatrixVo matrixVo);
 
-     MatrixVo getMatrixByUuid(String uuid);
+    MatrixVo getMatrixByUuid(String uuid);
 
-     List<MatrixVo> getMatrixListByUuidList(List<String> uuidList);
+    List<MatrixVo> getMatrixListByUuidList(List<String> uuidList);
 
-     List<MatrixVo> searchMatrix(MatrixVo matrixVo);
+    List<MatrixVo> searchMatrix(MatrixVo matrixVo);
 
-     int checkMatrixIsExists(String uuid);
+    int checkMatrixIsExists(String uuid);
 
-     int checkMatrixNameIsRepeat(MatrixVo matrixVo);
+    int checkMatrixNameIsRepeat(MatrixVo matrixVo);
 
-     int checkMatrixLabelIsRepeat(MatrixVo matrixVo);
+    int checkMatrixLabelIsRepeat(MatrixVo matrixVo);
 
-     int deleteMatrixByUuid(String uuid);
+    int deleteMatrixByUuid(String uuid);
 
-     int updateMatrixNameAndLcu(MatrixVo matrixVo);
+    int updateMatrixNameAndLcu(MatrixVo matrixVo);
 }

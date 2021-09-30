115d192e3116f05222495274fc1fd389b71b1d98
longrf <longrf@techsure.com.cn>
2021-09-30 17:56:29 +0800

    【Tagent管理功能迁移】 https://www.tapd.cn/54247054/prong/stories/view/1154247054001005084

--- src/main/java/codedriver/framework/common/util/IpUtil.java
+++ src/main/java/codedriver/framework/common/util/IpUtil.java
@@ -1,9 +1,12 @@
 package codedriver.framework.common.util;
 
+import codedriver.framework.dto.runner.GroupNetworkVo;
 import codedriver.framework.exception.util.IpSubnetMaskException;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class IpUtil {
     private IpUtil() {
@@ -59,6 +62,18 @@ public class IpUtil {
         } catch (Exception ex) {
             throw new IpSubnetMaskException(ip + " " + targetIp + " " + subnetMask);
         }
+    }
+
+    public static boolean isIp(String param) {
+        Pattern pattern = Pattern.compile("^((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]" + "|[*])\\.){3}(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]|[*])$");
+        return pattern.matcher(param).matches();
+    }
+
+    public static boolean isMask(int mask) {
+        if (mask > 0 && mask <= 32) {
+            return true;
+        }
+        return false;
     }
 
 }

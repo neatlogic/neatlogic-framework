2cfa3f790b13c959816fee9071270d6febc24ae7
林邦泉 <linbq@techsure.com.cn>
2021-10-18 12:15:41 +0800

    --task=1005396 --user=林邦泉 后端-通知策略模板参数代码优化 https://www.tapd.cn/54247054/s/1183286

---
+++ src/main/java/codedriver/module/framework/notify/handler/param/OperatorParamHandler.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.framework.notify.handler.param;
+
+import codedriver.framework.asynchronization.threadlocal.UserContext;
+import codedriver.framework.notify.constvalue.CommonNotifyParam;
+import codedriver.framework.notify.core.INotifyParamHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author linbq
+ * @since 2021/10/16 15:52
+ **/
+@Component
+public class OperatorParamHandler implements INotifyParamHandler {
+
+    @Override
+    public String getValue() {
+        return CommonNotifyParam.OPERATOR.getValue();
+    }
+
+    @Override
+    public Object getText(Object object) {
+        UserContext userContext = UserContext.get();
+        if (userContext != null) {
+            userContext.getUserName();
+        }
+        return null;
+    }
+}

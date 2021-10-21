11da799695d196147897223312faace37050f127
吕佐康 <lvzk@techsure.com.cn>
2021-10-21 14:46:24 +0800

    update Long vail

--- src/main/java/codedriver/framework/apiparam/validator/LongApiParam.java
+++ src/main/java/codedriver/framework/apiparam/validator/LongApiParam.java
@@ -2,27 +2,39 @@ package codedriver.framework.apiparam.validator;
 
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.param.validate.core.ApiParamValidatorBase;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 
 public class LongApiParam extends ApiParamValidatorBase {
 
-	@Override
-	public String getName() {
-		return "长整数";
-	}
+    @Override
+    public String getName() {
+        return "长整数";
+    }
 
-	@Override
-	public boolean validate(Object param, String rule) {
-		try {
-			Long.valueOf(param.toString());
-			return true;
-		} catch (Exception ex) {
-			return false;
-		}
-	}
+    @Override
+    public boolean validate(Object param, String rule) {
+        try {
+            if (StringUtils.isBlank(param.toString())) {
+                return true;
+            }
+            Long.valueOf(param.toString());
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
 
-	@Override
-	public ApiParamType getType() {
-		return ApiParamType.LONG;
-	}
+    @Override
+    public ApiParamType getType() {
+        return ApiParamType.LONG;
+    }
 
+
+    @Override
+    public void correctValue(JSONObject paramObj, String name, Object param) {
+        if (StringUtils.isBlank(param.toString())) {
+            paramObj.remove(name);
+        }
+    }
 }

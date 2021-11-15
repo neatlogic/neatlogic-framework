3d258f0b3722dc0006adb740104b8d824f64cd74
吕佐康 <lvzk@techsure.com.cn>
2021-11-15 23:07:02 +0800

    tagent 兼容 tagentId Long 为 空字符串

--- src/main/java/codedriver/framework/param/validate/core/ApiParamValidatorBase.java
+++ src/main/java/codedriver/framework/param/validate/core/ApiParamValidatorBase.java
@@ -1,7 +1,6 @@
 package codedriver.framework.param.validate.core;
 
 import codedriver.framework.common.constvalue.ApiParamType;
-import com.alibaba.fastjson.JSONObject;
 
 public abstract class ApiParamValidatorBase {
     public abstract String getName();
@@ -10,14 +9,4 @@ public abstract class ApiParamValidatorBase {
 
     public abstract ApiParamType getType();
 
-    /**
-     * 纠正数据
-     *
-     * @param paramObj 入参json
-     * @param name     key
-     * @param param    值
-     */
-    public void correctValue(JSONObject paramObj, String name, Object param) {
-
-    }
 }

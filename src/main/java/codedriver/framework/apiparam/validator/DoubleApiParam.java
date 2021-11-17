1b5bffad1da1835eb3889786d72f159d0da20c0f
陈其炜 <chenqw@techsure.com.cn>
2021-11-17 17:49:34 +0800

    分类标识管理
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/apiparam/validator/DoubleApiParam.java
+++ src/main/java/codedriver/framework/apiparam/validator/DoubleApiParam.java
@@ -1,28 +1,37 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.apiparam.validator;
 
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.param.validate.core.ApiParamValidatorBase;
+import org.apache.commons.lang3.StringUtils;
 
 public class DoubleApiParam extends ApiParamValidatorBase {
 
-	@Override
-	public String getName() {
-		return "双精度浮点数";
-	}
+    @Override
+    public String getName() {
+        return "双精度浮点数";
+    }
 
-	@Override
-	public boolean validate(Object param, String rule) {
-		try {
-			Double.valueOf(param.toString());
-			return true;
-		} catch (Exception ex) {
-			return false;
-		}
-	}
+    @Override
+    public boolean validate(Object param, String rule) {
+        if (param != null && StringUtils.isNotBlank(param.toString())) {
+            try {
+                Double.valueOf(param.toString());
+                return true;
+            } catch (Exception ex) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public ApiParamType getType() {
-		return ApiParamType.DOUBLE;
-	}
+    @Override
+    public ApiParamType getType() {
+        return ApiParamType.DOUBLE;
+    }
 
 }

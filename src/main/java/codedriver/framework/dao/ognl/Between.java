e7b110f0bab941eb2e54812543f4be78afe9479c
陈其炜 <chenqw@techsure.com.cn>
2021-11-27 16:55:42 +0800

    模型属性支持数字
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/dao/ognl/Between.java
+++ src/main/java/codedriver/framework/dao/ognl/Between.java
@@ -5,15 +5,30 @@
 
 package codedriver.framework.dao.ognl;
 
+/**
+ * 处理配置项查询属性between表达式，一定会返回一个长度为2的数组，代表前后两个值，如果处理不了则返回最小值和最大值，保证sql不出错
+ */
 public class Between {
-    public static String[] parse(String str) {
-        String[] s = new String[]{"", ""};
+    public static Double[] parse(String str) {
+        Double[] s = new Double[]{null, null};
         if (str.contains("~")) {
             String[] ss = str.split("~");
-            s[0] = ss[0];
-            if (ss.length > 1) {
-                s[1] = str.split("~")[1];
+            try {
+                s[0] = Double.parseDouble(ss[0]);
+            } catch (Exception ignored) {
+
             }
+            if (ss.length > 1) {
+                try {
+                    s[1] = Double.parseDouble(ss[1]);
+                } catch (Exception ignored) {
+
+                }
+            }
+        }
+        if (s[0] == null && s[1] == null) {
+            s[0] = -Double.MAX_VALUE;
+            s[1] = Double.MAX_VALUE;
         }
         return s;
     }

c4143118684d81a7ca40626ab12218434152a2fc
赖文韬 <laiwt@techsure.com.cn>
2021-11-30 11:09:19 +0800

    --story=1005540 --user=赖文韬 任务授权授权用户搜索优化（根据分组来过滤和选择用户） https://www.tapd.cn/54247054/s/1191306

--- src/main/java/codedriver/module/framework/groupsearch/handler/UserGroupHandler.java
+++ src/main/java/codedriver/module/framework/groupsearch/handler/UserGroupHandler.java
@@ -22,91 +22,93 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserGroupHandler implements IGroupSearchHandler {
-	@Resource
-	private UserMapper userMapper;
-	@Resource
-	UserService userService;
-	
-	@Override
-	public String getName() {
-		return GroupSearch.USER.getValue();
-	}
+    @Resource
+    private UserMapper userMapper;
+    @Resource
+    UserService userService;
 
-	@Override
-	public String getHeader() {
-		return GroupSearch.USER.getValuePlugin();
-	}
+    @Override
+    public String getName() {
+        return GroupSearch.USER.getValue();
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> List<T> search(JSONObject jsonObj) {
-		//总显示选项个数
-		Integer total = jsonObj.getInteger("total");
-		if(total == null) {
-			total = 18;
-		}
-		List<UserVo> userList = new ArrayList<UserVo>();
-		UserVo userVo = new UserVo();
-		userVo.setNeedPage(true);
-		userVo.setPageSize(total);
-		userVo.setCurrentPage(1);
-		userVo.setIsActive(1);
-		userVo.setKeyword(jsonObj.getString("keyword"));
-		//如果存在rangeList 则需要过滤option
-		JSONArray rangeList = jsonObj.getJSONArray("rangeList");
-		if(CollectionUtils.isNotEmpty(rangeList)){
-			userService.getUserByRangeList(userVo,rangeList.stream().map(Object::toString).collect(Collectors.toList()));
-		}
-		userList = userMapper.searchUser(userVo);
-		return (List<T>) userList;
-	}
+    @Override
+    public String getHeader() {
+        return GroupSearch.USER.getValuePlugin();
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> List<T> reload(JSONObject jsonObj) {
-		List<UserVo> userList = new ArrayList<UserVo>();
-		List<String> userUuidList = new ArrayList<String>();
-		List<String> valueList = JSONObject.parseArray(jsonObj.getJSONArray("valueList").toJSONString(), String.class);
-		for (Object value : valueList) {
-			if (value.toString().startsWith(getHeader())) {
-				userUuidList.add(value.toString().replace(getHeader(), ""));
-			}
-		}
-		if (userUuidList.size() > 0) {
-			userList = userMapper.getUserByUserUuidList(userUuidList);
-		}
-		return (List<T>) userList;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> List<T> search(JSONObject jsonObj) {
+        //总显示选项个数
+        Integer total = jsonObj.getInteger("total");
+        if (total == null) {
+            total = 18;
+        }
+        List<UserVo> userList = new ArrayList<UserVo>();
+        UserVo userVo = new UserVo();
+        userVo.setNeedPage(true);
+        userVo.setPageSize(total);
+        userVo.setCurrentPage(1);
+        userVo.setIsActive(1);
+        userVo.setKeyword(jsonObj.getString("keyword"));
+        //如果存在rangeList 则需要过滤option
+        JSONArray rangeList = jsonObj.getJSONArray("rangeList");
+        if (CollectionUtils.isNotEmpty(rangeList)) {
+            userService.getUserByRangeList(userVo, rangeList.stream().map(Object::toString).collect(Collectors.toList()));
+        }
+        userList = userMapper.searchUser(userVo);
+        return (List<T>) userList;
+    }
 
-	@Override
-	public <T> JSONObject repack(List<T> userList) {
-		JSONObject userObj = new JSONObject();
-		userObj.put("value", "user");
-		userObj.put("text", "用户");
-		JSONArray userArray = new JSONArray();
-		
-		for (T user : userList) {
-			JSONObject userTmp = new JSONObject();
-			userTmp.put("value", getHeader() + ((UserVo) user).getUuid());
-			userTmp.put("text", ((UserVo) user).getUserName() + "(" + ((UserVo) user).getUserId() + ")");
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> List<T> reload(JSONObject jsonObj) {
+        List<UserVo> userList = new ArrayList<UserVo>();
+        List<String> userUuidList = new ArrayList<String>();
+        List<String> valueList = JSONObject.parseArray(jsonObj.getJSONArray("valueList").toJSONString(), String.class);
+        for (Object value : valueList) {
+            if (value.toString().startsWith(getHeader())) {
+                userUuidList.add(value.toString().replace(getHeader(), ""));
+            }
+        }
+        if (userUuidList.size() > 0) {
+            userList = userMapper.getUserByUserUuidList(userUuidList);
+        }
+        return (List<T>) userList;
+    }
+
+    @Override
+    public <T> JSONObject repack(List<T> userList) {
+        JSONObject userObj = new JSONObject();
+        userObj.put("value", "user");
+        userObj.put("text", "用户");
+        JSONArray userArray = new JSONArray();
+
+        for (T user : userList) {
+            JSONObject userTmp = new JSONObject();
+            UserVo userVo = (UserVo) user;
+            userTmp.put("value", getHeader() + userVo.getUuid());
+            userTmp.put("text", userVo.getUserName() + "(" + userVo.getUserId() + ")");
 //			userTmp.put("userInfo", ((UserVo) user).getUserInfo());
-			userTmp.put("pinyin",((UserVo) user).getPinyin());
-			userTmp.put("avatar", ((UserVo) user).getAvatar());
-			userTmp.put("vipLevel", ((UserVo) user).getVipLevel());
-			userArray.add(userTmp);
-		}
-		userObj.put("sort", getSort());
-		userObj.put("dataList", userArray);
-		return userObj;
-	}
+            userTmp.put("pinyin", userVo.getPinyin());
+            userTmp.put("avatar", userVo.getAvatar());
+            userTmp.put("vipLevel", userVo.getVipLevel());
+            userTmp.put("team", String.join("/", userVo.getTeamNameList()));
+            userArray.add(userTmp);
+        }
+        userObj.put("sort", getSort());
+        userObj.put("dataList", userArray);
+        return userObj;
+    }
 
-	@Override
-	public int getSort() {
-		return 2;
-	}
+    @Override
+    public int getSort() {
+        return 2;
+    }
 
-	@Override
-	public Boolean isLimit() {
-		return true;
-	}
+    @Override
+    public Boolean isLimit() {
+        return true;
+    }
 }

b2e5a3f471fbec48496a883ab9a74abe11d3ec17
陈其炜 <chenqw@techsure.com.cn>
2021-11-30 19:26:42 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/store/mysql/CodeDriverRoutingDataSource.java
+++ src/main/java/codedriver/framework/store/mysql/CodeDriverRoutingDataSource.java
@@ -14,11 +14,10 @@ public class CodeDriverRoutingDataSource extends AbstractRoutingDataSource {
     @Override
     protected Object determineCurrentLookupKey() {
         if (TenantContext.get() != null && StringUtils.isNotBlank(TenantContext.get().getTenantUuid())) {
-            String key = TenantContext.get().getTenantUuid();
-            if (TenantContext.get().isData()) {
+            /*if (TenantContext.get().isData()) {
                 key += "_DATA";
-            }
-            return key;
+            }*/
+            return TenantContext.get().getTenantUuid();
         } else {
             return null;
         }

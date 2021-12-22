aa48738516a6fd4eba401e303f59d95828c319ad
林邦泉 <linbq@techsure.com.cn>
2021-12-22 18:20:09 +0800

    --bug=1009032 --user=林邦泉 流程中配置了步骤激活发邮件动作，但是当当前节点配置的是一个角色时，激活时并没有给角色中的所有人发送邮件 https://www.tapd.cn/54247054/s/1196666

--- src/main/java/codedriver/framework/service/UserServiceImpl.java
+++ src/main/java/codedriver/framework/service/UserServiceImpl.java
@@ -113,10 +113,29 @@ public class UserServiceImpl implements UserService {
             } else {
                 teamUuidSet.add(teamUuid);
             }
+            if (teamUuidSet.size() >= 100) {
+                userUuidList = userMapper.getUserUuidListByTeamUuidList(new ArrayList<>(teamUuidSet));
+                userUuidSet.addAll(userUuidList);
+                teamUuidSet.clear();
+            }
         }
-        userUuidList = userMapper.getUserUuidListByTeamUuidList(new ArrayList<>(teamUuidSet));
-        userUuidSet.addAll(userUuidList);
-        List<UserVo> userList = userMapper.getUserListByUuidList(new ArrayList<>(userUuidSet));
-        return userList;
+        if (teamUuidSet.size() > 0) {
+            userUuidList = userMapper.getUserUuidListByTeamUuidList(new ArrayList<>(teamUuidSet));
+            userUuidSet.addAll(userUuidList);
+        }
+        List<UserVo> allUserList = new ArrayList<>();
+        if (userUuidSet.size() > 0) {
+            List<String> allUserUuidList = new ArrayList<>(userUuidSet);
+            for (int fromIndex = 0; fromIndex < allUserUuidList.size(); fromIndex += 100) {
+                int toIndex = fromIndex + 100;
+                if (toIndex > allUserUuidList.size()) {
+                    toIndex = allUserUuidList.size();
+                }
+                userUuidList = allUserUuidList.subList(fromIndex, toIndex);
+                List<UserVo> userList = userMapper.getUserListByUuidList(userUuidList);
+                allUserList.addAll(userList);
+            }
+        }
+        return allUserList;
     }
 }

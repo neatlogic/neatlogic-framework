ff5e19d0b95a05e0a93c2e0a8a9eba41e35abfb0
林邦泉 <linbq@techsure.com.cn>
2021-12-31 10:26:01 +0800

    --task=1005759 --user=林邦泉 后端-新增矩阵引用cmdb模型处理器CmdbCiMatrixDependencyHandler https://www.tapd.cn/54247054/s/1199296

--- src/main/java/codedriver/module/framework/dependency/handler/CmdbCiMatrixDependencyHandler.java
+++ src/main/java/codedriver/module/framework/dependency/handler/CmdbCiMatrixDependencyHandler.java
@@ -80,7 +80,7 @@ public class CmdbCiMatrixDependencyHandler extends DependencyHandlerBase {
             if (matrixVo != null) {
                 ValueTextVo valueTextVo = new ValueTextVo();
                 valueTextVo.setValue(matrixVo.getUuid());
-                valueTextVo.setText(String.format("<a href=\"/%s/framework.html#/matrix-external-edit?uuid=%s&name=%s&type=%s\" target=\"_blank\">矩阵-%s</a>", TenantContext.get().getTenantUuid(), matrixVo.getUuid(), matrixVo.getName(), matrixVo.getType(), matrixVo.getName()));
+                valueTextVo.setText(String.format("<a href=\"/%s/framework.html#/matrix-view-edit?uuid=%s&name=%s&type=%s\" target=\"_blank\">矩阵-%s</a>", TenantContext.get().getTenantUuid(), matrixVo.getUuid(), matrixVo.getName(), matrixVo.getType(), matrixVo.getName()));
                 return valueTextVo;
             }
         }

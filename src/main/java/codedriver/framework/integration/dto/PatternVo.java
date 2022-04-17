bf451be7b9b0893d34d1ef04d83307cf3223f135
陈其炜 <chenqw@techsure.com.cn>
2022-04-17 18:12:43 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/integration/dto/PatternVo.java
+++ src/main/java/codedriver/framework/integration/dto/PatternVo.java
@@ -1,116 +1,126 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.integration.dto;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-
-import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.restful.annotation.EntityField;
-
 public class PatternVo implements Serializable {
 
-	private static final long serialVersionUID = 8687343787716050495L;
+    private static final long serialVersionUID = 8687343787716050495L;
 
-	public PatternVo() {
+    public PatternVo() {
 
-	}
+    }
 
-	public PatternVo(String _name, String _mode, ApiParamType _type, Integer _isRequired, String _description) {
-		name = _name;
-		type = _type.getValue();
-		mode = _mode;
-		isRequired = _isRequired;
-		description = _description;
-	}
+    public PatternVo(String _name, String _mode, ApiParamType _type, Integer _isRequired, String _description) {
+        name = _name;
+        type = _type.getValue();
+        mode = _mode;
+        isRequired = _isRequired;
+        description = _description;
+    }
 
-	public PatternVo(String _name, String _mode, ApiParamType _type) {
-		name = _name;
-		mode = _mode;
-		type = _type.getValue();
-		isRequired = 0;
-	}
+    public PatternVo(String _name, String _mode, ApiParamType _type) {
+        name = _name;
+        mode = _mode;
+        type = _type.getValue();
+        isRequired = 0;
+    }
 
-	@EntityField(name = "参数名", type = ApiParamType.STRING)
-	private String name;
-	@EntityField(name = "数据类型", type = ApiParamType.STRING)
-	private String type;
-	@EntityField(name = "参数类型", type = ApiParamType.STRING)
-	private String mode;
-	@EntityField(name = "参数类型名称", type = ApiParamType.STRING)
-	private String typeName;
-	@EntityField(name = "子参数", type = ApiParamType.JSONOBJECT)
-	private List<PatternVo> children;
-	@EntityField(name = "是否必填", type = ApiParamType.INTEGER)
-	private Integer isRequired;
-	@EntityField(name = "描述", type = ApiParamType.STRING)
-	private String description;
+    @EntityField(name = "参数名", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "唯一键，名称和类型的组合", type = ApiParamType.STRING)
+    private String key;
+    @EntityField(name = "数据类型", type = ApiParamType.STRING)
+    private String type;
+    @EntityField(name = "参数类型", type = ApiParamType.STRING)
+    private String mode;
+    @EntityField(name = "参数类型名称", type = ApiParamType.STRING)
+    private String typeName;
+    @EntityField(name = "子参数", type = ApiParamType.JSONOBJECT)
+    private List<PatternVo> children;
+    @EntityField(name = "是否必填", type = ApiParamType.INTEGER)
+    private Integer isRequired;
+    @EntityField(name = "描述", type = ApiParamType.STRING)
+    private String description;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public String getTypeName() {
-		if (StringUtils.isBlank(typeName) && StringUtils.isNotBlank(type)) {
-			typeName = ApiParamType.getText(type);
-		}
-		return typeName;
-	}
+    public String getTypeName() {
+        if (StringUtils.isBlank(typeName) && StringUtils.isNotBlank(type)) {
+            typeName = ApiParamType.getText(type);
+        }
+        return typeName;
+    }
 
-	public void setTypeName(String typeName) {
-		this.typeName = typeName;
-	}
+    public String getKey() {
+        return this.name + "_" + this.mode;
+    }
 
-	public List<PatternVo> getChildren() {
-		return children;
-	}
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
 
-	public void setChildren(List<PatternVo> childPatternList) {
-		this.children = childPatternList;
-	}
+    public List<PatternVo> getChildren() {
+        return children;
+    }
 
-	public void addChild(PatternVo patternVo) {
-		if (this.children == null) {
-			this.children = new ArrayList<>();
-		}
-		this.children.add(patternVo);
-	}
+    public void setChildren(List<PatternVo> childPatternList) {
+        this.children = childPatternList;
+    }
 
-	public Integer getIsRequired() {
-		return isRequired;
-	}
+    public void addChild(PatternVo patternVo) {
+        if (this.children == null) {
+            this.children = new ArrayList<>();
+        }
+        this.children.add(patternVo);
+    }
 
-	public void setIsRequired(Integer isRequired) {
-		this.isRequired = isRequired;
-	}
+    public Integer getIsRequired() {
+        return isRequired;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setIsRequired(Integer isRequired) {
+        this.isRequired = isRequired;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getMode() {
-		return mode;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setMode(String mode) {
-		this.mode = mode;
-	}
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
 
 }

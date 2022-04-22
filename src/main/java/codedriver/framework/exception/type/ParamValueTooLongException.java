18a25a99f4f5111ae58e9a59b9fc3f33242432ed
赖文韬 <laiwt@techsure.com.cn>
2022-04-22 12:08:03 +0800

    --story=1006145 --user=赖文韬 自定义工具增加自由参数、脚本编码 https://www.tapd.cn/54247054/s/1222613

--- src/main/java/codedriver/framework/exception/type/ParamValueTooLongException.java
+++ src/main/java/codedriver/framework/exception/type/ParamValueTooLongException.java
@@ -3,12 +3,12 @@ package codedriver.framework.exception.type;
 import codedriver.framework.exception.core.ApiRuntimeException;
 
 public class ParamValueTooLongException extends ApiRuntimeException {
-	/** 
-	* @Fields serialVersionUID : TODO 
-	*/
-	private static final long serialVersionUID = 5528197166107887380L;
+    /**
+     * @Fields serialVersionUID : TODO
+     */
+    private static final long serialVersionUID = 5528197166107887380L;
 
-	public ParamValueTooLongException(String paramName, int valueLength, int maxLength) {
-		super("参数：“" + paramName + "”允许最大长度是" + maxLength + "个字符");
-	}
+    public ParamValueTooLongException(String paramName, int valueLength, int maxLength) {
+        super("参数：“" + paramName + "”允许最大长度是" + maxLength + "个字符，当前长度：" + valueLength);
+    }
 }

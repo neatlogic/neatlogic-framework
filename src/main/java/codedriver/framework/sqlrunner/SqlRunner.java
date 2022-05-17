f53caff9aa300443eb60e46d4df843ece12136a7
linbq@techsure.com.cn <Besiter789>
2022-05-17 16:37:49 +0800

    --bug=1009642 --user=林邦泉 工单上报前和上报后表单显示信息不一致（表单联动） https://www.tapd.cn/54247054/s/1228615

--- src/main/java/codedriver/framework/sqlrunner/SqlRunner.java
+++ src/main/java/codedriver/framework/sqlrunner/SqlRunner.java
@@ -15,6 +15,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -27,47 +28,110 @@ import java.util.stream.Collectors;
 
 @Component
 public class SqlRunner {
-    private final static String DOCTYPE = "<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd\">";
+    private final String DOCTYPE = "<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd\">";
 
-    private static DataSource dataSource;
+//    @Resource
+    private DataSource dataSource;
 
-    private String namespace;
+//    private String namespace;
 
     private Configuration configuration;
     private SqlSessionFactory sqlSessionFactory;
 
-    public SqlRunner() {
-
-    }
-
-    public SqlRunner(String mapperXml) {
-        this(mapperXml, null, null);
-    }
-
-    public SqlRunner(String mapperXml, String namespace) {
-        this(mapperXml, namespace, null);
-    }
-
-    public SqlRunner(String mapperXml, DataSource dataSource) {
-        this(mapperXml, null, dataSource);
-    }
-
-    public SqlRunner(String mapperXml, String namespace, DataSource dataSource) {
-        if (namespace != null) {
-            this.namespace = namespace;
-        }
-        if (dataSource != null) {
-            this.dataSource = dataSource;
-        }
-
-        Configuration configuration = new Configuration();
+    @Autowired
+    public SqlRunner(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.configuration = new Configuration();
         configuration.addInterceptor(new SqlCostInterceptor());
+        System.out.println("CacheEnabled:" + configuration.isCacheEnabled());
+        System.out.println("dataSource:" + dataSource);
         Environment environment = new Environment("", new SpringManagedTransactionFactory(), this.dataSource);
         configuration.setEnvironment(environment);
+        this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+    }
+
+//    public SqlRunner(String mapperXml) {
+//        this(mapperXml, null, null);
+//    }
+
+//    public SqlRunner(String mapperXml, String namespace) {
+//        this(mapperXml, namespace, null);
+//    }
+
+//    public SqlRunner(String mapperXml, DataSource dataSource) {
+//        this(mapperXml, null, dataSource);
+//    }
+
+//    public SqlRunner(String mapperXml, String namespace, DataSource dataSource) {
+//        this(dataSource);
+////        if (namespace != null) {
+////            this.namespace = namespace;
+////        }
+//        if (dataSource != null) {
+//            this.dataSource = dataSource;
+//        }
+//        addMapperXml(mapperXml, namespace);
+////        Configuration configuration = new Configuration();
+////        configuration.addInterceptor(new SqlCostInterceptor());
+////        System.out.println("CacheEnabled:" + configuration.isCacheEnabled());
+////        Environment environment = new Environment("", new SpringManagedTransactionFactory(), this.dataSource);
+////        configuration.setEnvironment(environment);
+////        StringBuilder stringBuilder = new StringBuilder();
+////        stringBuilder.append(DOCTYPE);
+////        if (StringUtils.isNotBlank(this.namespace)) {
+////            stringBuilder.append("<mapper namespace=\"" + this.namespace + "\">");
+////        } else {
+////            stringBuilder.append("<mapper namespace=\"codedriver\">");
+////        }
+////
+////        stringBuilder.append(mapperXml.substring("<mapper>".length()));
+////        ByteArrayInputStream inputStream = null;
+////        Throwable var8 = null;
+////        try {
+////            inputStream = new ByteArrayInputStream(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+////            XMLMapperBuilder mapperParser = new XMLMapperBuilder(inputStream, configuration, this.namespace, configuration.getSqlFragments());
+////            mapperParser.parse();
+////        } catch (Throwable var32) {
+////            var8 = var32;
+////            throw var32;
+////        } finally {
+////            if (inputStream != null) {
+////                if (var8 != null) {
+////                    try {
+////                        inputStream.close();
+////                    } catch (Throwable var30) {
+////                        var8.addSuppressed(var30);
+////                    }
+////                } else {
+////                    try {
+////                        inputStream.close();
+////                    } catch (IOException e) {
+////                        e.printStackTrace();
+////                    }
+////                }
+////            }
+////        }
+////        this.configuration = configuration;
+////        this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+//    }
+
+//    public void setDataSource(DataSource _dataSource) {
+//        dataSource = _dataSource;
+//    }
+
+//    public void addMapperXml(String mapperXml) {
+//        addMapperXml(mapperXml, null);
+//    }
+    public void addMapperXml(String mapperXml, String namespace) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(DOCTYPE);
-        if (StringUtils.isNotBlank(this.namespace)) {
-            stringBuilder.append("<mapper namespace=\"" + this.namespace + "\">");
+//        if (StringUtils.isNotBlank(this.namespace)) {
+//            stringBuilder.append("<mapper namespace=\"" + this.namespace + "\">");
+//        } else {
+//            stringBuilder.append("<mapper namespace=\"codedriver\">");
+//        }
+        if (StringUtils.isNotBlank(namespace)) {
+            stringBuilder.append("<mapper namespace=\"" + namespace + "\">");
         } else {
             stringBuilder.append("<mapper namespace=\"codedriver\">");
         }
@@ -77,7 +141,7 @@ public class SqlRunner {
         Throwable var8 = null;
         try {
             inputStream = new ByteArrayInputStream(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
-            XMLMapperBuilder mapperParser = new XMLMapperBuilder(inputStream, configuration, this.namespace, configuration.getSqlFragments());
+            XMLMapperBuilder mapperParser = new XMLMapperBuilder(inputStream, configuration, namespace, configuration.getSqlFragments());
             mapperParser.parse();
         } catch (Throwable var32) {
             var8 = var32;
@@ -99,26 +163,21 @@ public class SqlRunner {
                 }
             }
         }
-        this.configuration = configuration;
-        this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
     }
-
-    @Resource
-    public void setDataSource(DataSource _dataSource) {
-        dataSource = _dataSource;
-    }
-
     /**
      * 执行mapper中所有select语句
      *
      * @param paramMap
      * @return
      */
-    public Map<String, List> runAllSql(Map<String, Object> paramMap) {
+    public Map<String, List> runAllSql(String namespace, Map<String, Object> paramMap) {
         Map<String, List> resultMap = new HashMap<>();
         List<String> selectIdList = new ArrayList<>();
         Collection<MappedStatement> mappedStatementList = configuration.getMappedStatements();
         for (MappedStatement mappedStatement : mappedStatementList) {
+            if (!Objects.equals(mappedStatement.getResource(), namespace)) {
+                continue;
+            }
             if (mappedStatement.getSqlCommandType() == SqlCommandType.SELECT) {
                 String selectId = mappedStatement.getId();
                 if (!selectIdList.contains(selectId)) {
@@ -130,7 +189,7 @@ public class SqlRunner {
         try {
             for (String selectId : selectIdList) {
                 List reportTypeList = sqlSession.selectList(selectId, paramMap);
-                resultMap.put(selectId.substring(this.namespace.length() + 1), reportTypeList);
+                resultMap.put(selectId.substring(namespace.length() + 1), reportTypeList);
             }
         } finally {
             sqlSession.close();
@@ -141,12 +200,12 @@ public class SqlRunner {
     /**
      * 执行mapper中特定某个select语句
      *
-     * @param id
+     * @param sqlInfo
      * @param paramMap
      * @return
      */
-    public <E> List<E>  runSqlById(String id, Map<String, Object> paramMap) {
-        MappedStatement mappedStatement = configuration.getMappedStatement(id);
+    public <E> List<E>  runSqlById(SqlInfo sqlInfo, Map<String, Object> paramMap) {
+        MappedStatement mappedStatement = configuration.getMappedStatement(sqlInfo.getId());
         if (mappedStatement == null) {
             return new ArrayList<>();
         }
@@ -155,7 +214,7 @@ public class SqlRunner {
         }
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try {
-            return sqlSession.selectList(this.namespace + "." + id, paramMap);
+            return sqlSession.selectList(sqlInfo.getNamespace() + "." + sqlInfo.getId(), paramMap);
         } finally {
             sqlSession.close();
         }
@@ -180,8 +239,9 @@ public class SqlRunner {
                 continue;
             }
             idList.add(id);
+            String namespace = mappedStatement.getResource();
             SqlInfo sqlInfo = new SqlInfo();
-            sqlInfo.setId(id.substring(this.namespace.length() + 1));
+            sqlInfo.setId(id.substring(namespace.length() + 1));
             BoundSql boundSql = mappedStatement.getBoundSql(paramMap);
             String sql = boundSql.getSql();
             sqlInfo.setSql(sql);
@@ -197,7 +257,7 @@ public class SqlRunner {
             }
             Integer timeout = mappedStatement.getTimeout();
             sqlInfo.setTimeout(timeout);
-            sqlInfo.setNamespace(this.namespace);
+            sqlInfo.setNamespace(namespace);
             sqlInfoList.add(sqlInfo);
         }
         return sqlInfoList;

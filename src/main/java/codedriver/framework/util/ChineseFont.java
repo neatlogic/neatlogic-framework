135a756e51bf4ddd41c9518c7e3b160bc15505ec
陈其炜 <chenqw@techsure.com.cn>
2022-05-23 15:38:16 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/util/ChineseFont.java
+++ src/main/java/codedriver/framework/util/ChineseFont.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.util;
 
 import java.net.URL;
@@ -5,29 +10,28 @@ import java.net.URL;
 public enum ChineseFont {
 
 	// 仿宋体
-	SIMFANG("SimFang", "simfang.ttf"),
-	// 黑体
-	SIMHEI("SimHei", "simhei.ttf"),
-	// 楷体
-	SIMKAI("SimKai", "simkai.ttf"),
-	// 宋体&新宋体
-	SIMSUM("SimSun", "simsun.ttc"),
-	// 华文仿宋
-	STFANGSO("StFangSo", "stfangso.ttf");
+    SIMFANG("SimFang", "simfang.ttf"),
+    // 黑体
+    SIMHEI("SimHei", "simhei.ttf"),
+    // 楷体
+    SIMKAI("SimKai", "simkai.ttf"),
+    // 宋体&新宋体
+    SIMSUM("SimSun", "simsun.ttc"),
+    // 华文仿宋
+    STFANGSO("StFangSo", "stfangso.ttf");
 
-	private String fontFileName;
-	private String fontName;
+    private final String fontFileName;
+    private final String fontName;
 
-	public URL getFontUrl() {
-		URL url = this.getClass().getResource("/codedriver/resources/fonts/" + this.fontFileName);
-		return url;
-	}
+    public URL getFontUrl() {
+        return this.getClass().getResource("/codedriver/resources/fonts/" + this.fontFileName);
+    }
 
-	public String getFontName() {
-		return fontName;
-	}
+    public String getFontName() {
+        return fontName;
+    }
 
-	private ChineseFont(String v, String fontFileName) {
+    private ChineseFont(String v, String fontFileName) {
 		this.fontName = v;
 		this.fontFileName = fontFileName;
 	}

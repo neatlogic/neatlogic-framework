97d01c90499969cb4298f4dcbfd9860440e361cf
lvzk <lvzk@techsure.com.cn>
2022-06-08 16:05:00 +0800

    支持. -

--- src/main/java/codedriver/framework/util/RegexUtils.java
+++ src/main/java/codedriver/framework/util/RegexUtils.java
@@ -9,10 +9,10 @@ import java.util.regex.Pattern;
 
 public final class RegexUtils {
     private static Logger logger = LoggerFactory.getLogger(RegexUtils.class);
-    public static final String NAME = "^[A-Za-z_\\d\\u4e00-\\u9fa5]+$";
-    public static final String NAME_WITH_SLASH = "^[A-Za-z_\\d\\u4e00-\\u9fa5/]+$";
-    public static final String ENGLISH_NUMBER_NAME = "^[a-zA-Z0-9_\\.]+$";
-    public static final String ENGLISH_NAME = "^[A-Za-z]+$";
+    public static final String NAME = "^[A-Za-z_\\.\\-\\d\\u4e00-\\u9fa5]+$";
+    public static final String NAME_WITH_SLASH = "^[A-Za-z_\\.\\-\\d\\u4e00-\\u9fa5/]+$";
+    public static final String ENGLISH_NUMBER_NAME = "^[a-zA-Z0-9_\\-\\.]+$";
+    public static final String ENGLISH_NAME = "^[A-Za-z\\.\\-]+$";
     public static final String API_TOKEN = "^[A-Za-z_\\{\\}\\d/]+$";
     public static final String DATE_TIME = "[1-9]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\\s+(20|21|22|23|[0-1]\\d):[0-5]\\d:[0-5]\\d";
     public static final String PASSWORD = "^(?!.*[\\u4E00-\\u9FA5\\s])(?!^[a-zA-Z]+$)(?!^[\\d]+$)(?!^[^a-zA-Z\\d]+$)^.{8,20}$";

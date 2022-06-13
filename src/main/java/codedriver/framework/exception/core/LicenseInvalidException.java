ab87085040a7a7fcbe29df83ea898a925f7b7f43
lvzk <lvzk@techsure.com.cn>
2022-06-13 16:41:02 +0800

    update

--- src/main/java/codedriver/framework/exception/core/LicenseInvalidException.java
+++ src/main/java/codedriver/framework/exception/core/LicenseInvalidException.java
@@ -8,4 +8,8 @@ public class LicenseInvalidException extends ApiRuntimeException {
         super("当前租户没有license授权或license无效");
     }
 
+    public LicenseInvalidException(String tenant) {
+        super("当前租户没有license授权");
+    }
+
 }

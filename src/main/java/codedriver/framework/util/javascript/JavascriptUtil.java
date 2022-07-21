5e62105fe2a75ae31fc5a3ba7910843dae2ffc42
陈其炜 <chenqw@techsure.com.cn>
2022-07-21 12:39:52 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/util/javascript/JavascriptUtil.java
+++ src/main/java/codedriver/framework/util/javascript/JavascriptUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -7,20 +7,19 @@ package codedriver.framework.util.javascript;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
 import java.io.StringWriter;
 
 public class JavascriptUtil {
     static Logger logger = LoggerFactory.getLogger(JavascriptUtil.class);
-    private static final ScriptEngineManager sem = new ScriptEngineManager();
+    //private static final ScriptEngineManager sem = new ScriptEngineManager();
+    private static final NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
 
     public static String transform(Object paramObj, String script) throws ScriptException, NoSuchMethodException {
         return transform(paramObj, script, null);
@@ -36,15 +35,14 @@ public class JavascriptUtil {
     public static boolean runExpression(JSONObject paramObj, String expression) throws ScriptException, NoSuchMethodException {
         //处理is-null和is-not-null两种表达式
         expression = expression.replace("-", "");
-        ScriptEngine se = sem.getEngineByName("nashorn");
+        ScriptEngine se = factory.getScriptEngine("-strict");
+        //ScriptEngine se = sem.getEngineByName("nashorn");
         if (MapUtils.isNotEmpty(paramObj)) {
             for (String key : paramObj.keySet()) {
                 se.put(key, paramObj.get(key));
             }
         }
-        String script = "function run(){\n" +
-                "return " + expression + ";\n" +
-                "}\n";
+        String script = "function run(){\n" + "return " + expression + ";\n" + "}\n";
 
         script += "function calculate(expression, dataValue, conditionValue){\n";
         script += "var calculateClass = Java.type('codedriver.framework.util.javascript.expressionHandler.'+ expression); \n";
@@ -61,7 +59,7 @@ public class JavascriptUtil {
         return false;
     }
 
-    public static void main(String[] v) throws ScriptException, NoSuchMethodException {
+    public static void main2(String[] v) throws ScriptException, NoSuchMethodException {
         JSONObject paramObj = new JSONObject();
         JSONArray attr1 = new JSONArray();
         attr1.add("1");
@@ -87,16 +85,42 @@ public class JavascriptUtil {
         System.out.println(runExpression(paramObj, expression));
     }
 
+    /*public static void main(String[] v) throws ScriptException {
+
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            Runnable r = () -> {
+                ScriptEngine se = factory.getScriptEngine("-strict", "-doe", "--no-java");
+                se.put("a", finalI);
+                se.put("b", 2);
+                se.put("c", 2);
+                try {
+                    System.out.println("result" + finalI + ":" + se.eval("function calFunction(a, b, c){return a+b+c;}calFunction(a,b,c);"));
+                } catch (ScriptException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+            Thread t = new Thread(r);
+            t.start();
+        }
+        System.out.println("======done!");
+    }*/
+
 
     public static String transform(Object paramObj, String script, StringWriter sw) throws ScriptException, NoSuchMethodException {
-        ScriptEngine se = sem.getEngineByName("nashorn");
+        if (StringUtils.isBlank(script)) {
+            if (paramObj != null) {
+                return JSONObject.toJSONString(paramObj);
+            } else {
+                return "{}";
+            }
+        }
+        //ScriptEngine se = sem.getEngineByName("nashorn");
+        ScriptEngine se = factory.getScriptEngine("-strict", "-doe", "--no-java");
         if (sw != null) {
             se.getContext().setWriter(sw);
         }
         se.put("O", paramObj);
-        if (StringUtils.isBlank(script)) {
-            se.put("N", paramObj);
-        }
         se.put("N", null);
         se.eval("try{O = JSON.parse(O);}catch(e){}");
         se.eval(script);

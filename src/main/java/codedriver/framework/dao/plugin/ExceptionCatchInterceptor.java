5f9898a1c6f4a45866f9123ef5c0a287b6eb4e0b
linbq@techsure.com.cn <Besiter789>
2022-09-02 12:18:19 +0800

    --story=1006882 --user=林邦泉 Mysql死锁异常信息打印到deallock.log日志文件 https://www.tapd.cn/54247054/s/1250860

--- src/main/java/codedriver/framework/dao/plugin/ExceptionCatchInterceptor.java
+++ src/main/java/codedriver/framework/dao/plugin/ExceptionCatchInterceptor.java
@@ -33,7 +33,7 @@ import java.sql.SQLException;
 })
 public class ExceptionCatchInterceptor implements Interceptor {
 
-    private static final Logger logger = LoggerFactory.getLogger("deallockAuditAppender");
+    private static final Logger logger = LoggerFactory.getLogger("deallockAudit");
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {

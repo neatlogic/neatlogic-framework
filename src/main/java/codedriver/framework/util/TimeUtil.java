6efeadf77c36cdc72a7cf22ecdb7529521277fc5
赖文韬 <laiwt@techsure.com.cn>
2022-09-13 15:12:08 +0800

    --story=1006866 --user=赖文韬 [自动发布]-持续集成 https://www.tapd.cn/54247054/s/1251756

--- src/main/java/codedriver/framework/util/TimeUtil.java
+++ src/main/java/codedriver/framework/util/TimeUtil.java
@@ -35,6 +35,7 @@ public class TimeUtil {
     private static final String yyyyMMddHHmmss = "yyyyMMddHHmmss";
 
     public static final String HH_MM = "HH:mm";
+    public static final String HH_MM_SS = "HH:mm:ss";
     public static final String YYYY_MM_DD = "yyyy-MM-dd";
     public static final String YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm";
 

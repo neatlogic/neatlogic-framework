83f8d9a3bf2ca2ef939baa0b609443d71913662b
longrf <a5739119>
2022-09-15 10:33:45 +0800

    --bug=1009954 --user=龙儒锋 数据仓库-查看数据时，用日期条件过滤数据异常 https://www.tapd.cn/54247054/s/1252247

--- src/main/java/codedriver/framework/datawarehouse/condition/handler/TimeSelectConditionHandler.java
+++ src/main/java/codedriver/framework/datawarehouse/condition/handler/TimeSelectConditionHandler.java
@@ -6,12 +6,31 @@
 package codedriver.framework.datawarehouse.condition.handler;
 
 import codedriver.framework.datawarehouse.condition.IDatasourceConditionHandler;
+import codedriver.framework.util.TimeUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static codedriver.framework.util.TimeUtil.YYYY_MM_DD_HH_MM_SS;
 
 public class TimeSelectConditionHandler implements IDatasourceConditionHandler {
     @Override
     public String getExpression(Long fieldId, Object value) {
         if (value != null) {
-            return "`" + fieldId + "` like %" + value.toString() + "%";
+            JSONObject object = (JSONObject) value;
+            Integer timeRange = object.getInteger("timeRange");
+            String timeUnit = object.getString("timeUnit");
+            String startTime = object.getString("startTime");
+            String endTime = object.getString("endTime");
+            SimpleDateFormat format = new SimpleDateFormat(YYYY_MM_DD_HH_MM_SS);
+            String startTime1 = "";
+            if (timeRange != null && StringUtils.isNotBlank(timeUnit)) {
+                startTime = TimeUtil.timeTransfer(timeRange, timeUnit);
+                endTime = format.format(new Date());
+            }
+            return "`" + fieldId + "`" + ">= STR_TO_DATE('" + startTime + "','%Y-%m-%d %H:%i:%s')" + "and `" + fieldId + "`<= STR_TO_DATE('" + endTime + "','%Y-%m-%d %H:%i:%s')";
         }
         return null;
     }

31e966b464abb47845f81fe1e50f23a8ac0d2e2e
赖文韬 <laiwt@techsure.com.cn>
2022-10-31 10:40:28 +0800

    fix: 数据仓库text类型条件搜索时SQL拼接错误

--- src/main/java/codedriver/framework/datawarehouse/condition/handler/TextConditionHandler.java
+++ src/main/java/codedriver/framework/datawarehouse/condition/handler/TextConditionHandler.java
@@ -11,7 +11,7 @@ public class TextConditionHandler implements IDatasourceConditionHandler {
     @Override
     public String getExpression(Long fieldId, Object value) {
         if (value != null) {
-            return "`" + fieldId + "` like %" + value.toString() + "%";
+            return "`" + fieldId + "` like '%" + value.toString() + "%'";
         }
         return null;
     }

5ec61d4ba5fb7cc33068b37fb790480d263f79c6
陈其炜 <chenqw@techsure.com.cn>
2022-12-14 15:19:23 +0800

    配置项超时功能
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/common/constvalue/SystemUser.java
+++ src/main/java/codedriver/framework/common/constvalue/SystemUser.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.framework.common.constvalue;
 
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
@@ -8,62 +13,66 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 
-* @Author:14378
-* @Time:2020年7月3日
-* @ClassName: SystemUser 
-* @Description: sla转交策略的定时作业执行转交逻辑时，需要验证权限，system用户拥有流程流转的所有权限
+ * @ClassName: SystemUser
+ * @Description: sla转交策略的定时作业执行转交逻辑时，需要验证权限，system用户拥有流程流转的所有权限
  */
 public enum SystemUser {
-	SYSTEM("system","system","系统"),
-	ANONYMOUS("anonymous","anonymous","匿名用户");
+    SYSTEM("system", "system", "系统"),
+    ANONYMOUS("anonymous", "anonymous", "匿名用户");
 
-	private final Logger logger = LoggerFactory.getLogger(SystemUser.class);
+    private final Logger logger = LoggerFactory.getLogger(SystemUser.class);
 
-	private String userId;
-	private String userUuid;
-	private String userName;
-	private String timezone = "+8:00";
-	private SystemUser(String userId, String userUuid, String userName) {
-		this.userId = userId;
-		this.userUuid = userUuid;
-		this.userName = userName;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public String getUserUuid() {
-		return userUuid;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	
-	public String getTimezone() {
-		return timezone;
-	}
-	public UserVo getUserVo() {
-		UserVo userVo = new UserVo();
-		userVo.setUuid(userUuid);
-		userVo.setUserId(userId);
-		userVo.setUserName(userName);
-		userVo.setTenant(TenantContext.get().getTenantUuid());
-		try {
-			JwtVo jwtVo = LoginAuthHandlerBase.buildJwt(userVo);
-			String authorizatioin = "Bearer_" + jwtVo.getJwthead() + "." + jwtVo.getJwtbody() + "." + jwtVo.getJwtsign();
-			userVo.setAuthorization(authorizatioin);
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return userVo;
-	};
-	
-	public static String getUserName(String userUuid) {
-		for(SystemUser user : values()) {
-			if(user.getUserUuid().equals(userUuid)) {
-				return user.getUserName();
-			}
-		}
-		return "";
-	}
+    private final String userId;
+    private final String userUuid;
+    private final String userName;
+    private final String timezone = "+8:00";
+
+    private SystemUser(String userId, String userUuid, String userName) {
+        this.userId = userId;
+        this.userUuid = userUuid;
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public UserVo getUserVo() {
+        UserVo userVo = new UserVo();
+        userVo.setUuid(userUuid);
+        userVo.setUserId(userId);
+        userVo.setUserName(userName);
+        userVo.setTenant(TenantContext.get().getTenantUuid());
+        try {
+            JwtVo jwtVo = LoginAuthHandlerBase.buildJwt(userVo);
+            String authorization = "Bearer_" + jwtVo.getJwthead() + "." + jwtVo.getJwtbody() + "." + jwtVo.getJwtsign();
+            userVo.setAuthorization(authorization);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return userVo;
+    }
+
+    ;
+
+    public static String getUserName(String userUuid) {
+        for (SystemUser user : values()) {
+            if (user.getUserUuid().equals(userUuid)) {
+                return user.getUserName();
+            }
+        }
+        return "";
+    }
 }

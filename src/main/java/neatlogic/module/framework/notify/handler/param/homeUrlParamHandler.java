18e791fa6e9a60004bc4bbe68e27cfa901229f38
chenqw <zenkiray@live.com>
2023-04-24 15:01:44 +0800

    adjust
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/module/framework/notify/handler/param/homeUrlParamHandler.java
+++ src/main/java/neatlogic/module/framework/notify/handler/param/homeUrlParamHandler.java
@@ -1,17 +1,17 @@
 /*
-Copyright(c) $today.year NeatLogic Co., Ltd. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package neatlogic.module.framework.notify.handler.param;
@@ -22,8 +22,6 @@ import neatlogic.framework.notify.constvalue.CommonNotifyParam;
 import neatlogic.framework.notify.core.INotifyParamHandler;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
 
 /**
  * @author linbq

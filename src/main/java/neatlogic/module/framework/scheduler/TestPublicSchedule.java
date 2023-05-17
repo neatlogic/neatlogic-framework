6ef621f72fbcc209da927618ba8cee9236072b49
lvzk <897706680@qq.com>
2023-05-17 12:23:09 +0800

    update "补充外部定时作业demo"

--- src/main/java/neatlogic/module/framework/scheduler/TestPublicSchedule.java
+++ src/main/java/neatlogic/module/framework/scheduler/TestPublicSchedule.java
@@ -1,8 +1,12 @@
 package neatlogic.module.framework.scheduler;
 
+import neatlogic.framework.scheduler.annotation.Input;
+import neatlogic.framework.scheduler.annotation.Param;
 import neatlogic.framework.scheduler.core.PublicJobBase;
 import neatlogic.framework.scheduler.dto.JobObject;
+import neatlogic.framework.scheduler.dto.JobVo;
 import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +21,18 @@ public class TestPublicSchedule extends PublicJobBase {
         return "TEST";
     }
 
+    @Input({
+            @Param(name = "a",controlType = "text", dataType = "string", description = "test")
+    })
     @Override
     public void executeInternal(JobExecutionContext context, JobObject jobObject) throws Exception {
+        JobDetail jobDetail = context.getJobDetail();
+        String jobUuid = jobDetail.getKey().getName();
+        JobVo jobVo = schedulerMapper.getJobByUuid(jobUuid);
+        if (jobVo != null) {
+            //用户入参
+            jobVo.getPropList();
+        }
 //        String key = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm.SSS").format(jobObject.getLoadTime());
 //        System.out.println("测试：" + new SimpleDateFormat("yyyy-MM-dd HH:ss:mm.SSS").format(new Date()) + ">>>>" + key);
         System.out.println("测试：" + new SimpleDateFormat("yyyy-MM-dd HH:ss:mm.SSS").format(new Date()));

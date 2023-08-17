8d2cd70bf6b1d974cd6256286009b316288b74c5
lvzk <897706680@qq.com>
2023-08-17 19:37:54 +0800

    [功能]localConfig也优先使用nacos的配置,否则才使用本地

--- src/main/java/neatlogic/framework/common/config/LocalConfig.java
+++ src/main/java/neatlogic/framework/common/config/LocalConfig.java
@@ -16,6 +16,8 @@
 
 package neatlogic.framework.common.config;
 
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
 import neatlogic.framework.common.util.RC4Util;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -30,6 +32,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -43,56 +46,49 @@ public class LocalConfig implements BeanFactoryPostProcessor, EnvironmentAware, 
     private Properties properties;
     private ConfigurableEnvironment environment;
 
-
     @Override
     public void setEnvironment(Environment environment) {
         this.environment = (ConfigurableEnvironment) environment;
-    }
-
-    private synchronized String getProperty(String keyName, String defaultValue) {
-        if (properties == null) {
-            properties = new Properties();
-            //加载本地配置
-            try {
-                InputStreamReader is = new InputStreamReader(Objects.requireNonNull(Config.class.getClassLoader().getResourceAsStream(CONFIG_FILE)), StandardCharsets.UTF_8);
-                properties.load(is);
-                is.close();
-            } catch (Exception ex) {
-                logger.error("load " + CONFIG_FILE + " error: " + ex.getMessage(), ex);
-            }
-        }
-        String value = "";
-        if (properties != null) {
-            value = properties.getProperty(keyName, defaultValue);
-            if (StringUtils.isNotBlank(value)) {
-                value = value.trim();
-            }
-        }
-
-        return value;
     }
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         MutablePropertySources propertySources = environment.getPropertySources();
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("db.driverClassName", this.getProperty("db.driverClassName", "com.mysql.cj.jdbc.Driver"));
-        paramMap.put("db.url", this.getProperty("db.url", "jdbc:mysql://localhost:3306/neatlogic?characterEncoding=UTF-8&jdbcCompliantTruncation=false"));
-        paramMap.put("db.username", this.getProperty("db.username", "username"));
-        paramMap.put("db.password", this.getProperty("db.password", "password"));
-        paramMap.put("db.transaction.timeout", this.getProperty("db.transaction.timeout", "-1"));
-        paramMap.put("conn.validationQuery", this.getProperty("conn.validationQuery", "select 1"));
-        paramMap.put("conn.testOnBorrow", this.getProperty("conn.testOnBorrow", "true"));
-        paramMap.put("conn.maxIdle", this.getProperty("conn.maxIdle", "16"));
-        paramMap.put("conn.initialSize", this.getProperty("conn.initialSize", "4"));
-        String mongoHost = this.getProperty("mongo.host", "localhost:27017");
-        String mongoUser = this.getProperty("mongo.username", "root");
-        String mongoPwd = this.getProperty("mongo.password", "root");
-        String mongoDb = this.getProperty("mongo.database", "admin");
-        mongoPwd = RC4Util.decrypt(mongoPwd);
-        paramMap.put("mongo.url", "mongodb://" + mongoUser + ":" + mongoPwd + "@" + mongoHost + "/" + mongoDb);
-        paramMap.put("jms.url", this.getProperty("jms.url", "tcp://localhost:8161"));
-        propertySources.addLast(new MapPropertySource("localconfig", paramMap));
+        Properties prop = new Properties();
+        try {
+            Properties properties = new Properties();
+            properties.put("serverAddr", System.getProperty("nacos.home"));
+            properties.put("namespace", System.getProperty("nacos.namespace"));
+
+            ConfigService configService = NacosFactory.createConfigService(properties);
+            String configInfo = configService.getConfig("config", "neatlogic.framework", 3000);
+            if (StringUtils.isNotBlank(configInfo)) {
+                prop.load(new InputStreamReader(new ByteArrayInputStream(configInfo.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8));
+            } else {
+                // 如果从nacos中读不出配置，则使用本地配置文件配置
+                prop.load(new InputStreamReader(Objects.requireNonNull(Config.class.getClassLoader().getResourceAsStream(CONFIG_FILE)), StandardCharsets.UTF_8));
+            }
+            paramMap.put("db.driverClassName", prop.getProperty("db.driverClassName", "com.mysql.cj.jdbc.Driver"));
+            paramMap.put("db.url", prop.getProperty("db.url", "jdbc:mysql://localhost:3306/neatlogic?characterEncoding=UTF-8&jdbcCompliantTruncation=false"));
+            paramMap.put("db.username", prop.getProperty("db.username", "username"));
+            paramMap.put("db.password", prop.getProperty("db.password", "password"));
+            paramMap.put("db.transaction.timeout", prop.getProperty("db.transaction.timeout", "-1"));
+            paramMap.put("conn.validationQuery", prop.getProperty("conn.validationQuery", "select 1"));
+            paramMap.put("conn.testOnBorrow", prop.getProperty("conn.testOnBorrow", "true"));
+            paramMap.put("conn.maxIdle", prop.getProperty("conn.maxIdle", "16"));
+            paramMap.put("conn.initialSize", prop.getProperty("conn.initialSize", "4"));
+            String mongoHost = prop.getProperty("mongo.host", "localhost:27017");
+            String mongoUser = prop.getProperty("mongo.username", "root");
+            String mongoPwd = prop.getProperty("mongo.password", "root");
+            String mongoDb = prop.getProperty("mongo.database", "admin");
+            mongoPwd = RC4Util.decrypt(mongoPwd);
+            paramMap.put("mongo.url", "mongodb://" + mongoUser + ":" + mongoPwd + "@" + mongoHost + "/" + mongoDb);
+            paramMap.put("jms.url", prop.getProperty("jms.url", "tcp://localhost:8161"));
+            propertySources.addLast(new MapPropertySource("localconfig", paramMap));
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
     @Override

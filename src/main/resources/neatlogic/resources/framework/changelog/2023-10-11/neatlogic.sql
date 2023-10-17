a8f8ba529eb35513efb73b52a836d9c7808e5d59
lvzk <897706680@qq.com>
2023-10-17 15:06:36 +0800

    [功能]changelog sql版本基线 #[983111163150336]changelog sql版本基线 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/983111163150336

--- src/main/resources/neatlogic/resources/framework/changelog/2023-10-11/neatlogic.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2023-10-11/neatlogic.sql
@@ -1,21 +1,3 @@
-CREATE TABLE `changelog_audit`  (
-  `version` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '版本',
-  `tenant_uuid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '租户uuid,主库是0',
-  `module_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '模块id',
-  `sql_hash` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'sql hash,防止重跑',
-  `sql_status` tinyint(1) NULL DEFAULT 1 COMMENT 'sql执行状态',
-  `error_msg` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '异常信息',
-  `ignored` tinyint(1) NULL DEFAULT 0 COMMENT '是否忽略，0:不忽略，1:已忽略，默认不忽略',
-  `lcd` timestamp(3) NULL DEFAULT NULL COMMENT '执行时间',
-  PRIMARY KEY (`tenant_uuid`, `module_id`, `version`, `sql_hash`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
-CREATE TABLE `changelog_audit_detail`  (
-  `hash` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'sql 哈希值',
-  `sql` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT 'sql ',
-  PRIMARY KEY (`hash`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
 ALTER TABLE `mongodb` ADD COLUMN `auth_config` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '存放租户管理用于认证的信息' AFTER `option`;
 
 ALTER TABLE `tenant` MODIFY COLUMN `expire_date` timestamp(3) NULL DEFAULT NULL COMMENT '有效期' AFTER `status`;
@@ -45,10 +27,3 @@ CREATE TABLE `tenant_module_dmlsql_detail`  (
   `sql` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'sql 语句',
   PRIMARY KEY (`hash`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
-CREATE TABLE `version`  (
-  `version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '版本',
-  `fcd` timestamp(3) NULL DEFAULT NULL COMMENT '创建时间',
-  `lcd` timestamp(3) NULL DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`version`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'neatlogic 库版本' ROW_FORMAT = Dynamic;

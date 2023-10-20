d5455d550b9d4fc528c9b54c33904844405bcb81
linbangquan <1437892690@qq.com>
2023-10-20 15:07:46 +0800

    [修复] Ldap同步用户和分组 优化
    关联 #[1005841514201088]Ldap同步用户和分组 优化 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1005841514201088

--- src/main/java/neatlogic/module/framework/scheduler/TestPublicSchedule.java
+++ src/main/java/neatlogic/module/framework/scheduler/TestPublicSchedule.java
@@ -1,16 +1,23 @@
 package neatlogic.module.framework.scheduler;
 
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.scheduler.annotation.Param;
 import neatlogic.framework.scheduler.annotation.Prop;
 import neatlogic.framework.scheduler.core.PublicJobBase;
 import neatlogic.framework.scheduler.dto.JobObject;
+import neatlogic.framework.scheduler.dto.JobVo;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+@Component
 @DisallowConcurrentExecution
 public class TestPublicSchedule extends PublicJobBase {
     @Override
@@ -21,13 +28,21 @@ public class TestPublicSchedule extends PublicJobBase {
     @Prop({
             @Param(name = "a",controlType = "text", dataType = "string", description = "test")
     })
+
+    @Transactional
     @Override
     public void executeInternal(JobExecutionContext context, JobObject jobObject) throws Exception {
         JobDetail jobDetail = context.getJobDetail();
         String jobUuid = jobDetail.getKey().getName();
+        JobVo jobVo = schedulerMapper.getJobBaseInfoByUuid(jobUuid);
+        System.out.println("jobVo = " + JSONObject.toJSONString(jobVo.getName()));
         System.out.println(jobObject.getProp("a"));
 //        String key = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm.SSS").format(jobObject.getLoadTime());
 //        System.out.println("测试：" + new SimpleDateFormat("yyyy-MM-dd HH:ss:mm.SSS").format(new Date()) + ">>>>" + key);
-        System.out.println("测试：" + new SimpleDateFormat("yyyy-MM-dd HH:ss:mm.SSS").format(new Date()));
+        System.out.println("测试：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
+        if (TransactionSynchronizationManager.isSynchronizationActive()) {
+            System.out.println("事务激活");
+        }
+        TimeUnit.MINUTES.sleep(2);
     }
 }

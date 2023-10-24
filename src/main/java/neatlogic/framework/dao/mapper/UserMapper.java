c478bdeee1d16613eb949faa1aefca9fc5bd3595
linbangquan <1437892690@qq.com>
2023-10-24 12:39:48 +0800

    [修复] 用户管理-模糊搜素回显的数据和统计结果不一致
    关联 #[1004401215373312]用户管理-模糊搜素回显的数据和统计结果不一致 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1004401215373312

--- src/main/java/neatlogic/framework/dao/mapper/UserMapper.java
+++ src/main/java/neatlogic/framework/dao/mapper/UserMapper.java
@@ -61,7 +61,7 @@ public interface UserMapper {
 
     List<UserVo> getUserByMail(String value);
 
-    List<String> searchUserBaseInfo(UserVo userVo);
+    List<String> searchUserUuidList(UserVo userVo);
 
     List<UserVo> searchUserDetailInfoByUuidList(List<String> uuidList);
 

63d4292768410f162ce9d432f9d38dece1c4493e
lvzk <897706680@qq.com>
2023-12-19 14:53:25 +0800

    [功能]角色管理-角色增加动态匹配生效字段 #[1014988100042752]角色管理-角色增加动态匹配生效字段 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1014988100042752

--- src/main/resources/neatlogic/resources/framework/changelog/2023-12-19/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2023-12-19/neatlogic_tenant.sql
@@ -7,3 +7,7 @@ ALTER TABLE `user_session` ADD COLUMN `token_create_time` bigint NULL DEFAULT NU
 ALTER TABLE `user_session` DROP PRIMARY KEY;
 
 ALTER TABLE `user_session` ADD PRIMARY KEY (`token_hash`) USING HASH;
+
+ALTER TABLE `role` ADD COLUMN `env` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '生效环境' AFTER `description`;
+
+ALTER TABLE `role` ADD INDEX `idx_env`(`env`) USING BTREE;

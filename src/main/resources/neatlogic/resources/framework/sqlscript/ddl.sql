c0c140f7b4baa747fcba5febf2eb02f57e3a0ebc
chenjg <17688741996@163.com>
2023-12-19 15:57:18 +0800

    [功能] 附加菜单管理

--- src/main/resources/neatlogic/resources/framework/sqlscript/ddl.sql
+++ src/main/resources/neatlogic/resources/framework/sqlscript/ddl.sql
@@ -1420,4 +1420,40 @@ CREATE TABLE IF NOT EXISTS `mail_server`  (
   PRIMARY KEY (`uuid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '邮件服务器表' ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for extramenu
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `extramenu`
+(
+    `id`          BIGINT                                                       NOT NULL COMMENT 'id',
+    `name`        VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
+    `type`        tinyint(1)                                                   NOT NULL DEFAULT '0' COMMENT '类型，0：目录，1：菜单',
+    `is_active`   tinyint(1)                                                   NOT NULL DEFAULT '0' COMMENT '是否激活',
+    `url`         VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci         DEFAULT NULL COMMENT '跳转链接',
+    `description` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci        DEFAULT NULL COMMENT '描述',
+    `parent_id`   BIGINT                                                                DEFAULT NULL COMMENT '父id',
+    `lft`         INT                                                                   DEFAULT NULL COMMENT '左编码',
+    `rht`         INT                                                                   DEFAULT NULL COMMENT '右编码',
+    KEY `idx_lft_rht` (`lft`, `rht`),
+    KEY `idx_parent_id` (`parent_id`)
+) ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='附加菜单表';
+
+-- ----------------------------
+-- Table structure for extramenu_authority
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `extramenu_authority`
+(
+    `menu_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci                          NOT NULL COMMENT '菜单目录id',
+    `type`    enum ('common','user','team','role') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '类型',
+    `uuid`    varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci                          NOT NULL COMMENT 'uuid',
+    PRIMARY KEY (`menu_id`, `type`, `uuid`) USING BTREE,
+    KEY `idx_uuid` (`uuid`) USING BTREE,
+    KEY `idx_menu_id` (`menu_id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='附加菜单授权表';
+
+
 SET FOREIGN_KEY_CHECKS = 1;

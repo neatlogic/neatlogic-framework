082dcd9b7cb112a15fd3713b164227b6152b444f
lvzk <897706680@qq.com>
2024-02-22 19:37:59 +0800

    [功能]补充修改sql

--- src/main/resources/neatlogic/resources/framework/changelog/2024-02-21/neatlogic.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2024-02-21/neatlogic.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `master_user`
 ADD COLUMN `reset_pwd_email_time` timestamp(3) NULL COMMENT '重置密码邮件发送时间' AFTER `is_super_admin`;
+
+ALTER TABLE `master_user_password` ADD UNIQUE INDEX `unique_useruuid`(`user_uuid`) USING BTREE;
+
+ALTER TABLE `master_user_password` ADD UNIQUE INDEX `unique_userid`(`user_id`) USING BTREE;

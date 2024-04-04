29b3f2ae3cfca7a2bda0d3ef121e373be1f8bcad
chenqw <zenkiray@live.com>
2024-04-04 21:24:49 +0800

    -[功能]模型导入支持多文件同时导入，并支持导入模型相互关联
    Signed-off-by: chenqw <zenkiray@live.com>

---
+++ src/main/java/neatlogic/framework/apiparam/validator/FileApiParam.java
@@ -0,0 +1,40 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.framework.apiparam.validator;
+
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.param.validate.core.ApiParamValidatorBase;
+import neatlogic.framework.util.$;
+
+public class FileApiParam extends ApiParamValidatorBase {
+
+    @Override
+    public String getName() {
+        return $.t("common.attachment");
+    }
+
+    @Override
+    public ApiParamType getType() {
+        return ApiParamType.FILE;
+    }
+
+    @Override
+    public boolean validate(Object param, String rule) {
+        System.out.println(param);
+        return true;
+    }
+
+}

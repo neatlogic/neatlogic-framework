e64340274c20a8dc9ceaa3a553bc4ca8f4a19915
lvzk <897706680@qq.com>
2024-04-18 17:49:46 +0800

    [修复]filter通过CommonUtil获取不到request对象

--- src/main/java/neatlogic/framework/common/util/CommonUtil.java
+++ src/main/java/neatlogic/framework/common/util/CommonUtil.java
@@ -1,5 +1,6 @@
 package neatlogic.framework.common.util;
 
+import neatlogic.framework.asynchronization.threadlocal.RequestContext;
 import neatlogic.framework.common.constvalue.DeviceType;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -17,30 +18,30 @@ public class CommonUtil {
 
 
     /**
-     *  JDK8 去重方法
+     * JDK8 去重方法
+     *
      * @param keyExtractor
      * @param <T>
      * @return
      */
-    public static  <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+    public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
         return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
 
     /**
-     * @Description: 获取访问设备类型
-     * @Author: 89770
-     * @Date: 2021/2/5 14:24
-     * @Params: []
-     * @Returns: java.lang.String
+     * 获取访问设备类型
      **/
-    public static String getDevice(){
+    public static String getDevice() {
         String device = DeviceType.PC.getValue();
-        if (RequestContextHolder.getRequestAttributes() != null) {
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-            if (DeviceType.MOBILE.getValue().equals(request.getHeader("Device"))) {
-                device = DeviceType.MOBILE.getValue();
-            }
+        HttpServletRequest request = null;
+        if (RequestContext.get() != null && RequestContext.get().getRequest() != null) {
+            request = RequestContext.get().getRequest();
+        } else if (RequestContextHolder.getRequestAttributes() != null) {
+            request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        }
+        if (request != null && (DeviceType.MOBILE.getValue().equals(request.getHeader("Device")))) {
+            device = DeviceType.MOBILE.getValue();
         }
         return device;
     }

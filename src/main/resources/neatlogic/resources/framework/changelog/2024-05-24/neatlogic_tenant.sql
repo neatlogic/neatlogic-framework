f85c044a7d783a0d935184d710d12e7aaaa31ad2
lvzk <897706680@qq.com>
2024-05-28 16:05:18 +0800

    update sql

--- src/main/resources/neatlogic/resources/framework/changelog/2024-05-24/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2024-05-24/neatlogic_tenant.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `region`
+DROP COLUMN `upward_name_pupward_name_path`,
 ADD COLUMN `upward_id_path` text NULL COMMENT '所有父节点id路径' AFTER `rht`,
-ADD COLUMN `upward_name_pupward_name_path` text NULL COMMENT '所有父节点name路径' AFTER `upward_id_path`;
+ADD COLUMN `upward_name_path` text NULL COMMENT '所有父节点name路径' AFTER `upward_id_path`;

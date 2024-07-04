0018d3075ae54d38d5120b1291fa7df0ae87ef80
lvzk <897706680@qq.com>
2024-07-04 18:51:14 +0800

    [功能]支持不同地方登录同个用户不挤下线 #[1193549586989056]支持不同地方登录同个用户不挤下线 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1193549586989056

--- src/main/java/neatlogic/framework/common/config/Config.java
+++ src/main/java/neatlogic/framework/common/config/Config.java
@@ -92,7 +92,8 @@ public class Config {
     private static String MAINTENANCE;//运维用户，默认是 administrator
     private static String MAINTENANCE_PASSWORD;//运维用户密码
     private static Boolean ENABLE_INTERFACE_VERIFY;//是否激活接口参数校验
-    private static Boolean ENABLE_NO_SECRET;//是否激活免密登录，用户只校验用户名，不校验密码
+    private static Boolean ENABLE_NO_SECRET;//是否激活免密登录，用户只校验用户名，不校验密码以及用户token创建时间
+    private static Boolean VALID_TOKEN_FCD;//校验用户token创建时间
     private static Boolean ENABLE_GZIP; //是否激活数据库大字段压缩
 
     private static String PUBLIC_API_AUTH_USERNAME;//外部访问接口basic认证用户名
@@ -147,6 +148,16 @@ public class Config {
             }
         } else {
             ENABLE_NO_SECRET = false;
+        }
+
+        if (StringUtils.isNotBlank(System.getProperty("enableNoSecret"))) {
+            try {
+                VALID_TOKEN_FCD = Boolean.valueOf(System.getProperty("validTokenFcd"));
+            } catch (Exception ex) {
+                VALID_TOKEN_FCD = true;
+            }
+        } else {
+            VALID_TOKEN_FCD = true;
         }
 
         if (StringUtils.isNotBlank(System.getProperty("enableInterfaceVerify"))) {
@@ -317,6 +328,10 @@ public class Config {
 
     public static boolean ENABLE_NO_SECRET() {
         return ENABLE_NO_SECRET;
+    }
+
+    public static boolean VALID_TOKEN_FCD() {
+        return VALID_TOKEN_FCD;
     }
 
     public static boolean ENABLE_GZIP() {

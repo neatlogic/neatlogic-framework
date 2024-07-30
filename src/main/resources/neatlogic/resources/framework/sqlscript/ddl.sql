7b10797f3a488b33b8799251c0bafcf810c11fbc
1437892690@qq.com <1437892690@qq.com>
2024-07-30 09:48:41 +0800

    [功能] 后端-全局控制用户默认首页
    关联 #[1209375258017792]后端-全局控制用户默认首页 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1209375258017792

--- src/main/resources/neatlogic/resources/framework/sqlscript/ddl.sql
+++ src/main/resources/neatlogic/resources/framework/sqlscript/ddl.sql
@@ -1318,3 +1318,25 @@ CREATE TABLE IF NOT EXISTS `user_session_content` (
   `content` text COLLATE utf8mb4_general_ci COMMENT '信息',
   PRIMARY KEY (`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户会话';
+
+-- ----------------------------
+-- Table structure for home_page
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `home_page` (
+  `id` bigint NOT NULL COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
+  `is_active` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否启用',
+  `sort` int NOT NULL COMMENT '排序',
+  `config` longtext COLLATE utf8mb4_general_ci NOT NULL COMMENT '配置',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT = '首页设置表';
+
+-- ----------------------------
+-- Table structure for home_page_authority
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `home_page_authority` (
+  `home_page_id` bigint NOT NULL COMMENT '首页ID',
+  `type` enum('common','user','team','role') COLLATE utf8mb4_general_ci NOT NULL COMMENT '类型',
+  `uuid` char(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'UUID',
+  PRIMARY KEY (`home_page_id`,`type`,`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='首页设置授权表';

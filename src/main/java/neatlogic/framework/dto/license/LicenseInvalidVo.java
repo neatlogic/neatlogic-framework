12e5a639b1c8f1e05adb5ce34fb3958bc2bd4399
lvzk <897706680@qq.com>
2024-10-24 20:07:14 +0800

    update vo

--- src/main/java/neatlogic/framework/dto/license/LicenseInvalidVo.java
+++ src/main/java/neatlogic/framework/dto/license/LicenseInvalidVo.java
@@ -17,6 +17,7 @@
 
 package neatlogic.framework.dto.license;
 
+import neatlogic.framework.asynchronization.threadlocal.TenantContext;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.dto.module.ModuleGroupVo;
 import neatlogic.framework.restful.annotation.EntityField;
@@ -25,6 +26,8 @@ import java.io.Serializable;
 import java.util.List;
 
 public class LicenseInvalidVo implements Serializable {
+    @EntityField(name = "租户", type = ApiParamType.JSONARRAY)
+    private String tenant;
     @EntityField(name = "模块组", type = ApiParamType.JSONARRAY)
     private List<ModuleGroupVo> moduleGroupVos;
     @EntityField(name = "类型", type = ApiParamType.STRING)
@@ -36,6 +39,15 @@ public class LicenseInvalidVo implements Serializable {
         this.moduleGroupVos = moduleGroupVos;
         this.type = type;
         this.msg = msg;
+        this.tenant = TenantContext.get().getTenantUuid();
+    }
+
+    public String getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(String tenant) {
+        this.tenant = tenant;
     }
 
     public List<ModuleGroupVo> getModuleGroupVos() {

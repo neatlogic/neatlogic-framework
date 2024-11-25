9e0973bb3cc345f7be1877808488ae1fe4061809
chenqw <gdnpc@163.com>
2024-11-25 09:41:29 +0800

    adjust
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/framework/common/dto/BasePageVo.java
+++ src/main/java/neatlogic/framework/common/dto/BasePageVo.java
@@ -77,7 +77,7 @@ public class BasePageVo implements Serializable {
 
     @EntityField(name = "最大分页", type = ApiParamType.INTEGER)
     @JSONField(serialize = false)
-    private int maxPageSize = 100;
+    private int maxPageSize = 500;
     @EntityField(name = "最大限制数，用于拦截", type = ApiParamType.LONG)
     private int policyLimit;
 

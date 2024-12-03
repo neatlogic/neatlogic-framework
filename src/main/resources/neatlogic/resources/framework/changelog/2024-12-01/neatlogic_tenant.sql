c2d6a14b7d891118e93366929cae1ca4d5077bcb
lvzk <897706680@qq.com>
2024-12-03 16:31:47 +0800

    [功能]tagent心跳更新迁移至mongodb

--- src/main/resources/neatlogic/resources/framework/changelog/2024-12-01/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2024-12-01/neatlogic_tenant.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `runner` ADD COLUMN `status` varchar(50) NULL COMMENT '状态' AFTER `is_delete`;
 ALTER TABLE `runner` ADD COLUMN `info` text NULL COMMENT '额外信息' AFTER `status`;
 ALTER TABLE `runner` ADD COLUMN `status_lcd` timestamp(3) NULL COMMENT '状态更新时间' AFTER `info`;
+update tagent set `status` = null , `pcpu` = null, `lcd` = null, `mem` = null, `runner_id` = null, `runner_ip` = null,`runner_port`=null,`runner_group_id` = null;

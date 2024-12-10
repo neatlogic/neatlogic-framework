a0fab6147ca6f3c2fdc121f8c4c41efed66f2c1b
1437892690@qq.com <1437892690@qq.com>
2024-12-10 18:47:55 +0800

    [功能] 系统公告优化
    关联 #[1308783484043264]系统公告优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1308783484043264

---
+++ src/main/java/neatlogic/module/framework/systemnotice/service/SystemNoticeServiceImpl.java
@@ -0,0 +1,224 @@
+/*
+ * Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.module.framework.systemnotice.service;
+
+import neatlogic.framework.asynchronization.thread.NeatLogicThread;
+import neatlogic.framework.asynchronization.threadlocal.UserContext;
+import neatlogic.framework.asynchronization.threadpool.CachedThreadPool;
+import neatlogic.framework.common.config.Config;
+import neatlogic.framework.common.constvalue.GroupSearch;
+import neatlogic.framework.common.constvalue.UserType;
+import neatlogic.framework.common.dto.BasePageVo;
+import neatlogic.framework.common.util.PageUtil;
+import neatlogic.framework.dao.mapper.RoleMapper;
+import neatlogic.framework.dao.mapper.TeamMapper;
+import neatlogic.framework.dao.mapper.UserSessionMapper;
+import neatlogic.framework.service.UserService;
+import neatlogic.framework.systemnotice.dao.mapper.SystemNoticeMapper;
+import neatlogic.framework.systemnotice.dto.SystemNoticeRecipientVo;
+import neatlogic.framework.systemnotice.dto.SystemNoticeUserVo;
+import neatlogic.framework.systemnotice.dto.SystemNoticeVo;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * @Title: SystemNoticeServiceImpl
+ * @Package: neatlogic.framework.systemnotice.service
+ * @Description:
+ * @Author: laiwt
+ * @Date: 2021/1/20 11:02
+ **/
+@Service
+public class SystemNoticeServiceImpl implements SystemNoticeService{
+
+    @Autowired
+    private SystemNoticeMapper systemNoticeMapper;
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    @Autowired
+    private TeamMapper teamMapper;
+
+    @Resource
+    private UserSessionMapper userSessionMapper;
+
+    @Resource
+    private UserService userService;
+
+    @Override
+    public void clearSystemNoticeUser() {
+        List<String> recipientUuidList = getRecipientUuidList();
+        Set<Long> noticeIdList = new HashSet<>();
+        /** 获取system_notice_user中，因公告被删除而残留的记录 **/
+        noticeIdList.addAll(systemNoticeMapper.getNotExistsNoticeIdListFromNoticeUserByUserUuid(UserContext.get().getUserUuid(true)));
+        /** 获取system_notice_user中，因更改公告通知对象而残留的记录  **/
+        noticeIdList.addAll(systemNoticeMapper.getNotInNoticeScopeNoticeIdListByUserUuid(recipientUuidList,UserContext.get().getUserUuid(true)));
+        /** 清理掉上述两种记录 **/
+        if(CollectionUtils.isNotEmpty(noticeIdList)){
+            systemNoticeMapper.deleteSystemNoticeUserByUserUuid(UserContext.get().getUserUuid(true),noticeIdList);
+        }
+    }
+
+    @Override
+    public void stopExpiredSystemNotice() {
+        List<String> recipientUuidList = getRecipientUuidList();
+        List<SystemNoticeVo> expiredNoticeList = systemNoticeMapper.getExpiredNoticeListByRecipientUuidList(recipientUuidList);
+        if (CollectionUtils.isNotEmpty(expiredNoticeList)) {
+            for (SystemNoticeVo vo : expiredNoticeList) {
+                systemNoticeMapper.stopSystemNoticeById(vo);
+            }
+        }
+    }
+
+    @Override
+    public void pullIssuedSystemNotice() {
+//        List<String> recipientUuidList = getRecipientUuidList();
+//        List<Long> issuedNoticeList = systemNoticeMapper.getIssuedNoticeIdListByRecipientUuidList(recipientUuidList);
+//        if (CollectionUtils.isNotEmpty(issuedNoticeList)) {
+//            List<SystemNoticeUserVo> noticeUserVoList = new ArrayList<>();
+//            for (Long id : issuedNoticeList) {
+//                noticeUserVoList.add(new SystemNoticeUserVo(id, UserContext.get().getUserUuid(true)));
+//            }
+//            systemNoticeMapper.batchInsertSystemNoticeUser(noticeUserVoList);
+//        }
+        String userUuid = UserContext.get().getUserUuid();
+        List<String> uuidList = UserContext.get().getUuidList();
+        uuidList.add(userUuid);
+        uuidList.add(UserType.ALL.getValue());
+        systemNoticeMapper.insertInsertSystemNoticeUser(userUuid, uuidList);
+    }
+
+    @Override
+    public void pullActiveSystemNotice() {
+        List<String> recipientUuidList = getRecipientUuidList();
+        List<SystemNoticeVo> hasBeenActiveNoticeList = systemNoticeMapper.getHasBeenActiveNoticeListByRecipientUuidList(recipientUuidList);
+        if (CollectionUtils.isNotEmpty(hasBeenActiveNoticeList)) {
+            List<SystemNoticeUserVo> currentUserNoticeList = new ArrayList<>();
+            /** 更改这些公告的状态为已下发 **/
+            for (SystemNoticeVo vo : hasBeenActiveNoticeList) {
+                vo.setStatus(SystemNoticeVo.Status.ISSUED.getValue());
+                vo.setIssueTime(vo.getStartTime());
+                systemNoticeMapper.updateSystemNoticeStatus(vo);
+                currentUserNoticeList.add(new SystemNoticeUserVo(vo.getId(), UserContext.get().getUserUuid(true)));
+                /** 如果没有忽略已读，那么更改is_read为0 **/
+                if(vo.getIgnoreRead() != null && vo.getIgnoreRead() == 0){
+                    systemNoticeMapper.updateSystemNoticeUserReadStatus(vo.getId(),UserContext.get().getUserUuid(true),0);
+                }
+            }
+            /** 发送给当前用户 **/
+            if (CollectionUtils.isNotEmpty(currentUserNoticeList)) {
+                systemNoticeMapper.batchInsertSystemNoticeUser(currentUserNoticeList);
+            }
+        }
+    }
+
+    @Override
+    public boolean issueSystemNotice(SystemNoticeVo vo) {
+        Integer PAGE_SIZE = 100;
+        /* 如果没有忽略已读，则把system_notice_user中的is_read设为0 **/
+        if (vo.getIgnoreRead() != null && vo.getIgnoreRead() == 0) {
+            /* 经测试，该语句update 53万条数据耗时约1.2s，故不单独开线程执行 **/
+            systemNoticeMapper.updateReadStatusToNotReadByNoticeId(vo.getId());
+        } else {
+            systemNoticeMapper.deleteSystemNoticeUserByNoticeId(vo.getId());
+        }
+
+        List<SystemNoticeRecipientVo> recipientList = systemNoticeMapper.getRecipientListByNoticeId(vo.getId());
+        if (CollectionUtils.isNotEmpty(recipientList)) {
+            long expireTime = System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(Config.USER_EXPIRETIME());
+            if (recipientList.stream().anyMatch(o -> UserType.ALL.getValue().equals(o.getUuid()))) {
+                /* 如果通知范围是所有人，那么找出当前所有的在线用户 **/
+                int allOnlineUserCount = userSessionMapper.getAllOnlineUserCount(new Date(expireTime));
+                if (allOnlineUserCount > 0) {
+                    CachedThreadPool.execute(new NeatLogicThread("NOTICE-INSERTER") {
+                        @Override
+                        protected void execute() {
+                            Date expireDate = new Date(expireTime);
+                            BasePageVo pageVo = new BasePageVo();
+                            pageVo.setPageSize(PAGE_SIZE);
+                            pageVo.setPageCount(PageUtil.getPageCount(allOnlineUserCount, pageVo.getPageSize()));
+                            List<SystemNoticeUserVo> noticeUserVoList = new ArrayList<>();
+                            for (int i = 1; i <= pageVo.getPageCount(); i++) {
+                                pageVo.setCurrentPage(i);
+                                List<String> allOnlineUser = userSessionMapper.getAllOnlineUser(expireDate, pageVo.getStartNum(), pageVo.getPageSize());
+                                if (CollectionUtils.isNotEmpty(allOnlineUser)) {
+                                    allOnlineUser.forEach(o -> noticeUserVoList.add(new SystemNoticeUserVo(vo.getId(), o)));
+                                    systemNoticeMapper.batchInsertSystemNoticeUser(noticeUserVoList);
+                                    noticeUserVoList.clear();
+                                }
+                            }
+                        }
+                    });
+                }
+            } else {
+                List<String> userUuidList = recipientList.stream()
+                        .filter(o -> GroupSearch.USER.getValue().equals(o.getType()))
+                        .map(SystemNoticeRecipientVo::getUuid)
+                        .collect(Collectors.toList());
+                List<String> teamUuidList = recipientList.stream()
+                        .filter(o -> GroupSearch.TEAM.getValue().equals(o.getType()))
+                        .map(SystemNoticeRecipientVo::getUuid)
+                        .collect(Collectors.toList());
+                List<String> roleUuidList = recipientList.stream()
+                        .filter(o -> GroupSearch.ROLE.getValue().equals(o.getType()))
+                        .map(SystemNoticeRecipientVo::getUuid)
+                        .collect(Collectors.toList());
+                List<String> allUserUuidlist = userService.getUserUuidListByUserUuidListAndTeamUuidListAndRoleUuidList(userUuidList, teamUuidList, roleUuidList);
+                if (CollectionUtils.isNotEmpty(allUserUuidlist)) {
+                    CachedThreadPool.execute(new NeatLogicThread("NOTICE-INSERTER") {
+                        @Override
+                        protected void execute() {
+                            Date expireDate = new Date(expireTime);
+                            List<SystemNoticeUserVo> noticeUserVoList = new ArrayList<>();
+                            for (int fromIndex = 0; fromIndex < allUserUuidlist.size(); fromIndex += PAGE_SIZE) {
+                                int toIndex = fromIndex + PAGE_SIZE;
+                                if (toIndex > allUserUuidlist.size()) {
+                                    toIndex = allUserUuidlist.size();
+                                }
+                                List<String> list = allUserUuidlist.subList(fromIndex, toIndex);
+                                List<String> onlineUserList = userSessionMapper.getOnlineUserUuidListByUserUuidListAndGreaterThanSessionTime(list, expireDate);
+                                if (CollectionUtils.isNotEmpty(onlineUserList)) {
+                                    onlineUserList.forEach(o -> noticeUserVoList.add(new SystemNoticeUserVo(vo.getId(), o)));
+                                    systemNoticeMapper.batchInsertSystemNoticeUser(noticeUserVoList);
+                                    noticeUserVoList.clear();
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<String> getRecipientUuidList(){
+        List<String> uuidList = new ArrayList<>();
+        uuidList.add(UserContext.get().getUserUuid(true));
+        uuidList.add(UserType.ALL.getValue());
+        uuidList.addAll(teamMapper.getTeamUuidListByUserUuid(UserContext.get().getUserUuid(true)));
+        uuidList.addAll(roleMapper.getRoleUuidListByUserUuid(UserContext.get().getUserUuid(true)));
+        return uuidList;
+    }
+}

8c868c059b5e4f6e3415f2b1257a3403f106b07c
chenqw <gdnpc@163.com>
2024-12-12 16:18:16 +0800

    -[功能]重建索引增加对es的支持
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/resources/neatlogic/resources/framework/changelog/2024-12-12/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2024-12-12/neatlogic_tenant.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `fulltextindex_rebuild_audit`
     ADD COLUMN `handler` enum ('database','elasticsearch') NULL DEFAULT 'database' COMMENT '处理器' AFTER `server_id`;
 ALTER TABLE `fulltextindex_rebuild_audit`
-    MODIFY COLUMN `handler` enum('database','elasticsearch') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'database' COMMENT '处理器' AFTER `server_id`,
-    DROP PRIMARY KEY,
+    DROP PRIMARY KEY;
+ALTER TABLE `fulltextindex_rebuild_audit`
     ADD PRIMARY KEY (`type`, `handler`) USING BTREE;

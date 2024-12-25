58c19dcb9f54427fd30c310e4003d64acf8c0be3
chenqw <gdnpc@163.com>
2024-12-25 10:41:35 +0800

    -[修改]修正继承乱伦关系导致的显示问题
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/framework/asynchronization/threadlocal/InputFromContext.java
+++ src/main/java/neatlogic/framework/asynchronization/threadlocal/InputFromContext.java
@@ -24,7 +24,7 @@ import java.io.Serializable;
 public class InputFromContext implements Serializable {
     private static final long serialVersionUID = -5732345436786224L;
     @JSONField(serialize = false)
-    private final transient static ThreadLocal<InputFromContext> instance = new ThreadLocal<>();
+    private static final ThreadLocal<InputFromContext> instance = new ThreadLocal<>();
     private InputFrom inputFrom;
 
     public static void init(InputFrom inputFrom) {

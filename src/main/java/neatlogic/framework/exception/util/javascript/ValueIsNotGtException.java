a6ec4147cb9dc7a772ce9f0a3716264191c36a21
chenqw <gdnpc@163.com>
2025-01-07 17:53:48 +0800

    adjust
    Signed-off-by: chenqw <gdnpc@163.com>

---
+++ src/main/java/neatlogic/framework/exception/util/javascript/ValueIsNotGtException.java
@@ -0,0 +1,24 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+package neatlogic.framework.exception.util.javascript;
+
+import neatlogic.framework.exception.core.ApiRuntimeException;
+
+
+public class ValueIsNotGtException extends ApiRuntimeException {
+    public ValueIsNotGtException(String prefix, Double value, Double otherValue) {
+        super("{0}值{1}不大于{2}", prefix, value, otherValue);
+    }
+}
---
+++ src/main/java/neatlogic/framework/util/javascript/expressionHandler/gt.java
@@ -0,0 +1,65 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.framework.util.javascript.expressionHandler;
+
+import com.alibaba.fastjson.JSONArray;
+import neatlogic.framework.exception.core.ApiRuntimeException;
+import neatlogic.framework.exception.util.javascript.ConditionIsIrregularException;
+import neatlogic.framework.exception.util.javascript.ValueIsIrregularException;
+import neatlogic.framework.exception.util.javascript.ValueIsNotGtException;
+import neatlogic.framework.util.javascript.JavascriptUtil;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class gt {
+    private static final Logger logger = LoggerFactory.getLogger(gt.class);
+
+    public static boolean calculate(JSONArray dataValueList, JSONArray conditionValueList, String label) {
+        String prefix = (StringUtils.isNotBlank(label) ? label + "的" : "");
+        List<ApiRuntimeException> errorList = JavascriptUtil.getErrorList();
+        if (CollectionUtils.isNotEmpty(dataValueList) && CollectionUtils.isNotEmpty(conditionValueList)) {
+            for (int i = 0; i < dataValueList.size(); i++) {
+                Double d;
+                try {
+                    d = dataValueList.getDouble(i);
+                } catch (Exception e) {
+                    errorList.add(new ValueIsIrregularException(prefix));
+                    return false;
+                }
+                for (int j = 0; j < conditionValueList.size(); j++) {
+                    Double c;
+                    try {
+                        c = conditionValueList.getDouble(i);
+                    } catch (Exception e) {
+                        errorList.add(new ConditionIsIrregularException(prefix));
+                        return false;
+                    }
+                    if (d <= c) {
+                        errorList.add(new ValueIsNotGtException(prefix, d, c));
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+}

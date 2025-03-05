db172e5f925a5eeb809a230ac034345fe583ddf3
lvzk <897706680@qq.com>
2025-03-05 11:57:19 +0800

    [功能]补充附件类型

---
+++ src/main/java/neatlogic/framework/systemnotice/file/SystemNoticeFileHandler.java
@@ -0,0 +1,53 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.framework.systemnotice.file;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.file.core.FileTypeHandlerBase;
+import neatlogic.framework.file.dto.FileVo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SystemNoticeFileHandler extends FileTypeHandlerBase {
+
+
+    @Override
+    public boolean valid(String userUuid, FileVo fileVo, JSONObject jsonObj) {
+        return true;
+    }
+
+    @Override
+    public boolean validDeleteFile(FileVo fileVo) {
+        return true;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "文件型属性附件";
+    }
+
+    @Override
+    protected boolean myDeleteFile(FileVo fileVo, JSONObject paramObj) {
+        return true;
+    }
+
+
+    @Override
+    public String getName() {
+        return "SYSTEM_NOTICE";
+    }
+
+}

9bb4560185a7c47e2720f13b37c693e8c6502a5e
lvzk <lvzk@neatlogic.com>
2025-03-27 14:00:20 +0000

    Revert "[修复]tagent心跳上下文泄漏"
    This reverts commit 8f991fc9ab19d456ced7d56ccd43495dbe02f306.

--- src/main/java/neatlogic/framework/asynchronization/queue/NeatLogicUniqueBlockingQueue.java
+++ src/main/java/neatlogic/framework/asynchronization/queue/NeatLogicUniqueBlockingQueue.java
@@ -60,7 +60,12 @@ public class NeatLogicUniqueBlockingQueue<T> {
     public T take() throws InterruptedException {
         Task<T> task = blockingQueue.take(); // 阻塞式获取任务
         taskMap.remove(task.getUniqueKey()); // 移除已处理任务的唯一标记
-        TenantContext.init().switchTenant(task.getTenantUuid());
+        TenantContext tenantContext = TenantContext.get();
+        if (tenantContext != null) {
+            tenantContext.switchTenant(task.getTenantUuid());
+        } else {
+            TenantContext.init(task.getTenantUuid());
+        }
         return task.getT();
     }
 

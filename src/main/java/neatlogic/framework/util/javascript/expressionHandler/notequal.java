9a59c32094237f396c9201d020cde84483e1e042
chenqw <gdnpc@163.com>
2025-05-21 12:16:54 +0800

    -[修复]equal和notequal优化比较逻辑，忽略值类型，忽略大小写
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/framework/util/javascript/expressionHandler/notequal.java
+++ src/main/java/neatlogic/framework/util/javascript/expressionHandler/notequal.java
@@ -24,20 +24,34 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class notequal {
     private static final Logger logger = LoggerFactory.getLogger(notequal.class);
 
+    private static List<String> convertJsonArray(JSONArray list) {
+        List<String> result = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(list)) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) != null) {
+                    result.add(list.getString(i).toLowerCase());
+                }
+            }
+        }
+        result.sort(Comparator.comparing(String::toLowerCase));
+        return result;
+    }
+
     public static boolean calculate(JSONArray dataValueList, JSONArray conditionValueList, String label) {
         String prefix = (StringUtils.isNotBlank(label) ? label + "的" : "");
         List<ApiRuntimeException> errorList = JavascriptUtil.getErrorList();
         if (CollectionUtils.isNotEmpty(dataValueList) && CollectionUtils.isNotEmpty(conditionValueList)) {
             if (dataValueList.size() == conditionValueList.size()) {
-                dataValueList.sort(Comparator.comparing(Object::toString));
-                conditionValueList.sort(Comparator.comparing(Object::toString));
-                if (!dataValueList.toString().trim().equalsIgnoreCase(conditionValueList.toString().trim())) {
+                List<String> newDataList = convertJsonArray(dataValueList);
+                List<String> newConditionList = convertJsonArray(conditionValueList);
+                if (!newDataList.equals(newConditionList)) {
                     return true;
                 } else {
                     ApiRuntimeException error = new ValueIsEqualException(prefix, getValue(dataValueList), getValue(conditionValueList));

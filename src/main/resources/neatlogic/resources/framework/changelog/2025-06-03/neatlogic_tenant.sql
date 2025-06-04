5072168e9b481bf053ec18ad7c42ac88e0fc3942
linbangquan <1437892690@qq.com>
2025-06-04 07:12:50 +0000

    !201 [功能] neatlogic数据仓库支持配置第三方数据库获取数据 * [功能] neatlogic数据仓库支持配置第三方数据库获取数据

--- src/main/resources/neatlogic/resources/framework/changelog/2025-06-03/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2025-06-03/neatlogic_tenant.sql
@@ -9,3 +9,6 @@ CREATE TABLE `database` (
 
 ALTER TABLE `datawarehouse_datasource`
   ADD COLUMN `database_id` BIGINT NULL COMMENT '数据源' AFTER `db_type`;
+
+ALTER TABLE `datawarehouse_datasource`
+    CHANGE `db_type` `db_type` ENUM ('mysql', 'mongodb', 'jdbc') CHARSET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'mysql' NULL COMMENT '数据库类型';

4301b33bd2a8ba8aae52dd7473a002f8cf754aa6
1437892690@qq.com <1437892690@qq.com>
2025-06-04 14:04:07 +0800

    [功能] neatlogic数据仓库支持配置第三方数据库获取数据
    关联 #[1432467032998096]neatlogic数据仓库支持配置第三方数据库获取数据 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1432467032998096

--- src/main/resources/neatlogic/resources/framework/sqlscript/ddl.sql
+++ src/main/resources/neatlogic/resources/framework/sqlscript/ddl.sql
@@ -120,6 +120,7 @@ CREATE TABLE IF NOT EXISTS  `database` (
     `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
     `type` varchar(100) COLLATE utf8mb4_general_ci NOT NULL COMMENT '类型',
     `config` text COLLATE utf8mb4_general_ci NOT NULL COMMENT '配置信息',
+    `file_id_list` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '附件id列表',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 

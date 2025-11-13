48a045660c6d6b1b64137499b4699873cbfeaf0c
1437892690@qq.com <1437892690@qq.com>
2025-11-13 15:12:46 +0800

    [功能] 表单组件优化
    关联 #[1552833097203712]表单组件优化 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1552833097203712

--- src/main/java/neatlogic/framework/form/constvalue/FormHandler.java
+++ src/main/java/neatlogic/framework/form/constvalue/FormHandler.java
@@ -41,7 +41,6 @@ public enum FormHandler implements IFormHandler {
     FORMTAB("formtab", "选项卡"),
     FORMCOLLAPSE("formcollapse", "折叠面板"),
     FORMSUBASSEMBLY("formsubassembly", "子表单"),
-    FORMAUTOEXECSERVICE("formautoexecservice", "自动化服务组件"),
     FORMCUSTOM("formcustom", "自定义组件");
 
     private final String handler;

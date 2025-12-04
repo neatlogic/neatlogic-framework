a0945461cc568db9c436d86c1a7bc0dd27a57812
1437892690@qq.com <1437892690@qq.com>
2025-12-04 11:33:18 +0800

    [功能] 屏蔽定时作业健康检查失败时打印日志
    关联 #[1568762954743810]屏蔽定时作业健康检查失败时打印日志 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1568762954743810

--- src/main/java/neatlogic/framework/scheduler/core/JobBase.java
+++ src/main/java/neatlogic/framework/scheduler/core/JobBase.java
@@ -192,7 +192,7 @@ public abstract class JobBase implements IJob {
             // 例如：A Server 修改cron 每天0点跑作业，B Server 修改cron每分钟跑。 当A Server 发现not healthy 则会unload 并删除status，lock。后续判断会导致B Server 也不会再跑作业。
             // 应该有jobHandler来自行判断是否需要unloadJob
             //schedulerManager.unloadJob(jobObject);
-            logger.error("执行定时作业(jobName={},jobGroup={})时，isHealthy()方法检查出作业不正常", jobName, jobGroup);
+//            logger.error("执行定时作业(jobName={},jobGroup={})时，isHealthy()方法检查出作业不正常", jobName, jobGroup);
             return;
         }
         Date currentFireTime = context.getFireTime();// 本次执行激活时间

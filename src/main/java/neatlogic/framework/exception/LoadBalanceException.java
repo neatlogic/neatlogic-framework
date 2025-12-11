554865de65afb8a9620cb458aa1cc7b29323e082
lvzk <897706680@qq.com>
2025-12-11 19:03:26 +0800

    [功能]清除缓存补充 serverId和会话信息

---
+++ src/main/java/neatlogic/framework/exception/LoadBalanceException.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2025  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.framework.exception;
+
+import neatlogic.framework.exception.core.ApiRuntimeException;
+
+import java.io.Serial;
+
+public class LoadBalanceException extends ApiRuntimeException {
+    @Serial
+    private static final long serialVersionUID = -260889166393833441L;
+
+    public LoadBalanceException(int sourceServerId, int targetServerId){
+        super("后端节点漂移，权限服务器ID“{0}”，当前服务器ID“{1}”,请联系管理员！",sourceServerId,targetServerId);
+    }
+}

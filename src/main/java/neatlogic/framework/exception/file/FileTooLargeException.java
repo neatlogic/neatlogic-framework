7744ffa10b7717f2efc3a8b8be2404ec4256dfb7
1437892690@qq.com <1437892690@qq.com>
2025-12-17 10:19:38 +0800

    [功能] 登录记录不记录系统用户
    关联 #[1578147013689352]登录记录不记录系统用户 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1578147013689352

--- src/main/java/neatlogic/framework/exception/file/FileTooLargeException.java
+++ src/main/java/neatlogic/framework/exception/file/FileTooLargeException.java
@@ -5,7 +5,7 @@ import neatlogic.framework.exception.core.ApiRuntimeException;
 @SuppressWarnings("serial")
 public class FileTooLargeException extends ApiRuntimeException {
     public FileTooLargeException(Long fileSize, Long maxSize) {
-        super("附件最大支持：{1}字节，当前文件：{0}字节", maxSize, fileSize);
+        super("附件最大支持：{0}字节，当前文件：{1}字节", maxSize, fileSize);
     }
 
 }

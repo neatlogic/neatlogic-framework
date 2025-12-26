0c4cd08a89b1c509eac3ed0325e380c0067bc771
chenqw <gdnpc@163.com>
2025-12-26 17:36:13 +0800

    adjust
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/framework/asynchronization/threadpool/ScheduledThreadPool.java
+++ src/main/java/neatlogic/framework/asynchronization/threadpool/ScheduledThreadPool.java
@@ -13,16 +13,14 @@ import java.util.concurrent.TimeUnit;
  * @Title: ScheduledThreadPool
  * @Package: neatlogic.framework.asynchronization.threadpool
  * @Description: 定时执行线程池
- * @author: chenqiwei
- * @date: 2021/3/19:50 上午
  **/
 public class ScheduledThreadPool {
-    private static final Log logger = LogFactory.getLog(CachedThreadPool.class);
-    private static final ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(1);
+    private static final Log logger = LogFactory.getLog(ScheduledThreadPool.class);
+    private static final ScheduledExecutorService pool = Executors.newScheduledThreadPool(1);
 
     public static void execute(NeatLogicThread command, long delay) {
         try {
-            scheduledThreadPool.schedule(command, delay, TimeUnit.SECONDS);
+            pool.schedule(command, delay, TimeUnit.SECONDS);
         } catch (RejectedExecutionException ex) {
             logger.error(ex.getMessage(), ex);
         }

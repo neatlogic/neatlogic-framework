1a2fce2f6bc529abb58b3b13e68b94d674162755
chenqw <gdnpc@163.com>
2026-01-16 11:12:24 +0800

    adjust
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/resources/neatlogic/resources/framework/changelog/2025-09-10/neatlogic.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2025-09-10/neatlogic.sql
@@ -1,5 +1,8 @@
-ALTER TABLE `tenant_module_dmlsql` ADD COLUMN `ignored` tinyint(1) NULL COMMENT '是否忽略，0:不忽略，1:已忽略，默认忽略' AFTER `fcd`;
+ALTER TABLE `tenant_module_dmlsql`
+    ADD COLUMN `ignored` tinyint(1) NULL COMMENT '是否忽略，0:不忽略，1:已忽略，默认忽略' AFTER `fcd`;
 
-ALTER TABLE `tenant_module` DROP COLUMN `ddl_status`,
+ALTER TABLE `tenant_module`
+    DROP COLUMN `ddl_status`;
 
-ALTER TABLE `tenant_module` DROP COLUMN `dml_status`;
+ALTER TABLE `tenant_module`
+    DROP COLUMN `dml_status`;

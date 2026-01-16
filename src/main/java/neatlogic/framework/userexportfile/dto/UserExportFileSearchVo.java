320aab3e17998f63c0c3b468e5583f96202d1641
1437892690@qq.com <1437892690@qq.com>
2026-01-16 17:37:42 +0800

    [功能] 异步导出功能
    关联 #[1512923648720896]异步导出功能 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1512923648720896

---
+++ src/main/java/neatlogic/framework/userexportfile/dto/UserExportFileSearchVo.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package neatlogic.framework.userexportfile.dto;
+
+import neatlogic.framework.common.dto.BasePageVo;
+
+import java.util.Date;
+
+public class UserExportFileSearchVo extends BasePageVo {
+    private String userUuid;
+    private Date startTime;
+    private Date endTime;
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+    public void setUserUuid(String userUuid) {
+        this.userUuid = userUuid;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
---
+++ src/main/java/neatlogic/framework/userexportfile/exception/UserExportFileDeleteDeniedException.java
@@ -0,0 +1,16 @@
+package neatlogic.framework.userexportfile.exception;
+
+import neatlogic.framework.exception.core.ApiRuntimeException;
+
+import java.io.Serial;
+
+public class UserExportFileDeleteDeniedException extends ApiRuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 3196317808541689156L;
+
+    public UserExportFileDeleteDeniedException(String name) {
+        super("您没有权限删除{0}文件");
+    }
+
+}

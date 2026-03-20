17493379f4d7fb83f19f6a37792337a9c414c0b5
linbangquan <1437892690@qq.com>
2026-03-20 12:55:08 +0000

    !311 [功能] 矩阵属性数据查询-下拉接口优化
    * [功能] 矩阵属性数据查询-下拉接口优化

---
+++ src/main/java/neatlogic/framework/crossover/IMatrixCrossoverService.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package neatlogic.framework.crossover;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.matrix.dto.MatrixFilterVo;
+
+import java.util.List;
+
+public interface IMatrixCrossoverService extends ICrossoverService {
+
+    JSONObject searchMatrixColumnDataForSelect(
+            String matrixUuid,
+            String keywordColumn,
+            String valueField,
+            String textField,
+            List<String> hiddenFieldList,
+            List<MatrixFilterVo> filterList,
+            String keyword,
+            Integer currentPage,
+            Integer pageSize,
+            Boolean needPage,
+            JSONArray defaultValue
+    );
+
+    JSONObject searchMatrixColumnDataForSelect(
+            String keyword,
+            Integer currentPage,
+            Integer pageSize,
+            Boolean needPage,
+            JSONArray defaultValue,
+            String matrixLabel,
+            String keywordColumnUniqueIdentifier,
+            String valueFieldUniqueIdentifier,
+            String textFieldUniqueIdentifier,
+            List<String> hiddenFieldUniqueIdentifierList,
+            List<MatrixFilterVo> filterList
+    );
+}

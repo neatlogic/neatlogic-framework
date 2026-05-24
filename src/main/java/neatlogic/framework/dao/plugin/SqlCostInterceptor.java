c2de162ad704eca6cbf48ec0ed3ec67adea16504
1437892690@qq.com <1437892690@qq.com>
2026-05-24 13:46:25 +0800

    [功能] sql监控增加统计指定接口拦截统计
    关联 #[1460056485232640]sql监控增加统计指定接口拦截统计 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1460056485232640

--- src/main/java/neatlogic/framework/dao/plugin/SqlCostInterceptor.java
+++ src/main/java/neatlogic/framework/dao/plugin/SqlCostInterceptor.java
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
  * This file is part of the NeatLogic software.
- * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x - 2025.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
  * You may use this file only in compliance with the License.
  * See the LICENSE file distributed with this work for the full license text.
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,

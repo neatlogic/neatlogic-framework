bdc54474a56b698fae98b670b6aa95978e87b996
1437892690@qq.com <1437892690@qq.com>
2026-05-25 16:23:29 +0800

    [功能] serverid增加分组概念：接管作业只能接管同组server；定时作业只能看到自己组内的数据
    关联 #[1416699956396032]serverid增加分组概念：接管作业只能接管同组server；定时作业只能看到自己组内的数据 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1416699956396032

---
+++ src/main/resources/neatlogic/resources/framework/changelog/2026-05-25/neatlogic.sql
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+ALTER TABLE `server_status`
+    ADD COLUMN `server_group` VARCHAR(50) NULL COMMENT '应用服务分组' AFTER `server_id`;

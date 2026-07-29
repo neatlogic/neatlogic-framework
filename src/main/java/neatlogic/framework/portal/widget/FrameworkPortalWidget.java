3ee496b7db6d6211f2f0f54025a34e9b7fec2168
1437892690@qq.com <1437892690@qq.com>
2026-07-29 19:27:47 +0800

    [功能] 后端-门户管理接口
    关联 #[1734939029274631]后端-门户管理接口 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1734939029274631

---
+++ src/main/java/neatlogic/framework/portal/widget/FrameworkPortalWidget.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package neatlogic.framework.portal.widget;
+
+import neatlogic.framework.portal.widget.core.IPortalWidget;
+import neatlogic.framework.portal.widget.core.IPortalWidgetGroup;
+
+public enum FrameworkPortalWidget implements IPortalWidget {
+    welcomeOverview("welcomeOverview", "欢迎概况", 1, FrameworkPortalWidgetGroup.frameworkGroup1),
+    todayFocus("todayFocus", "今日关注", 2, FrameworkPortalWidgetGroup.frameworkGroup1),
+    quickEntry("quickEntry", "快捷入口", 3, FrameworkPortalWidgetGroup.frameworkGroup2),
+    notice("notice", "系统通知", 4, FrameworkPortalWidgetGroup.frameworkGroup2),
+    recentAccess("recentAccess", "最近访问", 5, FrameworkPortalWidgetGroup.frameworkGroup2),
+    calendar("calendar", "日历提醒", 6, FrameworkPortalWidgetGroup.frameworkGroup2),
+    ;
+    private final String value;
+    private final String text;
+    private final Integer sort;
+    private final IPortalWidgetGroup group;
+
+    FrameworkPortalWidget(String value, String text, Integer sort, IPortalWidgetGroup group) {
+        this.value = value;
+        this.text = text;
+        this.sort = sort;
+        this.group = group;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String getText() {
+        return this.text;
+    }
+
+    @Override
+    public Integer getSort() {
+        return this.sort;
+    }
+
+    @Override
+    public IPortalWidgetGroup getGroup() {
+        return this.group;
+    }
+}

5244b4c4713b893f37fb2abe8cefd0f86352b6c8
1437892690@qq.com <1437892690@qq.com>
2026-08-27 17:41:55 +0800

    [功能] 将数据仓库数据源移到commercial
    关联 #[1760227318562821]将数据仓库数据源移到commercial http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1760227318562821

--- src/main/resources/neatlogic/resources/framework/changelog/2025-06-03/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/framework/changelog/2025-06-03/neatlogic_tenant.sql
@@ -1,12 +1,3 @@
-CREATE TABLE `database` (
-    `id` bigint NOT NULL COMMENT '主键ID',
-    `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
-    `type` varchar(100) COLLATE utf8mb4_general_ci NOT NULL COMMENT '类型',
-    `config` text COLLATE utf8mb4_general_ci NOT NULL COMMENT '配置信息',
-    `file_id_list` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '附件id列表',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 ALTER TABLE `datawarehouse_datasource`
   ADD COLUMN `database_id` BIGINT NULL COMMENT '数据源' AFTER `db_type`;
 

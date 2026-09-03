52c3c5f8fb38c878e2fd6b6928f9be0c13253277
1437892690@qq.com <1437892690@qq.com>
2026-09-03 17:32:00 +0800

    [功能] 后端-机房管理
    关联 #[1729096900673543]后端-机房管理 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1729096900673543

--- src/main/java/neatlogic/framework/form/attribute/core/IFormAttributeHandler.java
+++ src/main/java/neatlogic/framework/form/attribute/core/IFormAttributeHandler.java
@@ -250,7 +250,7 @@ public interface IFormAttributeHandler {
      * 保存属性数据后需要的自定义操作
      *
      */
-    default void beforeSaveData(AttributeDataVo attributeDataVo, FormAttributeVo fromAttributeVo, Long sourceId, String source) {
-
-    }
+//    default void beforeSaveData(AttributeDataVo attributeDataVo, FormAttributeVo fromAttributeVo, Long sourceId, String source) {
+//
+//    }
 }

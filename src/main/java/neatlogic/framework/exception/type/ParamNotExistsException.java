bac4dd35fdf9fdee8d3da3b156b1a2dd13610e33
lvzk <897706680@qq.com>
2026-09-23 11:07:56 +0800

    补充翻译

--- src/main/java/neatlogic/framework/exception/type/ParamNotExistsException.java
+++ src/main/java/neatlogic/framework/exception/type/ParamNotExistsException.java
@@ -13,6 +13,7 @@
 package neatlogic.framework.exception.type;
 
 import neatlogic.framework.exception.core.ApiRuntimeException;
+import neatlogic.framework.util.$;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,32 +22,50 @@ public class ParamNotExistsException extends ApiRuntimeException {
 
     private static final long serialVersionUID = -2608671566655133328L;
 
+    /**
+     * 构造单个必填参数缺失异常。
+     */
     public ParamNotExistsException(String paramNames) {
-        super("参数“{0}”不能为空", paramNames);
+        super("nfe.paramnotexistsexception.required", paramNames);
     }
 
+    /**
+     * 构造数组元素字段缺失异常。
+     */
     public ParamNotExistsException(int index, String keyName) {
-        super("第{0}个参数的“{2}”不能为空", index, keyName);
+        super("nfe.paramnotexistsexception.indexedfieldrequired", index, keyName);
     }
 
+    /**
+     * 构造具名数组参数元素字段缺失异常。
+     */
     public ParamNotExistsException(int index, String paramName, String keyName) {
-        super("第{0}个参数“{1}”的“{2}”不能为空", index, paramName, keyName);
+        super("nfe.paramnotexistsexception.namedindexedfieldrequired", index, paramName, keyName);
     }
 
+    /**
+     * 构造多个参数不能同时为空的异常。
+     */
     public ParamNotExistsException(String... paramNames) {
-        super("参数“{0}”不能同时为空", String.join("、", paramNames));
+        super("nfe.paramnotexistsexception.allblank", String.join($.t("nfe.paramnotexistsexception.groupseparator"), paramNames));
     }
 
+    /**
+     * 构造互斥参数组至少填写一组的异常。
+     */
     @SafeVarargs
     public ParamNotExistsException(List<String>... paramGroups) {
-        super(buildMessage(paramGroups));
+        super("nfe.paramnotexistsexception.selectonegroup", buildGroupText(paramGroups));
     }
 
-    private static String buildMessage(List<String>... paramGroups) {
+    /**
+     * 根据当前语言连接参数组，仅格式化参数标识，不翻译业务数据。
+     */
+    private static String buildGroupText(List<String>... paramGroups) {
         List<String> groupStrList = new ArrayList<>();
         for (List<String> group : paramGroups) {
-            groupStrList.add("[" + String.join("、", group) + "]");
+            groupStrList.add("[" + String.join($.t("nfe.paramnotexistsexception.groupseparator"), group) + "]");
         }
-        return "必须在" + String.join("、", groupStrList) + "中选择一组填写";
+        return String.join($.t("nfe.paramnotexistsexception.groupseparator"), groupStrList);
     }
 }
